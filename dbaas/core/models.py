350e04678b3e8f53b5ce79c3a3f86adfe4a96e1a
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-17 10:31:58 -0300

    removing base class

--- dbaas/core/models.py
+++ dbaas/core/models.py
@@ -3,11 +3,3 @@ from datetime import datetime
 from django.db import models
 
 
-class ExtendedModel(models.Model):
-    """Base model class"""
-
-    created_at = models.DateTimeField(verbose_name=_("created_at"), auto_now_add=True, default=datetime.now())
-    updated_at = models.DateTimeField(verbose_name=_("updated_at"), auto_now=True, default=datetime.now())
-
-    class Meta:
-        abstract = True
