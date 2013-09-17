53a262f6a9299c98aaa1b98bcc955e848e72b716
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-17 18:12:12 -0300

    list display environment

--- dbaas/base/admin/environment.py
+++ dbaas/base/admin/environment.py
@@ -6,4 +6,5 @@ from base.models import Environment
 class EnvironmentAdmin(admin.ModelAdmin):
     search_fields = ("name", )
     list_filter = ("is_active", )
+    list_display = ("name", "is_active")
     save_on_top = True
