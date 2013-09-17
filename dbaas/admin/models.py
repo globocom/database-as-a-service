5870289601dcd6f036c8ef15ea5f8af3a1ec6275
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-17 18:11:15 -0300

    empty models

---
+++ dbaas/admin/models.py
@@ -0,0 +1,2 @@
+# coding=utf-8
+from django.db import models
--- dbaas/base/admin/environment.py
+++ dbaas/base/admin/environment.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from base.models import Environment
 
 class EnvironmentAdmin(admin.ModelAdmin):
-    search_fields = ["name"]
+    search_fields = ("name", )
     list_filter = ("is_active", )
     save_on_top = True
