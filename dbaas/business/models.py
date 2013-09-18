74a135e4d95e1d49c7dfd1d401d1441c945c327e
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-18 17:25:04 -0300

    typo

--- dbaas/business/models.py
+++ dbaas/business/models.py
@@ -8,8 +8,8 @@ from base.models import BaseModel
 
 class Product(BaseModel):
 
-    name = models.CharField(verbose_name=_("product_name"), max_length=100, unique=True)
-    is_active = models.BooleanField(verbose_name=_("product_is_active"), default=True)
+    name = models.CharField(verbose_name=_("Product name"), max_length=100, unique=True)
+    is_active = models.BooleanField(verbose_name=_("Is product active"), default=True)
     slug = models.SlugField()
 
     def __unicode__(self):
@@ -18,8 +18,8 @@ class Product(BaseModel):
 
 class Plan(BaseModel):
 
-    name = models.CharField(verbose_name=_("plan_name"), max_length=100, unique=True)
-    is_active = models.BooleanField(verbose_name=_("plan_is_active"), default=True)
+    name = models.CharField(verbose_name=_("Plan name"), max_length=100, unique=True)
+    is_active = models.BooleanField(verbose_name=_("Is plan active"), default=True)
     environment = models.ManyToManyField("base.Environment", related_name="plans")
 
     def __unicode__(self):
@@ -28,8 +28,8 @@ class Plan(BaseModel):
 
 class PlanAttribute(BaseModel):
 
-    name = models.CharField(verbose_name=_("plan_attribute_name"), max_length=200)
-    value = models.CharField(verbose_name=_("plan_attribute_value"), max_length=200)
+    name = models.CharField(verbose_name=_("Plan attribute name"), max_length=200)
+    value = models.CharField(verbose_name=_("Plan attribuge value"), max_length=200)
     plan = models.ForeignKey('business.Plan', related_name="plan_attributes")
 
 simple_audit.register(Product, Plan, PlanAttribute)
