65e96306c48f4e04cb3200c5238cb0b5f87e0710
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-18 14:01:14 -0300

    host list display

--- dbaas/base/admin/host.py
+++ dbaas/base/admin/host.py
@@ -6,5 +6,6 @@ from ..service import HostService
 class HostAdmin(admin.DjangoServicesAdmin):
     service_class = HostService
     search_fields = ["fqdn"]
+    list_display = ("fqdn", "environment", "is_active")
     list_filter = ("is_active", )
     save_on_top = True
