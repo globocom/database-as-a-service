c910b507be12dea5701eb88036bcdef461b97d41
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-19 11:27:27 -0300

    add mapping to server static files in debug mode

--- dbaas/dbaas/urls.py
+++ dbaas/dbaas/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
-import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -20,4 +20,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url('^api/', include(django_services.api.urls))
 
-) + static(settings.ADMIN_STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
