cead18de67622789123dc951399f1c5326e0412c
Silvano Buback <silvano@corp.globo.com>
2013-09-20 18:44:21 -0300

    moved call_script from mongo/engine.py to base/engine.py. Create base methods to support mongo calls

--- dbaas/base/engine/base.py
+++ dbaas/base/engine/base.py
@@ -1,3 +1,13 @@
+import logging
+import subprocess
+import os.path
+from django.utils.translation import ugettext_lazy as _
+
+# See http://docs.python.org/2/library/subprocess.html#popen-constructor if you
+# have questions about this variable
+DEFAULT_OUTPUT_BUFFER_SIZE = 4096
+
+LOG = logging.getLogger(__name__)
 
 
 class BaseEngine(object):
@@ -46,4 +56,52 @@ class BaseEngine(object):
     def list_databases(self):
         """list databases in a instance"""
         raise NotImplementedError()
+
+    def call_script(self, script_name, args=[], envs={}):
+        working_dir = "./scripts"
+        working_dir = os.path.abspath(working_dir)
+
+        logging_cmdline = "%s %s %s" % (
+            " ".join([ "%s=%s" % (k, "xxx" if k.endswith("_PASSWORD") else v) for (k,v) in envs.items()]),
+            script_name,
+            " ".join(args),
+        )
+        return_code = None
+        try:
+            LOG.info('Running on path %s command: %s', working_dir, logging_cmdline)
+
+            # For future, if scripts have lot of output can be better
+            # create a temporary file for stdout. Scripts with lot of output and subprocess.PIPE
+            # can lock because this method not consume stdout without script finish execute.
+            process = subprocess.Popen(
+                [script_name] + args,
+                bufsize=DEFAULT_OUTPUT_BUFFER_SIZE,
+                stdin=None,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT, # stderr and stdout are the same
+                close_fds=True,
+                cwd=working_dir,
+                env=envs,
+                universal_newlines=True)
+            process.wait()
+
+            output = process.stdout.read()
+            return_code = process.returncode
+
+            if return_code != 0:
+                raise RuntimeError("Error executing %s, exit code = %d: '%s'" % (script_name, return_code, output))
+            return output
+        except:
+            # if any error happen, log cmdline to error
+            LOG.error("Error running cmdline (exit code %s): %s", return_code, logging_cmdline, exc_info=True)
+            raise
+
+    def to_envs(self, obj):
+        """ Create a dictionary with object variable, to be used as environment variables to script """
+        obj_name = obj._meta.object_name.upper()
+
+        envs = {}
+        for field in obj._meta.fields:
+            envs["%s_%s" % (obj_name, field.name.upper())] = field.value_to_string(obj)
+        return envs
 
--- dbaas/mongodb/engine.py
+++ dbaas/mongodb/engine.py
@@ -1,13 +1,5 @@
-from base.engine import BaseEngine
 import logging
-import subprocess
-import os.path
-
-# ./MongoManager.sh 
-
-# See http://docs.python.org/2/library/subprocess.html#popen-constructor if you
-# have questions about this variable
-DEFAULT_OUTPUT_BUFFER_SIZE = 4096
+from base.engine import BaseEngine
 
 LOG = logging.getLogger(__name__)
 
@@ -16,59 +8,29 @@ class MongoDB(BaseEngine):
     
     def url(self):
         return u"mongodb://%s:%s" % (self.instance.name, self.port)
-    
-    #TODO: Move call_script to BaseEngine or a more base class
-    def call_script(self, script_name, args=[], envs={}):
-        working_dir = "./scripts"
-        working_dir = os.path.abspath(working_dir)
 
-        logging_cmdline = "%s %s %s" % (
-            " ".join([ "%s=%s" % (k, "xxx" if k.endswith("_PASSWORD") else v) for (k,v) in envs.items()]),
-            script_name,
-            " ".join(args),
-        )
-        return_code = None
-        try:
-            LOG.info('Running on path %s command: %s', working_dir, logging_cmdline)
+    def status(self):
+        raise NotImplementedError()
 
-            # For future, if scripts have lot of output can be better
-            # create a temporary file for stdout. Scripts with lot of output and subprocess.PIPE
-            # can lock because this method not consume stdout without script finish execute.
-            process = subprocess.Popen(
-                [script_name] + args,
-                bufsize=DEFAULT_OUTPUT_BUFFER_SIZE,
-                stdin=None,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT, # stderr and stdout are the same
-                close_fds=True,
-                cwd=working_dir,
-                env=envs,
-                universal_newlines=True)
-            process.wait()
+    def create_user(self, credential):
+        self.run_mongo()
 
-            output = process.stdout.read()
-            return_code = process.returncode
+    def remove_user(self, credential):
+        raise NotImplementedError()
 
-            if return_code != 0:
-                raise RuntimeError("Error executing %s, exit code = %d: '%s'" % (script_name, return_code, output))
-            return output
-        except:
-            # if any error happen, log cmdline to error
-            LOG.error("Error running cmdline (exit code %s): %s", return_code, logging_cmdline, exc_info=True)
-            raise
+    def create_database(self, database):
+        raise NotImplementedError()
 
+    def remove_database(self, database):
+        raise NotImplementedError()
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    envs = {
-        'INSTANCE_NAME': 'localhost',
-        'INSTANCE_CONNECTION': 'localhost:27017',
-        'INSTANCE_USER': ' ',
-        'INSTANCE_PASSWORD': ' ',
-        'DATABASE_NAME': 'my_db',
-        'CREDENTIALS_USER': 'my_usr',
-        'CREDENTIALS_PASSWORD': 'blablalba',
-    }
-    output = MongoDB().call_script('./MongoManager.sh', args=['adduser'], envs=envs)
-    print "output=", repr(output)
+    def list_databases(self):
+        """list databases in a instance"""
+        raise NotImplementedError()
+
+    def run_mongo(self, operation, objects):
+        envs = {}
+        for obj in objects:
+            envs.update(self.to_envs(obj))
+        self.call_script("./MongoManager.sh", envs=envs)
 
