97ba57c85bbc51f8c8baed144901daf15d82b3b3
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-20 15:08:22 -0300

    adding more attributes

--- dbaas/base/engine/base.py
+++ dbaas/base/engine/base.py
@@ -1,19 +1,20 @@
 from django.conf import settings
 
+
 class BaseEngine(object):
     """
     BaseEngine interface
     """
-    
+
     @staticmethod
     def is_engine_available(name):
         return name in settings.INSTALLED_APPS
 
     @staticmethod
     def factory(name, node):
-        
+
         engine_name = name.lower()
-        
+
         # TODO: import Engines dynamically
         if engine_name == "mongodb":
             if BaseEngine.is_engine_available(engine_name):
@@ -24,11 +25,42 @@ class BaseEngine(object):
 
         assert 0, "Bad Engine Type: " + name
 
-    
+
     def __init__(self, *args, **kwargs):
-        
+
         if 'node' in kwargs:
             self.node = kwargs.get('node')
-        
+
     def url(self):
+        raise NotImplementedError()
+
+    def port(self):
+        raise NotImplementedError()
+
+    def address(self):
+        raise NotImplementedError()
+
+    def user(self):
+        raise NotImplementedError()
+
+    def password(self):
+        raise NotImplementedError()
+
+    def status(self, instance):
+        raise NotImplementedError()
+
+    def create_user(self, credential, database):
+        raise NotImplementedError()
+
+    def delete_user(self, credential):
+        raise NotImplementedError()
+
+    def create_database(self, database):
+        raise NotImplementedError()
+
+    def delete_database(self, database):
+        raise NotImplementedError()
+
+    def list_databases(self, instance):
+        """list databases in a instance"""
         raise NotImplementedError()
