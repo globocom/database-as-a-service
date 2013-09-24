d4027bd3b793d9793b8a0a1a03db7b6bc6d6107b
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-24 16:02:33 -0300

    list display and search fields

--- dbaas/base/admin/database.py
+++ dbaas/base/admin/database.py
@@ -6,3 +6,5 @@ from ..service.database import DatabaseService
 
 class DatabaseAdmin(admin.DjangoServicesAdmin):
     service_class = DatabaseService
+    search_fields = ("name", "instance__name")
+    list_display = ("name", "instance",)
