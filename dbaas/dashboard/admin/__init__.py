23562d64f180ea0a78b48dcac206c6448dae1a2b
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-26 17:24:42 -0300

    fixing test

--- dbaas/dashboard/admin/__init__.py
+++ dbaas/dashboard/admin/__init__.py
@@ -1,6 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 from django.contrib import admin
+from adminplus.sites import AdminSitePlus
 from ..views import dashboard
-# 
+
+#hack to pass test
+if not isinstance(admin.site, AdminSitePlus):
+    admin.site = AdminSitePlus()
+
 admin.site.register_view('/dashboard/', view=dashboard)
 
