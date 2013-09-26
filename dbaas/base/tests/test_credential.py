464e5d5e054f4b1f130f95f054eae2965b1450e3
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-26 11:04:56 -0300

    fixing text after the addition of on_delete

--- dbaas/base/tests/test_credential.py
+++ dbaas/base/tests/test_credential.py
@@ -11,8 +11,8 @@ from base.tests import factory
 from base.engine import base
 
 
-class FakeEngine(base.BaseEngine):
-    
+class FakeDriver(base.BaseDriver):
+
     def get_connection(self):
         return CONNECTION_TEST
 
@@ -20,56 +20,49 @@ class DatabaseTestCase(TestCase):
 
     def setUp(self):
         self.instance = factory.InstanceFactory()
-        self.engine = FakeEngine(instance=self.instance)
+        self.engine = FakeDriver(instance=self.instance)
         self.database = Database.objects.create(name="super", instance=self.instance)
 
     def tearDown(self):
+        self.engine = None
         self.database.delete()
-        self.instance.delete()
-        self.instance = self.engine = None
 
     def test_create_credential(self):
-        
+
         credential = Credential.objects.create(user="super", password="super", database=self.database)
-        
+
         self.assertTrue(credential.id)
-        credential.delete()
 
     def test_slugify_user_credential(self):
-        
+
         credential = Credential.objects.create(user="a b c", password="super", database=self.database)
-        
+
         self.assertTrue(credential.id)
         self.assertEqual(credential.user, 'a_b_c')
-        credential.delete()
 
 
     def test_underscore_in_slugged_user_credential(self):
-        
+
         credential = Credential.objects.create(user="a_b_c_d", password="super", database=self.database)
-        
+
         self.assertTrue(credential.id)
         self.assertEqual(credential.user, "a_b_c_d")
-        credential.delete()
 
     def test_cannot_edit_user_credential(self):
-        
+
         credential = Credential.objects.create(user="super2", password="super2", database=self.database)
-        
+
         self.assertTrue(credential.id)
-        
+
         credential.user = "super3"
-        
+
         self.assertRaises(AttributeError, credential.save)
-        credential.delete()
 
     def test_cannot_edit_database_credential(self):
-        
+
         credential = Credential.objects.create(user="super4", password="super4", database=self.database)
         database = Database.objects.create(name="oxente", instance=self.instance)
-        
+
         credential.database = database
-        
+
         self.assertRaises(AttributeError, credential.save)
-        database.delete()
-        credential.delete()
--- dbaas/base/tests/test_database.py
+++ dbaas/base/tests/test_database.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from django.test.client import Client
 from django.test import TestCase
+from django.test.client import RequestFactory
 from django.db import IntegrityError
 
 from ..models import Database
@@ -9,27 +11,25 @@ from base.tests import factory
 from base.engine import base
 
 
-class FakeEngine(base.BaseEngine):
+class FakeDriver(base.BaseDriver):
     
     def get_connection(self):
-        return "a"
+        return CONNECTION_TEST
 
 class DatabaseTestCase(TestCase):
 
     def setUp(self):
         self.instance = factory.InstanceFactory()
-        self.engine = FakeEngine(instance=self.instance)
+        self.engine = FakeDriver(instance=self.instance)
 
     def tearDown(self):
-        self.instance.delete()
-        self.instance = self.engine = None
+        self.engine = None
 
     def test_create_database(self):
         
         database = Database.objects.create(name="super", instance=self.instance)
-
+        
         self.assertTrue(database.id)
-        database.delete()
 
     def test_create_duplicate_database_error(self):
         
@@ -38,7 +38,6 @@ class DatabaseTestCase(TestCase):
         self.assertTrue(database.id)
         
         self.assertRaises(IntegrityError, Database.objects.create, name="super", instance=self.instance)
-        database.delete()
 
     def test_slugify_database_name(self):
         
@@ -46,13 +45,11 @@ class DatabaseTestCase(TestCase):
         
         self.assertTrue(database.id)
         self.assertEqual(database.name, 'w_h_a_t')
-        database.delete()
         
         database2 = Database.objects.create(name="w.h.e.r.e", instance=self.instance)
         
         self.assertTrue(database2.id)
         self.assertEqual(database2.name, 'w_h_e_r_e')
-        database2.delete()
 
     def test_cannot_edit_database_name(self):
         
@@ -63,7 +60,6 @@ class DatabaseTestCase(TestCase):
         database.name = "super3"
         
         self.assertRaises(AttributeError, database.save)
-        database.delete()
 
 
 
