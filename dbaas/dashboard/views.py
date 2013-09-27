9e7e9f8f7504292a137cddc73b95185a23f73c36
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-09-27 12:28:42 -0300

    very basic dashboard

--- dbaas/dashboard/views.py
+++ dbaas/dashboard/views.py
@@ -8,6 +8,8 @@ from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
+from django.utils.datastructures import SortedDict
+
 from base.service.instance import InstanceService
 
 LOG = logging.getLogger(__name__)
@@ -15,19 +17,19 @@ LOG = logging.getLogger(__name__)
 @login_required
 def dashboard(request, *args, **kwargs):
     instance_service = InstanceService(request)
-    instances = instance_service.list()
-    if not instances:
-        LOG.info("No instance found")
-
-    # for instance in instances:
-    #     instance_status = instance_service.get_instance_status(instance)
-    #     html += '<h2>%s</h2>' % instance.name
-    #     html += '<p>version = %s</p>' % instance_status.version
-    #     html += '<p>size (mb) = %s</p>' % instance_status.size_in_mbytes
-    # 
-    #     for database_status in instance_status.databases_status.values():
-    #         html += '<h3>%s</h3>' % database_status.name
-    #         html += '<p>size (mb) = %s</p>' % (database_status.size_in_mbytes)
+    instances = []
     
-    #return HttpResponse(html)
+    for instance in instance_service.list():
+        instance_status = instance_service.get_instance_status(instance)
+        data = SortedDict()
+        data["name"] = instance.name
+        data["version"] = instance_status.version
+        data["size"] = instance_status.size_in_mbytes
+        data["databases"] = []
+    
+        for database_status in instance_status.databases_status.values():
+            data["databases"].append({"name" : database_status.name, "size" : database_status.size_in_mbytes})
+        
+        instances.append(data)
+    
     return render_to_response("dashboard/dashboard.html", locals(), context_instance=RequestContext(request))
