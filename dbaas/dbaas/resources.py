620bb883c3a979e37af17c48cd6c6f5629933f59
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-10-16 11:49:16 -0300

    resources

--- dbaas/dbaas/resources.py
+++ dbaas/dbaas/resources.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.utils.translation import ugettext_lazy as _
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+class ResourcesViewSet(viewsets.ViewSet):
+    
+    pass
