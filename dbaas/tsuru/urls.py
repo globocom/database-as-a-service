fec45c5473a6dc654b73e9941f76e61675bba3f7
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-10-18 16:01:15 -0300

    refactoring urls

--- dbaas/tsuru/urls.py
+++ dbaas/tsuru/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('tsuru.views',
-    url(r'^(?P<engine_name>\w+)/(?P<engine_version>.+)/resources/(?P<service_name>.+)/status/?$', "status", name="tsuru.status"),
-    url(r'^(?P<engine_name>\w+)/(?P<engine_version>.+)/resources/(?P<service_name>.+)/hostname/(?P<host>.+)/?$', "service_unbind", name="tsuru.service_unbind"),
-    url(r'^(?P<engine_name>\w+)/(?P<engine_version>.+)/resources/(?P<service_name>.+)/?$', "service_bind", name="tsuru.service_bind"),
+    url(r'^(?P<engine_name>\w+)/(?P<engine_version>.+)/resources/(?P<service_name>.[^/]+)/hostname/(?P<host>.+)/?$', "service_unbind", name="tsuru.service_unbind"),
+    url(r'^(?P<engine_name>\w+)/(?P<engine_version>.+)/resources/(?P<service_name>.[^/]+)/status/?$', "service_status", name="tsuru.service_status"),
+    url(r'^(?P<engine_name>\w+)/(?P<engine_version>.+)/resources/(?P<service_name>.[^/]+)/?$', "service_bind_remove", name="tsuru.service_bind_remove"),
     url(r'^(?P<engine_name>\w+)/(?P<engine_version>.+)/resources/?$', "service_add", name="tsuru.service_add"),
 )
