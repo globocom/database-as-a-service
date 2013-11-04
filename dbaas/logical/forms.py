c5bedaf98125d94986018552cf713578267eb461
Leandro Souza <leandro.souza@corp.globo.com>
2013-11-04 16:47:57 -0200

    created CredentialForm

--- dbaas/logical/forms.py
+++ dbaas/logical/forms.py
@@ -4,8 +4,9 @@ from django.utils.translation import ugettext_lazy as _
 import logging
 from django.forms import models
 from django import forms
-from .models import Database
+from .models import Database, Credential
 from physical.models import Plan, DatabaseInfra
+from util import make_db_random_password
 
 LOG = logging.getLogger(__name__)
 
@@ -54,4 +55,20 @@ class DatabaseForm(models.ModelForm):
 
     def save_m2m(self, *args, **kwargs):
         pass
+
+
+class CredentialForm(models.ModelForm):
+    #password = forms.CharField()
+    
+    def __init__(self, *args, **kwargs):
+        super(CredentialForm, self).__init__(*args, **kwargs)
+        #instance = getattr(self, 'instance', None)
+        # if instance and instanceself.pk:
+        # self.fields['password'].widget.attrs['readonly'] = True
+        self.fields['password'].initial = make_db_random_password()
+        self.fields['password'].required = False
+        self.fields['password'].widget.attrs['readonly'] = True
+    
+    class Meta:
+        model = Credential
 
