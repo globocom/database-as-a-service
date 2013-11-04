8a9181db7d241883d2f30b6fd49ada66d6220d72
Leandro Souza <leandro.souza@corp.globo.com>
2013-11-04 16:48:51 -0200

    new method to generate password

--- dbaas/util/__init__.py
+++ dbaas/util/__init__.py
@@ -1,4 +1,10 @@
 from slugify import slugify as slugify_function
+from django.contrib.auth.models import User
+
 
 def slugify(string):
     return slugify_function(string, separator="_")
+
+
+def make_db_random_password():
+    return User.objects.make_random_password()
