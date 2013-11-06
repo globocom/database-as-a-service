4799b98638d28c152c014e08fd792508128b5c54
Silvano Buback <silvano@corp.globo.com>
2013-11-06 17:50:31 -0200

    refactor in credential view and implemented delete action

--- dbaas/logical/urls.py
+++ dbaas/logical/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url
+from .views import CredentialView
 
-urlpatterns = patterns('logical.views',
-    url(r"^credential/(?P<credential_id>\d+)/reset_password$", "reset_password", name="reset_password"),
-    url(r"^credential/$", "create_credential", name="create_credential"),
+
+urlpatterns = patterns('',
+    
+    url(r"^credential/(?P<pk>\d*)$", CredentialView.as_view(), name="credential-detail"),
 )
--- dbaas/logical/views.py
+++ dbaas/logical/views.py
@@ -1,39 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.shortcuts import get_object_or_404
+from django.views.generic.detail import BaseDetailView
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ValidationError
+from django.utils.decorators import method_decorator
 from .models import Credential, Database
 from util import as_json
 
 
-# FIXME FIX this to allow csrf with ajax
+class CredentialView(BaseDetailView):
+    model = Credential
 
-@csrf_exempt
-@as_json
-def reset_password(request, credential_id):
+    @method_decorator(csrf_exempt(as_json))
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get("username", None)
+        database_id = request.POST.get("database_id", None)
+        try:
+            database = get_object_or_404(Database, pk=database_id)
+            credential = Credential.create_new_credential(username, database)
+            return {"credential" : { "user": credential.user, "password": credential.password, "id": credential.pk } }
+        except ValidationError, e:
+            return { "error": ", ".join(e.messages) }
 
-    credential = get_object_or_404(Credential, pk=credential_id)
-    credential.reset_password()
-    return {'password': credential.password }
+    @method_decorator(csrf_exempt(as_json))
+    def put(self, request, *args, **kwargs):
+        credential = self.get_object()
+        credential.reset_password()
+        return {'password': credential.password }
 
-@csrf_exempt
-@as_json
-def delete_credential(request, credential_id):
-
-    credential = get_object_or_404(Credential, pk=credential_id)
-    credential.delete()
-    return {'password': credential.password }
-
-@csrf_exempt
-@as_json
-def create_credential(request):
-
-    username = request.POST.get("username", None)
-    database_id = request.POST.get("database_id", None)
-    try:
-        database = get_object_or_404(Database, pk=database_id)
-        credential = Credential.create_new_credential(username, database)
-        return {"credential" : { "user": credential.user, "password": credential.password, "id": credential.pk } }
-    except ValidationError, e:
-        return { "error": ", ".join(e.messages) }
+    @method_decorator(csrf_exempt(as_json))
+    def delete(self, request, *args, **kwargs):
+        credential = self.get_object()
+        credential.delete()
+        return {'password': credential.password }
