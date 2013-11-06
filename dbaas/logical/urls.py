b1f75eba55b1c03d43b67f2869968528f0ec8820
Silvano Buback <silvano@corp.globo.com>
2013-11-06 16:54:48 -0200

    added creating new credentials

--- dbaas/logical/urls.py
+++ dbaas/logical/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('logical.views',
-    url(r"credential/(?P<credential_id>\d+)/reset_password", "reset_password", name="reset_password"),
+    url(r"^credential/(?P<credential_id>\d+)/reset_password$", "reset_password", name="reset_password"),
+    url(r"^credential/$", "create_credential", name="create_credential"),
 )
--- dbaas/logical/views.py
+++ dbaas/logical/views.py
@@ -2,7 +2,8 @@
 from __future__ import absolute_import, unicode_literals
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from .models import Credential
+from django.core.exceptions import ValidationError
+from .models import Credential, Database
 from util import as_json
 
 
@@ -23,3 +24,16 @@ def delete_credential(request, credential_id):
     credential = get_object_or_404(Credential, pk=credential_id)
     credential.delete()
     return {'password': credential.password }
+
+@csrf_exempt
+@as_json
+def create_credential(request):
+
+    username = request.POST.get("username", None)
+    database_id = request.POST.get("database_id", None)
+    try:
+        database = get_object_or_404(Database, pk=database_id)
+        credential = Credential.create_new_credential(username, database)
+        return {"credential" : { "user": credential.user, "password": credential.password, "id": credential.pk } }
+    except ValidationError, e:
+        return { "error": ", ".join(e.messages) }
