2014f87e0c9b74f9bab16554554c4c7c22c0890e
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-11-07 17:39:37 -0200

    get roles and groups

--- dbaas/account/views.py
+++ dbaas/account/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.datastructures import SortedDict
-
+from account.models import get_user_groups, get_user_roles
 from logical.models import Database
 
 LOG = logging.getLogger(__name__)
@@ -20,10 +20,13 @@ LOG = logging.getLogger(__name__)
 def profile(request, user_id=None):
     user = None
     databases = None
+    groups = None
+    roles = None
     try :
         user = User.objects.get(id=user_id)
         databases = Database.objects.filter(group__in=[group.id for group in user.groups.all()])
-        
+        groups = get_user_groups(user=user)
+        roles = get_user_roles(user=user)
     except Exception, e:
         LOG.warning("Ops... %s" % e)
     
