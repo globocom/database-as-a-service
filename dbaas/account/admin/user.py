851ef354029211bdb028d702ab193be49bd1c01e
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-11-08 12:51:20 -0200

    fix permissions for proxied modules

--- dbaas/account/admin/user.py
+++ dbaas/account/admin/user.py
@@ -6,32 +6,14 @@ from django.contrib.auth.admin import UserAdmin
 
 LOG = logging.getLogger(__name__)
 
-#
+
 class UserAdmin(UserAdmin):
-    
+
     fieldsets_basic = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    
-    def has_add_permission(self, request):
-        if not request.user.is_active:
-            return False
-        else:
-            return request.user.has_perm("auth.add_user", obj=None)
-
-    def has_change_permission(self, request, obj=None):
-        if not request.user.is_active:
-            return False
-        else:
-            return request.user.has_perm("auth.change_user", obj=None)
-
-    def has_delete_permission(self, request, obj=None):
-        if not request.user.is_active:
-            return False
-        else:
-            return request.user.has_perm("auth.delete_user", obj=None)
 
     def get_fieldsets(self, request, obj=None):
         if not obj:
