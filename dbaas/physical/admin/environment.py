31f5a070c8ded61d8f57d03422414dc08b46e54e
Leandro Souza <leandro.souza@corp.globo.com>
2013-11-11 17:45:00 -0200

    changed to  ModelAdmin

--- dbaas/physical/admin/environment.py
+++ dbaas/physical/admin/environment.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from django_services import admin
-from ..service.environment import EnvironmentService
+from django.contrib import admin
 
 
-class EnvironmentAdmin(admin.DjangoServicesAdmin):
-    service_class = EnvironmentService
+class EnvironmentAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     list_display = ("name",)
     save_on_top = True
