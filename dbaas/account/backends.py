6c88de2f962a8ae932d81ce3fa67618208b70018
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-11-11 17:48:14 -0200

    overriding get all permissions

--- dbaas/account/backends.py
+++ dbaas/account/backends.py
@@ -6,7 +6,15 @@ import logging
 LOG = logging.getLogger(__name__)
 
 class DbaasBackend(ModelBackend):
-    
+
+    def get_all_permissions(self, user_obj, obj=None):
+        if user_obj.is_anonymous() or obj is not None:
+            return set()
+        if not hasattr(user_obj, '_perm_cache'):
+            user_obj._perm_cache = set(["%s.%s" % (p.content_type.app_label, p.codename) for p in user_obj.user_permissions.select_related()])
+            user_obj._perm_cache.update(self.get_group_permissions(user_obj))
+        return user_obj._perm_cache
+
     def has_perm(self, user_obj, perm, obj=None):
         #LOG.debug("validating perm %s for user %s on object %s" % (perm, user_obj, obj))
         # We are using django_services in admin, but it does not provide any support to 
