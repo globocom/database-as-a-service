956f617911ce715dd732288c7cbbe942907e1152
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-11-13 11:53:34 -0200

    refactory team form to show only users not in teams

--- dbaas/account/forms/team.py
+++ dbaas/account/forms/team.py
@@ -12,7 +12,7 @@ from ..models import Team
 #as in http://stackoverflow.com/questions/6097210/assign-user-objects-to-a-group-while-editing-group-object-in-django-admin
 class TeamAdminForm(forms.ModelForm):
     users = forms.ModelMultipleChoiceField(
-        queryset=User.objects.all(), 
+        queryset=Team.user_objects.all(), 
         required=False,
         widget=FilteredSelectMultiple(
             verbose_name=_('Users'),
@@ -26,18 +26,18 @@ class TeamAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(TeamAdminForm, self).__init__(*args, **kwargs)
 
-        if self.instance and self.instance.pk:
-            self.fields['users'].initial = self.instance.user_set.all()
-
-    def save(self, commit=True):
-        team = super(TeamAdminForm, self).save(commit=commit)
-
-        if commit:
-            team.user_set = self.cleaned_data['users']
-        else:
-            old_save_m2m = self.save_m2m
-            def new_save_m2m():
-                old_save_m2m()
-                team.user_set = self.cleaned_data['users']
-            self.save_m2m = new_save_m2m
-        return team
+    #     if self.instance and self.instance.pk:
+    #         self.fields['users'].initial = self.instance.user_set.all()
+    # 
+    # def save(self, commit=True):
+    #     team = super(TeamAdminForm, self).save(commit=commit)
+    # 
+    #     if commit:
+    #         team.user_set = self.cleaned_data['users']
+    #     else:
+    #         old_save_m2m = self.save_m2m
+    #         def new_save_m2m():
+    #             old_save_m2m()
+    #             team.user_set = self.cleaned_data['users']
+    #         self.save_m2m = new_save_m2m
+    #     return team
