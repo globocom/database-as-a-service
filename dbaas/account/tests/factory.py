6234c577348f6977043379c13d77c672a6acc250
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-11-13 16:43:24 -0200

    role factory

--- dbaas/account/tests/factory.py
+++ dbaas/account/tests/factory.py
@@ -10,6 +10,12 @@ class UserFactory(factory.DjangoModelFactory):
     username = factory.Sequence(lambda n: 'user_{0}'.format(n))
     email = factory.Sequence(lambda n: 'user_{0}@email.test.com'.format(n))
 
+class RoleFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = models.Role
+
+    name = factory.Sequence(lambda n: 'role_{0}'.format(n))
+
+
 
 
 
