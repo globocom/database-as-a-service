01882d24283e6e9817d90904fddc46c4203944d9
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-11-21 16:19:04 -0200

    team in user listing

--- dbaas/account/admin/user.py
+++ dbaas/account/admin/user.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 import logging
+from django.utils.html import format_html, escape
 from django.contrib.auth.admin import UserAdmin
 
 from account.models import Team
@@ -22,6 +23,11 @@ class UserAdmin(UserAdmin):
     
     inlines = (TeamInline, )
     
+    list_display = ('username', 'email', 'first_name', 'last_name', 'get_team_for_user')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+    
     fieldsets_basic = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'is_active')}),
@@ -33,6 +39,21 @@ class UserAdmin(UserAdmin):
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'is_staff', 'is_active', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
+    
+    def get_team_for_user(self, user):
+        teams = user.team_set.all()
+        team_html = []
+        if teams:
+            team_html.append("<ul>")
+            for team in teams:
+                team_html.append("<li>%s</li>" % team.name)
+            team_html.append("</ul>")
+            return format_html("".join(team_html))
+        else:
+            return "N/A"
+
+    
+    get_team_for_user.short_description = "Team(s)"
 
     def get_fieldsets(self, request, obj=None):
         if not obj:
