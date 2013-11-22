22581bccb9f12809cc9f0526a7ba2eedecb248a8
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-11-22 11:26:10 -0200

    typo

--- dbaas/logical/admin/database.py
+++ dbaas/logical/admin/database.py
@@ -25,7 +25,7 @@ class DatabaseAdmin(admin.DjangoServicesAdmin):
     list_display_basic = ["name_html", "engine_type", "environment", "get_capacity_html",]
     list_display_advanced = list_display_basic + ["quarantine_dt_format"]
     list_filter_basic = ["databaseinfra", "project", "databaseinfra__environment"]
-    list_filter_advanced = list_filter_basic + ["is_in_quarantine"] + ["team"]
+    list_filter_advanced = list_filter_basic + ["is_in_quarantine", "team"]
     add_form_template = "logical/database_add_form.html"
     change_form_template = "logical/database_change_form.html"
     delete_button_name = "Delete"
