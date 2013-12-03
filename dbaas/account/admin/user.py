4ce45892d74b3ad4e74c198293f6ad7e0806afa1
Leandro Souza <leandro.souza@corp.globo.com>
2013-12-03 16:22:18 -0200

    removed the option TeamInline from users

--- dbaas/account/admin/user.py
+++ dbaas/account/admin/user.py
@@ -1,46 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.utils.translation import ugettext_lazy as _
-from django.contrib import admin
 import logging
-from django.utils.html import format_html, escape
+from django.utils.html import format_html
 from django.contrib.auth.admin import UserAdmin
 
-from account.models import Team
 from util import notifications
 
 LOG = logging.getLogger(__name__)
 
-class TeamInline(admin.StackedInline):
-    model = Team.users.through
-    can_delete = False
-    extra = 1
-    max_num = 1
-    verbose_name = _("Team")
-    verbose_name_plural = _("Teams")
-
 
 class UserAdmin(UserAdmin):
-    
-    inlines = (TeamInline, )
-    
+
     list_display = ('username', 'email', 'get_team_for_user')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
-    
+
     fieldsets_basic = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'is_active', 'is_staff')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    
+
     fieldsets_advanced = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    
+
     def get_team_for_user(self, user):
         teams = user.team_set.all()
         team_html = []
@@ -53,7 +41,6 @@ class UserAdmin(UserAdmin):
         else:
             return "N/A"
 
-    
     get_team_for_user.short_description = "Team(s)"
 
     def get_fieldsets(self, request, obj=None):
@@ -70,7 +57,8 @@ class UserAdmin(UserAdmin):
         """
         if user is not superuser, than is_staff field should be readonly
         """
-        if obj: #In edit mode
+
+        if obj:  # In edit mode
             if request.user.is_superuser:
                 return ()
             else:
@@ -88,8 +76,6 @@ class UserAdmin(UserAdmin):
         super(UserAdmin, self).save_related(request, form, formsets, change)
         teams_after_save = [team.id for team in instance.team_set.all()]
         LOG.debug("after save for user %s | teams: %s" % (instance, teams_after_save))
-        
+
         if cmp(teams_before_save, teams_after_save):
             notifications.notify_team_change_for(user=instance)
-    
-
