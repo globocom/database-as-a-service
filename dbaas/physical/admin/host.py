df17fa3173807094744e97a003e27c6c4eedbd26
Leandro Souza <leandro.souza@corp.globo.com>
2013-12-05 16:31:30 -0200

    enable link in list of hosts

--- dbaas/physical/admin/host.py
+++ dbaas/physical/admin/host.py
@@ -7,5 +7,11 @@ from ..service.host import HostService
 class HostAdmin(admin.DjangoServicesAdmin):
     service_class = HostService
     search_fields = ("hostname",)
-    list_display = ("hostname", "monitor_url",)
+    list_display = ("hostname", "monitor_url_html",)
     save_on_top = True
+    
+    def monitor_url_html(self, host):
+        return "<a href='%(u)s' target='_blank'>%(u)s</a>" % {'u': host.monitor_url }
+    monitor_url_html.allow_tags = True
+    monitor_url_html.short_description = "Monitor url"
+    monitor_url_html.admin_order_field = "monitor_url"
