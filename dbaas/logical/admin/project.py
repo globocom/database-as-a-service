18e7b28a575f3f33ad9873de221ce3a47d616568
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-12-09 18:18:11 -0200

    override form

--- dbaas/logical/admin/project.py
+++ dbaas/logical/admin/project.py
@@ -2,9 +2,16 @@
 from __future__ import absolute_import, unicode_literals
 from django_services import admin
 from ..service.project import ProjectService
-
+from ..forms import ProjectForm
 
 class ProjectAdmin(admin.DjangoServicesAdmin):
+    form = ProjectForm
     service_class = ProjectService
     prepopulated_fields = {"slug": ("name",)}
-    list_display = ("name", "is_active", "slug")
+    list_display = ("name", "is_active", )
+
+    # fieldsets = (
+    #     (None, {
+    #         'fields': ('name', 'is_active', 'slug')
+    #     }),
+    # )
