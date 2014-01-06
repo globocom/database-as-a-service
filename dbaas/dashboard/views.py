7a58e18c870363cb2fe9e99d1b91c304f8aa4dc0
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-01-06 14:55:44 -0200

    typo

--- dbaas/dashboard/views.py
+++ dbaas/dashboard/views.py
@@ -18,26 +18,28 @@ LOG = logging.getLogger(__name__)
 def dashboard(request, *args, **kwargs):
     databaseinfra_service = DatabaseInfraService(request)
     databaseinfras = []
-    
+
     for databaseinfra in databaseinfra_service.list():
+
         try:
             databaseinfra_status = databaseinfra_service.get_databaseinfra_status(databaseinfra)
         except AttributeError, e:
             response = HttpResponse(content='The follow driver error was find: ' + e.message, content_type='text/plain', status=500)
             return response
+
         data = SortedDict()
         data["name"] = databaseinfra.name
         data["engine"] = databaseinfra.engine.engine_type
         data["version"] = databaseinfra_status.version
         data["size"] = databaseinfra_status.used_size_in_bytes / 1024.0 / 1024
         data["databases"] = []
-    
+
         for database_status in databaseinfra_status.databases_status.values():
             data["databases"].append({
                 "name" : database_status.name,
                 "size" : database_status.used_size_in_bytes / 1024.0 / 1024,
                 "usage": round(100 * database_status.used_size_in_bytes / databaseinfra_status.used_size_in_bytes)
             })
-        
+
         databaseinfras.append(data)
     return render_to_response("dashboard/dashboard.html", locals(), context_instance=RequestContext(request))
