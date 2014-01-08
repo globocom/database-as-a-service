faced033d159458bb16a557db0672a4246f00c82
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-01-08 12:05:42 -0200

    move instance formset class

--- dbaas/physical/admin/databaseinfra.py
+++ dbaas/physical/admin/databaseinfra.py
@@ -1,36 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.contrib import admin as django_admin
-from django.forms.models import BaseInlineFormSet
 from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
 from django_services import admin
 from ..service.databaseinfra import DatabaseInfraService
 from ..models import Instance
-from ..forms import DatabaseInfraForm
+from ..forms import DatabaseInfraForm, InstanceModelFormSet
+
 from util.html import render_progress_bar
-
-
-class InstanceModelFormSet(BaseInlineFormSet):
-
-    def clean(self):
-        super(InstanceModelFormSet, self).clean()
-
-        for error in self.errors:
-            if error:
-                return
-
-        completed = 0
-        for cleaned_data in self.cleaned_data:
-            # form has data and we aren't deleting it.
-            if cleaned_data and not cleaned_data.get('DELETE', False):
-                completed += 1
-
-        # example custom validation across forms in the formset:
-        if completed  == 0:
-            raise ValidationError({'instances': _("Specify at least one valid instance")})
-        # elif completed > 1:
-            # raise ValidationError({'instances': _("Currently, you can have only one instance per databaseinfra")})
 
 
 class InstanceAdmin(django_admin.TabularInline):
