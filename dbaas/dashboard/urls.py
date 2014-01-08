3c614f9d341989a6711364ee8d3929cdf5ca8a19
Leandro Souza <leandro.souza@corp.globo.com>
2014-01-08 18:25:09 -0200

    url to list databases

--- dbaas/dashboard/urls.py
+++ dbaas/dashboard/urls.py
@@ -3,4 +3,6 @@ from django.conf import settings
 
 urlpatterns = patterns('dashboard.views',
     url(r"^$", "dashboard", name="dashboard.index"),
+    url(r'^databaseinfra/(?P<infra_id>\d+)/?$', 'databaseinfra', name='databaseinfra.index'),
+    
 )
