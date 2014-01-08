7e7466abf7fae4774bb4a1fbc9b2928a6ffbc85a
Leandro Souza <leandro.souza@corp.globo.com>
2014-01-08 16:23:03 -0200

    it was necessary the engine_type

--- dbaas/dashboard/views.py
+++ dbaas/dashboard/views.py
@@ -11,8 +11,9 @@ LOG = logging.getLogger(__name__)
 @login_required
 def dashboard(request):
     env_id = request.GET.get('env_id')
-    if env_id:
-        dbinfra = DatabaseInfra.objects.filter(environment__id=env_id)
+    engine_type = request.GET.get('engine_type')
+    if env_id and engine_type:
+        dbinfra = DatabaseInfra.objects.filter(environment__id=env_id, engine__engine_type__name=engine_type)
     else:
         dbinfra = DatabaseInfra.objects.all()
     return render_to_response("dashboard/dashboard.html", {'dbinfra': dbinfra}, context_instance=RequestContext(request))
