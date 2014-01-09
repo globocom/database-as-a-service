4fe6faeb673879b7bf4e2cf407bfc838f05626ce
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-01-09 16:34:12 -0200

    fix #79

--- dbaas/physical/static/js/databaseinfra_admin.js
+++ dbaas/physical/static/js/databaseinfra_admin.js
@@ -41,8 +41,11 @@
             this.update_components();
         },
         update_components: function() {
-            var el = $("#id_engine");
-            var engine_id = el.val() || "none";
+            this.update_endpoint();
+            this.update_plans();
+        },
+        update_endpoint: function() {
+            var engine_id = $("#id_engine").val() || "none";
             var engine_name = $('#id_engine :selected').text() || "none";
 
             if (engine_name.match(/mongo/g)) {
@@ -54,6 +57,45 @@
                     this.show_endpoint();
                 };
             };
+        },
+        clean_plan_options: function() {
+            $('#id_plan option:gt(0)').remove();
+        },
+        update_plans: function() {            
+            var first_plan_id = $("#id_plan").val() || "0";
+
+            //remove options from plan, except the first one
+            this.clean_plan_options();
+
+            var engine_id = $("#id_engine").val() || "none";
+            if (engine_id != "none") {
+                
+                $.ajax({
+                    "dataType": "json",
+                    "url": "/api/plan/",
+                    "type": "GET",
+                    "data": { "engine_id": engine_id, },
+                }).done(function(data) {
+                    if (data.error) {
+                        alert(data.error);
+                    } else {
+                        var plan = $("#id_plan");
+                        $.each(data, function(index, item) {
+                            plan.append($("<option></option>").attr("value", item.id).text(item.name));
+                            /* 
+                            discover if it is the selected value. This is necessary in cases where the form is submited
+                            but is returned with a validation error.
+                            */
+                            if (parseInt(item.id) == parseInt(first_plan_id)) {
+                                plan.val(first_plan_id);
+                            };
+                        });
+                    };
+                }).fail(function() {
+                    alert("invalid server response");
+                });
+            };
+            
         },
         hide_endpoint: function() {
             var engine_id = $("#id_engine").val() || "none";
@@ -81,6 +123,9 @@
         
         //hide endpoint?
         databaseinfra.hide_endpoint();
+        
+        //update plans
+        databaseinfra.update_plans();
         
         // 
         $("#id_engine").on("change", function() {
