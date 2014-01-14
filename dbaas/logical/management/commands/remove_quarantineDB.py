24a7a6685cde98652e576e015b5a7f635677d6ed
Leandro Souza <leandro.souza@corp.globo.com>
2014-01-14 18:43:50 -0200

    moved to database models

--- dbaas/logical/management/commands/remove_quarantineDB.py
+++ dbaas/logical/management/commands/remove_quarantineDB.py
@@ -1,9 +1,7 @@
 from django.core.management.base import BaseCommand
-from datetime import date, timedelta
 import logging
 
 from logical.models import Database
-from system.models import Configuration
 
 
 LOG = logging.getLogger(__name__)
@@ -15,9 +13,4 @@ class Command(BaseCommand):
     '''
 
     def handle(self, *args, **options):
-        quarantine_time = Configuration.get_by_name_as_int('quarantine_retention_days')
-        quarantine_time_dt = date.today() - timedelta(days=quarantine_time)
-        databases = Database.objects.filter(is_in_quarantine=True, quarantine_dt__lte=quarantine_time_dt)
-        for database in databases:
-            database.delete()
-            LOG.info("The database %s was deleted, because it was set to quarentine %d days ago" % (database.name, quarantine_time))
+        Database.purge_quarantine()
