144f1cfdbc8f300825b3c7507ca87ceba3e2490f
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-01-17 15:31:39 -0200

    enhancing engine api responde

--- dbaas/api/engine.py
+++ dbaas/api/engine.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from rest_framework import viewsets, serializers
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from physical import models
-
+from .engine_type import EngineTypeSerializer
 
 class EngineSerializer(serializers.HyperlinkedModelSerializer):
+
+    engine_type = EngineTypeSerializer(read_only=True)
 
     class Meta:
         model = models.Engine
