47a9f0456a8f083c9284ad60f71ea8fda49f92c8
Leandro Souza <leandro.souza@corp.globo.com>
2014-01-22 19:35:59 -0200

    improved search by team and projec

--- dbaas/dashboard/search_indexes.py
+++ dbaas/dashboard/search_indexes.py
@@ -7,31 +7,11 @@ from logical.models import Project
 class DatabaseIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.NgramField(document=True, use_template=True)
     name =  indexes.CharField(model_attr='name')
+    team = indexes.CharField(model_attr='team')
+    project = indexes.CharField(model_attr='project', null=True)
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
 
     def get_model(self):
         return Database
-
-
-class TeamIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.NgramField(document=True, use_template=True)
-    name =  indexes.CharField(model_attr='name')
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
-
-    def get_model(self):
-        return Team
-
-
-class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.NgramField(document=True, use_template=True)
-    name =  indexes.CharField(model_attr='name')
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
-
-    def get_model(self):
-        return Project
