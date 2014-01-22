f2503a3e529c09fb2c5f486a9ab42cd91e9bdc26
Leandro Souza <leandro.souza@corp.globo.com>
2014-01-22 12:03:36 -0200

    implemented search for team and project

--- dbaas/dashboard/search_indexes.py
+++ dbaas/dashboard/search_indexes.py
@@ -1,5 +1,7 @@
 from haystack import indexes
 from logical.models import Database
+from account.models import Team
+from logical.models import Project
 
 
 class DatabaseIndex(indexes.SearchIndex, indexes.Indexable):
@@ -11,3 +13,25 @@ class DatabaseIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Database
+
+
+class TeamIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.NgramField(document=True, use_template=True)
+    name =  indexes.CharField(model_attr='name')
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+    def get_model(self):
+        return Team
+
+
+class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.NgramField(document=True, use_template=True)
+    name =  indexes.CharField(model_attr='name')
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+    def get_model(self):
+        return Project
