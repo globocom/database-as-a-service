8a29550c8b957f18ad188c673aca07fac18c86d9
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-01-28 16:24:49 -0200

    celery

--- dbaas/notification/tasks.py
+++ dbaas/notification/tasks.py
@@ -4,12 +4,12 @@ from __future__ import absolute_import
 import os
 import logging
 
-from celery import shared_task
+from dbaas.celery import app
 
 LOG = logging.getLogger(__name__)
 
-@shared_task
-def clone_database(database, credential):
+@app.task
+def clone_database(self, database, credential):
 
     task_name = "clone_database"
     LOG.debug("task name: %s" % clone_database.name)
