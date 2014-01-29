46eaba91ef67dbf2188079d418f8e42a53db150c
Leandro Souza <leandro.souza@corp.globo.com>
2014-01-29 17:59:30 -0200

    running the command

--- dbaas/notification/tasks.py
+++ dbaas/notification/tasks.py
@@ -40,31 +40,31 @@ def clone_database(self, origin_database, dest_database, user=None):
 
     
     #origin
-    # origin_instance=origin_database.databaseinfra.instances.all()[0]
-    # 
-    # db_orig=origin_database.name
-    # user_orig=origin_database.databaseinfra.user
-    # pass_orig=origin_database.databaseinfra.password
-    # host_orig=origin_instance.address
-    # port_orig=origin_instance.port
-    # 
-    # #destination
-    # dest_instance=dest_database.databaseinfra.instances.all()[0]
-    # 
-    # db_dest=dest_database.name
-    # user_dest=dest_database.databaseinfra.user
-    # pass_dest=dest_database.databaseinfra.password
-    # host_dest=dest_instance.address
-    # port_dest=dest_instance.port
-    # 
-    # path_of_dump=Configuration.get_by_name('database_clone_dir')
-    # engine=origin_database.databaseinfra.engine.name
-    # 
-    # args=[db_orig, user_orig, pass_orig, host_orig, port_orig, 
-    #         db_dest, user_dest, pass_dest, host_dest, port_dest, 
-    #         path_of_dump, engine
-    # ]
-    #call_script(CLONE_DATABASE_SCRIPT_NAME, working_dir=settings.SCRIPTS_PATH, args=args)
+    origin_instance=origin_database.databaseinfra.instances.all()[0]
+    
+    db_orig=origin_database.name
+    user_orig=origin_database.databaseinfra.user
+    pass_orig=origin_database.databaseinfra.password
+    host_orig=origin_instance.address
+    port_orig=origin_instance.port
+    
+    #destination
+    dest_instance=dest_database.databaseinfra.instances.all()[0]
+    
+    db_dest=dest_database.name
+    user_dest=dest_database.databaseinfra.user
+    pass_dest=dest_database.databaseinfra.password
+    host_dest=dest_instance.address
+    port_dest=dest_instance.port
+    
+    path_of_dump=Configuration.get_by_name('database_clone_dir')
+    engine=origin_database.databaseinfra.engine.name
+    
+    args=[db_orig, user_orig, pass_orig, host_orig, str(int(port_orig)), 
+            db_dest, user_dest, pass_dest, host_dest, str(int(port_dest)), 
+            path_of_dump, engine
+    ]
+    call_script(CLONE_DATABASE_SCRIPT_NAME, working_dir=settings.SCRIPTS_PATH, args=args)
     
     task_history.update_status_for(TaskHistory.STATUS_FINISHED)
     return
