39b8835f97fa427c93a81e8fe6020c0de24257b2
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-01-30 13:48:18 -0200

    drivers/scripts/dummy_clone.sh
    fix dummy

--- dbaas/drivers/scripts/dummy_clone.sh
+++ dbaas/drivers/scripts/dummy_clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ $# -ne 12 ]] && echo "usage: db_orig user_orig pass_orig host_orig port_orig db_dest user_dest pass_dest host_dest port_dest path_of_dump engine" && exit 1
+[[ $# -ne 11 ]] && echo "usage: db_orig user_orig pass_orig host_orig port_orig db_dest user_dest pass_dest host_dest port_dest path_of_dump" && exit 1
 
 
 db2clone=${1}
@@ -16,12 +16,11 @@ host_dest=${9}
 port_dest=${10}
 
 path_of_dump=${11}
-engine=${12}
 
 
-echo -e "\n#### db to be cloned #### \n name: $db2clone \n user: $user2clone \n password: $pass2clone \n host: $host2clone \n\n\n##### db dest ##### \n name: $db_dest \n user: $user_dest \n password: $pass_dest \n host: $host_dest \n\n path of dump: $path_of_dump \n engine: $engine\n\n\n generated at: `date`\n" > "$path_of_dump/$$.txt"
+echo -e "\n#### db to be cloned #### \n name: $db2clone \n user: $user2clone \n password: $pass2clone \n host: $host2clone \n\n\n##### db dest ##### \n name: $db_dest \n user: $user_dest \n password: $pass_dest \n host: $host_dest \n\n path of dump: $path_of_dump \n\n\n generated at: `date`\n" > "$path_of_dump/$$.txt"
 
-for a in {1..10}
+for a in {1..300}
 do
     dd if=/dev/zero of="$path_of_dump/$$.dump" bs=1k count=100000
     sleep 1
