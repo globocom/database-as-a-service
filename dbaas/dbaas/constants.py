0105be44d0bead3064f2fc1761215aac126ae738
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-02-04 18:18:07 -0200

    constants file

---
+++ dbaas/dbaas/constants.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+
+PERM_MANAGE_QUARANTINE_DATABASE="logical.can_manage_quarantine_databases"
+PERM_ADD_DATABASE_INFRA="physical.add_databaseinfra"
