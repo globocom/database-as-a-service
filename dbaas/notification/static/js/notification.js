270c5689b076e4076e363f7e6882a3dd43ff67ef
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-02-07 18:13:39 -0200

    first commit task history countdown

--- dbaas/notification/static/js/notification.js
+++ dbaas/notification/static/js/notification.js
@@ -6,7 +6,18 @@
 
     Notification.prototype = {
         setupRefresh: function() {
-            setInterval("location.reload()", 10000);
+            var REFRESH_TIMEOUT=10000;
+            // setInterval("location.reload()", 10000);
+            $('.countdown.callback').countdown({
+              date: + (new Date) + REFRESH_TIMEOUT,
+              render: function(data) {
+                $(this.el).text(this.leadingZeros(data.sec, 2) + " sec to refresh");
+              },
+              onEnd: function() {
+                $(this.el).addClass('ended');
+                location.reload();
+              }
+            });
         },
     };
 
