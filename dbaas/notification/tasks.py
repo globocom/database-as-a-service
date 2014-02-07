cfe56eff30abd3197e9e28d382b16f07303133f3
Leandro Souza <leandro.souza@corp.globo.com>
2014-02-07 19:18:13 -0200

    removed redis lock

--- dbaas/notification/tasks.py
+++ dbaas/notification/tasks.py
@@ -69,26 +69,12 @@ def clone_database(self, origin_database, dest_database, user=None):
 def databaseinfra_notification():
     from physical.models import DatabaseInfra
     from django.db.models import Sum, Count
-    import redis
-    # lock the task when someone worker is runing
-    REDIS_CLIENT = redis.Redis()
-    have_lock = False
-    lock = REDIS_CLIENT.lock("notification-lock", timeout=10)
-    
-    try:
-        have_lock = lock.acquire(blocking=False)
-        if have_lock:
-            infras = DatabaseInfra.objects.values('plan__name', 'environment__name', 'engine__engine_type__name').annotate(capacity=Sum('capacity'))
-            for infra in infras:
-                used = DatabaseInfra.objects.filter(plan__name=infra['plan__name'], environment__name=infra['environment__name'], engine__engine_type__name=infra['engine__engine_type__name']).aggregate(used=Count('databases'))
-                percent = int(used['used'] * 100 / infra['capacity'])
-                if percent >= Configuration.get_by_name_as_int("threshold_infra_notification", default=50):
-                    LOG.info('Plan %s in environment %s with %s%% occupied' % (infra['plan__name'], infra['environment__name'],percent))
-                    LOG.info("Sending notification...")
-                    notifications.databaseinfra_ending(infra['plan__name'], infra['environment__name'], used['used'],infra['capacity'],percent)
-        else:
-            LOG.info("lock file...")
-    finally:
-        if have_lock:
-            lock.release()
-        return
+    infras = DatabaseInfra.objects.values('plan__name', 'environment__name', 'engine__engine_type__name').annotate(capacity=Sum('capacity'))
+    for infra in infras:
+        used = DatabaseInfra.objects.filter(plan__name=infra['plan__name'], environment__name=infra['environment__name'], engine__engine_type__name=infra['engine__engine_type__name']).aggregate(used=Count('databases'))
+        percent = int(used['used'] * 100 / infra['capacity'])
+        if percent >= Configuration.get_by_name_as_int("threshold_infra_notification", default=50):
+            LOG.info('Plan %s in environment %s with %s%% occupied' % (infra['plan__name'], infra['environment__name'],percent))
+            LOG.info("Sending notification...")
+            notifications.databaseinfra_ending(infra['plan__name'], infra['environment__name'], used['used'],infra['capacity'],percent)
+    return
