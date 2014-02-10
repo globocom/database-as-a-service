2056ea0bf0fa7ebd728a570e8457532d8d92d7a9
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-02-10 11:43:41 -0200

    setting up syslog logging in celery

--- dbaas/dbaas/celery.py
+++ dbaas/dbaas/celery.py
@@ -9,6 +9,27 @@ from celery import Celery
 from django.conf import settings
 from dbaas import celeryconfig
 
+from logging.handlers import SysLogHandler
+from celery.log import redirect_stdouts_to_logger
+
+from celery.signals import after_setup_task_logger, after_setup_logger
+
+def setup_log(**args): 
+    # redirect stdout and stderr to logger
+    redirect_stdouts_to_logger(args['logger'])
+    # logs to local syslog
+    syslog = SysLogHandler(address=settings.SYSLOG_FILE, facility=logging.handlers.SysLogHandler.LOG_LOCAL3)
+    # setting log level
+    syslog.setLevel(args['loglevel'])
+    # setting log format 
+    formatter = logging.Formatter('dbaas: %(name)s %(message)s')
+    syslog.setFormatter(formatter)
+    # add new handler to logger
+    args['logger'].addHandler(syslog)
+
+after_setup_logger.connect(setup_log)
+after_setup_task_logger.connect(setup_log)
+
 LOG = logging.getLogger(__name__)
 
 #set this variable to True to run celery tasks synchronously
