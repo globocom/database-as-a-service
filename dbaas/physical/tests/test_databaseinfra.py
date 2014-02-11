d964b17c598674bc4edebc0f9b3a802761ef3109
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-02-11 14:28:41 -0200

    fix database infra test

--- dbaas/physical/tests/test_databaseinfra.py
+++ dbaas/physical/tests/test_databaseinfra.py
@@ -7,6 +7,9 @@ from ..models import DatabaseInfra
 from . import factory
 from drivers.fake import FakeDriver
 from django.core.cache import cache
+import logging
+
+LOG = logging.getLogger(__name__)
 
 
 class DatabaseInfraTestCase(TestCase):
@@ -14,33 +17,26 @@ class DatabaseInfraTestCase(TestCase):
     def setUp(self):
         # to avoid caching, clear it before tests
         cache.clear()
-        pass
-        # self.client = Client()
-        # self.factory = RequestFactory()
-        # self.databaseinfra = DatabaseInfraFactory()
-        # self.hostname = HostFactory()
-        # self.new_instance = InstanceFactory(address="new_instance.localinstance",
-        #                             port=123,
-        #                             is_active=True,
-        #                             is_arbiter = False,
-        #                             databaseinfra=self.databaseinfra)
 
     def test_best_for_without_plan_and_environment_options_returns_None(self):
         plan = factory.PlanFactory()
         environment = plan.environments.all()[0]
         self.assertIsNone(DatabaseInfra.best_for(plan=plan, environment=environment))
 
-    def test_best_for_with_only_one_datainfra_per_plan_and_environment_returns_it(self):
+    def test_best_for_with_only_one_datainfra_per_plan_and_environment(self):
         plan = factory.PlanFactory()
         environment = plan.environments.all()[0]
         datainfra = factory.DatabaseInfraFactory(plan=plan, environment=environment)
+        instance = factory.InstanceFactory(address="127.0.0.1", port=27017, databaseinfra=datainfra)
         self.assertEqual(datainfra, DatabaseInfra.best_for(plan=plan, environment=environment))
 
     def test_best_for_with_only_two_datainfra_per_plan_and_environment_returns_rounding_robin_them(self):
         plan = factory.PlanFactory()
         environment = plan.environments.all()[0]
         datainfra1 = factory.DatabaseInfraFactory(plan=plan, environment=environment, capacity=10)
+        instance1 = factory.InstanceFactory(address="127.0.0.1", port=27017, databaseinfra=datainfra1)
         datainfra2 = factory.DatabaseInfraFactory(plan=plan, environment=environment, capacity=10)
+        instance2 = factory.InstanceFactory(address="127.0.0.2", port=27017, databaseinfra=datainfra2)
         for i in range(10):
             should_choose = (datainfra1, datainfra2)[i%2]
             choosed = DatabaseInfra.best_for(plan=plan, environment=environment)
@@ -50,10 +46,12 @@ class DatabaseInfraTestCase(TestCase):
 
 
     def test_best_for_with_only_over_capacity_datainfra_returns_None(self):
+        """tests database infra capacity"""
         NUMBER_OF_DATABASES_TO_TEST = 4
         plan = factory.PlanFactory()
         environment = plan.environments.all()[0]
         datainfra = factory.DatabaseInfraFactory(plan=plan, environment=environment, capacity=NUMBER_OF_DATABASES_TO_TEST)
+        instance = factory.InstanceFactory(address="127.0.0.1", port=27017, databaseinfra=datainfra)
         for i in range(NUMBER_OF_DATABASES_TO_TEST):
             self.assertEqual(datainfra, DatabaseInfra.best_for(plan=plan, environment=environment))
             factory_logical.DatabaseFactory(databaseinfra=datainfra)
