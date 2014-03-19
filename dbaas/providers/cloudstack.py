0a3f4db9d209632e712e694ae5a965c4be234430
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-03-19 11:59:21 -0300

    Change CloudStack 'client' to accept 'POST' and 'GET' HTTP methods

--- dbaas/providers/cloudstack.py
+++ dbaas/providers/cloudstack.py
@@ -16,13 +16,13 @@ class SignedApiCall(object):
         self.apiKey = apiKey
         self.secret = secret
  
-    def request(self, args):
+    def request(self, args, action):
         args['apiKey'] = self.apiKey
  
         self.params = []
         self._sort_request(args)
         self._create_signature()
-        self._build_post_request()
+        self._build_post_request(action)
  
     def _sort_request(self, args):
         keys = sorted(args.keys())
@@ -39,28 +39,39 @@ class SignedApiCall(object):
  
         self.signature = base64.b64encode(digest)
  
-    def _build_post_request(self):
+    def _build_post_request(self, action):
         self.query += '&signature=' + urllib.quote_plus(self.signature)
-        self.value = self.api_url + '?' + self.query
+        self.value = self.api_url
+        if action == 'GET':
+            self.value += '?' + self.query
 
 
 class CloudStackClient(SignedApiCall):
     def __getattr__(self, name):
         def handlerFunction(*args, **kwargs):
+            action = args[0] 
             if kwargs:
                 return self._make_request(name, kwargs)
-            return self._make_request(name, args[0])
+            return self._make_request(name, args[1], action)
         return handlerFunction
  
     def _http_get(self, url):
         response = urllib.urlopen(url)
         return response.read()
+
+    def _http_post(self, url, data):
+        response = urllib.urlopen(url,data)
+        return response.read()
  
-    def _make_request(self, command, args):
+    def _make_request(self, command, args, action):
         args['response'] = 'json'
         args['command'] = command
-        self.request(args)
-        data = self._http_get(self.value)
+        self.request(args,action)
+        if action == 'GET':
+            data = self._http_get(self.value)
+        else:
+            data = self._http_post(self.value, self.query)
+        # The response is of the format {commandresponse: actual-data}
         key = command.lower() + "response"
         return json.loads(data)[key]
 
@@ -80,7 +91,7 @@ class CloudStackProvider(object):
                           'projectid': '0be19820-1fe2-45ea-844e-77f17e16add5'
                         }
 
-        response = api.deployVirtualMachine(request)
+        response = api.deployVirtualMachine('GET',request)
         print response
         host = models.Host()
         host.cp_id = response['id']
@@ -89,7 +100,7 @@ class CloudStackProvider(object):
 
             LOG.debug("VirtualMachine created!")
             request = {'projectid': '0be19820-1fe2-45ea-844e-77f17e16add5', 'id':'%s' % (response['id']) }
-            response = api.listVirtualMachines(request)
+            response = api.listVirtualMachines('GET',request)
             
             host.hostname = response['virtualmachine'][0]['nic'][0]['ipaddress']
             host.cloud_portal_host = True
@@ -146,7 +157,7 @@ class CloudStackProvider(object):
         request = {  'projectid': '0be19820-1fe2-45ea-844e-77f17e16add5',
                            'id': '%s' % (host.cp_id)
                         }
-        response = api.destroyVirtualMachine(request)
+        response = api.destroyVirtualMachine('GET',request)
         
         if response['jobid']:
             LOG.warning("VirtualMachine destroyed!")
