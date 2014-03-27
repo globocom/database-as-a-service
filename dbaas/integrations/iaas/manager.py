b06a0c55ffad10ae817f51bf61c7fabc0def0175
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-03-27 14:12:25 -0300

    Add destroy to iaas manager

--- dbaas/integrations/iaas/manager.py
+++ dbaas/integrations/iaas/manager.py
@@ -1,5 +1,4 @@
 from cloudstack.cloudstack_client import CloudStackProvider
-from cloudstack.models import PlanAttr
 from pre_provisioned.pre_provisioned_client import  PreProvisionedProvider
 import logging
 
@@ -7,15 +6,25 @@ LOG = logging.getLogger(__name__)
 
 class IaaSManager():
     
-    def __init__(self, plan, environment):
+    def __init__(self, plan=None, environment=None, database=None, *args, **kwargs):
         LOG.info("IaaS manager initialized...")
-        self.plan = plan
-        self.environment = environment
 
-        if plan.provider == 0:
-            self.create_pre_provisioned_instance()
-        elif plan.provider == 1:
-            self.create_cloud_stack_instance()
+        if database:
+            provider = database.plan.provider
+            if provider == 0:
+                LOG.info("Destroying pre provisioned database...")
+                PreProvisionedProvider().destroy_instance(database, *args, **kwargs)
+            elif provider == 1:
+                LOG.info("Destroying cloud stack instance...")
+                CloudStackProvider().destroy_instance(database, *args, **kwargs)
+        else:
+            self.plan = plan
+            self.environment = environment
+
+            if plan.provider == 0:
+                self.create_pre_provisioned_instance()
+            elif plan.provider == 1:
+                self.create_cloud_stack_instance()
 
     def create_cloud_stack_instance(self):
         LOG.info("Creating cloud stack instance...")
@@ -24,4 +33,7 @@ class IaaSManager():
     def create_pre_provisioned_instance(self):
         LOG.info("Creating pre provisioned instance...")
         self.instance =  PreProvisionedProvider().create_instance(self.plan, self.environment)
+
+
+
 
