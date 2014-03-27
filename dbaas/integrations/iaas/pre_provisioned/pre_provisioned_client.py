8cc37e6f4b07ed7279171a3f3060e6dc0452fb14
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-03-27 14:13:24 -0300

    Refactor pre provisioned destroy

--- dbaas/integrations/iaas/pre_provisioned/pre_provisioned_client.py
+++ dbaas/integrations/iaas/pre_provisioned/pre_provisioned_client.py
@@ -1,5 +1,14 @@
 from ..base import BaseProvider
 from physical.models import DatabaseInfra
+from logical.models import Database, Credential
+from util import make_db_random_password
+import datetime
+from drivers import factory_for
+import logging
+
+
+
+LOG = logging.getLogger(__name__)
 
 class PreProvisionedProvider(BaseProvider):
     
@@ -14,5 +23,32 @@ class PreProvisionedProvider(BaseProvider):
             return None
         return best_datainfra
 
-    def destroy_instance(self):
-        pass
+    def destroy_instance(self, database,*args, **kwargs):
+        """
+        Overrides the delete method so that a database can be put in quarantine and not removed
+        """
+        #do_something()
+        if database.is_in_quarantine:
+            LOG.warning("Database %s is in quarantine and will be removed" % database.name)
+            for credential in database.credentials.all():
+                instance = factory_for(database.databaseinfra)
+                instance.remove_user(credential)
+            super(Database, database).delete(*args, **kwargs)  # Call the "real" delete() method.
+        else:
+            LOG.warning("Putting database %s in quarantine" % database.name)
+            if database.credentials.exists():
+                for credential in database.credentials.all():
+                    new_password = make_db_random_password()
+                    new_credential = Credential.objects.get(pk=credential.id)
+                    new_credential.password = new_password
+                    new_credential.save()
+
+                    instance = factory_for(database.databaseinfra)
+                    instance.update_user(new_credential)
+
+            else:
+                LOG.info("There is no credential on this database: %s" % database.databaseinfra)
+
+            database.is_in_quarantine = True
+            database.quarantine_dt = datetime.datetime.now().date()
+            database.save()
