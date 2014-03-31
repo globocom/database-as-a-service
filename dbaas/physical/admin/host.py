762f3920b0b1b3bd3d73e3530bd53402d331267e
Ricardo Dias <ricardo.gestec@corp.globo.com>
2014-03-31 16:37:30 -0300

    inline HostAttr on host

--- dbaas/physical/admin/host.py
+++ dbaas/physical/admin/host.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from django_services import admin
+from django_services import admin as services_admin
+from django.contrib import admin
 from ..service.host import HostService
+from integrations.iaas.cloudstack.models import HostAttr
 
 
-class HostAdmin(admin.DjangoServicesAdmin):
+class HostAttrInline(admin.StackedInline):
+    model = HostAttr
+    max_num = 0
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class HostAdmin(services_admin.DjangoServicesAdmin):
     service_class = HostService
     search_fields = ("hostname",)
     list_display = ("hostname", "monitor_url_html",)
@@ -15,3 +23,7 @@ class HostAdmin(admin.DjangoServicesAdmin):
     monitor_url_html.allow_tags = True
     monitor_url_html.short_description = "Monitor url"
     monitor_url_html.admin_order_field = "monitor_url"
+
+    inlines = [
+        HostAttrInline,
+    ]
