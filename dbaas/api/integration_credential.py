491f1941342d3fd5c9b002af6d4f3525dff88eb2
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-09 15:38:46 -0300

    Add integration credential api

---
+++ dbaas/api/integration_credential.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from rest_framework import viewsets, serializers
+from integrations.credentials.models import IntegrationCredential
+from .environment import EnvironmentSerializer
+from .integration_type import IntegrationTypeSerializer
+
+
+class IntegrationCredentialSerializer(serializers.HyperlinkedModelSerializer):
+
+    environments = EnvironmentSerializer(many=True, read_only=True)
+    integration_type = IntegrationTypeSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = IntegrationCredential
+        fields = ('user', 'password', 'integration_type', 'token', 'secret', 'endpoint', 'environments')
+
+
+class IntegrationCredentialAPI(viewsets.ModelViewSet):
+    """
+    Integration Credential Api
+    """
+    serializer_class = IntegrationCredentialSerializer
+    queryset = IntegrationCredential.objects.all()
+
+
