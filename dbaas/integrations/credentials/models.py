cec5df22bd812ed1ef7226c56d0a3a9aa014d359
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-09 11:03:19 -0300

    Add Integrations Credentials

--- dbaas/integrations/credentials/models.py
+++ dbaas/integrations/credentials/models.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals
 from util.models import BaseModel
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from physical.models import Environment
+from django_extensions.db.fields.encrypted import EncryptedCharField
 
 
 
@@ -20,3 +22,25 @@ class IntegrationType(BaseModel):
     type = models.IntegerField(choices=INTEGRATION_CHOICES,
                                 default=0)
     
+
+class Credentials(BaseModel):
+
+    user = models.CharField(verbose_name=_("User."),
+                            max_length=100,
+                            help_text=_("User used to authenticate."),
+                            blank=False,
+                            null=False)
+    password = EncryptedCharField(verbose_name=_("Password"), max_length=255, blank=True, null=False)
+    type = models.ForeignKey(IntegrationType, related_name="type", on_delete=models.PROTECT)
+    token = models.CharField(verbose_name=_("Authentication Token"),
+                            max_length=255,
+                            blank=True,
+                            null=True)
+    secret = EncryptedCharField(verbose_name=_("Token"), max_length=255, blank=True, null=False)
+    endpoint = models.CharField(verbose_name=_("Endpoint"),
+                            max_length=255,
+                            help_text=_("Usually it is in the form host:port. Authentication endpoint."),
+                            blank=True,
+                            null=True)
+    environments = models.ManyToManyField(Environment)
+
