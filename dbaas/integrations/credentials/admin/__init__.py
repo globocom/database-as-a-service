5867a09fb43f8c4480d7aef89a200e952406a648
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-09 16:04:54 -0300

    Enable integration credential and integration type admin

--- dbaas/integrations/credentials/admin/__init__.py
+++ dbaas/integrations/credentials/admin/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 from django.contrib import admin
+from .integration_credential import IntegrationCredentialAdmin
+from .integration_type import IntegrationTypeAdmin
 from .. import models
 
-admin.site.register(models.IntegrationType, )
-admin.site.register(models.IntegrationCredential, )
+admin.site.register(models.IntegrationType, IntegrationTypeAdmin)
+admin.site.register(models.IntegrationCredential, IntegrationCredentialAdmin)
