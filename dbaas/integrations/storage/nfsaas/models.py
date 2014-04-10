baf54cf2fe7a0f7314299552753a982c4ce2504c
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-10 12:07:19 -0300

    Change nfsaas attributes name

--- dbaas/integrations/storage/nfsaas/models.py
+++ dbaas/integrations/storage/nfsaas/models.py
@@ -5,13 +5,19 @@ from django.utils.translation import ugettext_lazy as _
 
 class EnvironmentAttr(BaseModel):
     dbaas_environment = models.ForeignKey('physical.Environment', related_name="nfsaas_environment_attributes")
-    nfsaas_environment = models.CharField(verbose_name=_("Nfaas Environment ID"), max_length=10)
+    nfsaas_environment = models.CharField(verbose_name=_("Environment ID"), max_length=10)
+    class Meta:
+    	verbose_name_plural = 'NFaaS Custom Environment Attributes'
 
 class PlanAttr(BaseModel):
     dbaas_plan = models.ForeignKey('physical.Plan', related_name="nfsaas_plan_attributes")
-    nfsaas_plan = models.CharField(verbose_name=_("Nfaas Plan ID"), max_length=10)
+    nfsaas_plan = models.CharField(verbose_name=_("Plan ID"), max_length=10)
+    class Meta:
+    	verbose_name_plural = 'NFaaS Custom Plan Attributes'
 
 class HostAttr(BaseModel):
     host = models.ForeignKey('physical.Host', related_name="nfsaas_host_attributes", unique=True)
-    nfsaas_export_id = models.CharField(verbose_name=_("Nfaas Export ID"), max_length=10)
-    nfsaas_path = models.CharField(verbose_name=_("Nfaas Path"), max_length=100)
+    nfsaas_export_id = models.CharField(verbose_name=_("Export ID"), max_length=10)
+    nfsaas_path = models.CharField(verbose_name=_("Path"), max_length=100)
+    class Meta:
+    	verbose_name_plural = 'NFaaS Custom Host Attributes'
