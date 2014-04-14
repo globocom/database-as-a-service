46c0af276d7f8028edc5023dbf592cb8711ed703
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-04-14 18:34:28 -0300

    filtering teams by username

--- dbaas/api/team.py
+++ dbaas/api/team.py
@@ -2,7 +2,9 @@
 from __future__ import absolute_import, unicode_literals
 from rest_framework import viewsets, serializers
 from account import models
+import logging
 
+LOG = logging.getLogger(__name__)
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -17,3 +19,16 @@ class TeamAPI(viewsets.ReadOnlyModelViewSet):
     """
     serializer_class = TeamSerializer
     queryset = models.Team.objects.all()
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = models.Team.objects.all()
+        username = self.request.QUERY_PARAMS.get('username', None)
+        if username is not None:
+            LOG.info("filtering teams by username %s" % username)
+            queryset = queryset.filter(users__username=username)
+        return queryset
+
