174547663db0a1a5ac858790168e18d966d49d90
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-24 13:03:39 -0300

    Add zabbix integration

--- dbaas/integrations/monitoring/manager.py
+++ dbaas/integrations/monitoring/manager.py
@@ -1,4 +1,5 @@
 from dbmonitor.provider import DBMonitorProvider
+from dbaas_zabbix.provider import ZabbixProvider
 import logging
 
 LOG = logging.getLogger(__name__)
@@ -8,9 +9,11 @@ class MonitoringManager():
     @classmethod
     def create_monitoring(cls, databaseinfra):
         LOG.info("Creating monitoring...")
+        ZabbixProvider().create_monitoring(dbinfra=databaseinfra)
         return DBMonitorProvider().create_dbmonitor_monitoring(databaseinfra)
             
     @classmethod 
     def remove_monitoring(cls, databaseinfra):
         LOG.info("Removing monitoring...")
+        ZabbixProvider().destroy_monitoring(dbinfra=databaseinfra)
         return DBMonitorProvider().remove_dbmonitor_monitoring(databaseinfra)
