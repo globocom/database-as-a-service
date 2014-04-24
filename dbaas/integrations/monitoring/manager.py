dd78f68ed0e6dcecc66f1b87972f73f887edfb3e
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-24 17:48:09 -0300

    Add exception handling

--- dbaas/integrations/monitoring/manager.py
+++ dbaas/integrations/monitoring/manager.py
@@ -8,12 +8,20 @@ class MonitoringManager():
     
     @classmethod
     def create_monitoring(cls, databaseinfra):
-        LOG.info("Creating monitoring...")
-        ZabbixProvider().create_monitoring(dbinfra=databaseinfra)
-        return DBMonitorProvider().create_dbmonitor_monitoring(databaseinfra)
+        try:
+            LOG.info("Creating monitoring...")
+            ZabbixProvider().create_monitoring(dbinfra=databaseinfra)
+            return DBMonitorProvider().create_dbmonitor_monitoring(databaseinfra)
+        except Exception, e:
+            LOG.warn("Exception: %s" % e)
+            return None
             
     @classmethod 
     def remove_monitoring(cls, databaseinfra):
-        LOG.info("Removing monitoring...")
-        ZabbixProvider().destroy_monitoring(dbinfra=databaseinfra)
-        return DBMonitorProvider().remove_dbmonitor_monitoring(databaseinfra)
+        try:
+            LOG.info("Removing monitoring...")
+            ZabbixProvider().destroy_monitoring(dbinfra=databaseinfra)
+            return DBMonitorProvider().remove_dbmonitor_monitoring(databaseinfra)
+        except Exception, e:
+            LOG.warn("Exception: %s" % e)
+            return None
