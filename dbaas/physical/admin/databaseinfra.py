9da0186ee97c8f4b869a4467b6de8a43024e19e0
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-30 11:01:42 -0300

    Add dbinfra cloudstack custom attributes

--- dbaas/physical/admin/databaseinfra.py
+++ dbaas/physical/admin/databaseinfra.py
@@ -6,9 +6,19 @@ from django_services import admin
 from ..service.databaseinfra import DatabaseInfraService
 from ..models import Instance
 from ..forms import DatabaseInfraForm, InstanceModelFormSet
+from dbaas_cloudstack.models import DatabaseInfraAttr
+
 
 from util.html import render_progress_bar
 
+
+class DatabaseInfraAttrInline(django_admin.TabularInline):
+    model = DatabaseInfraAttr
+    max_num = 2
+    fields = ('ip', 'is_write',)
+    template = 'admin/physical/shared/inline_form.html'
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 class InstanceAdmin(django_admin.TabularInline):
     model = Instance
@@ -30,7 +40,8 @@ class DatabaseInfraAdmin(admin.DjangoServicesAdmin):
     change_form_template = "admin/physical/databaseinfra/change_form.html"
     
     inlines = [
-        InstanceAdmin
+        InstanceAdmin,
+        DatabaseInfraAttrInline,
     ]
 
     def capacity_bar(self, datainfra):
