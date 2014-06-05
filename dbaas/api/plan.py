2126c73152c9bd9b086c70a562e97e736dfff7e9
Leandro Souza <leandro.souza@corp.globo.com>
2014-06-05 11:41:29 -0300

    changed method def list to queryset

--- dbaas/api/plan.py
+++ dbaas/api/plan.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from rest_framework import viewsets, serializers
-from rest_framework.response import Response
 from physical import models
 from .environment import EnvironmentSerializer
 
@@ -22,17 +21,18 @@ class PlanAPI(viewsets.ReadOnlyModelViewSet):
     serializer_class = PlanSerializer
     queryset = models.Plan.objects.all()
 
-    def list(self, request):
+    
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
         queryset = models.Plan.objects.all()
-        engine_id = request.QUERY_PARAMS.get('engine_id', None)
-    
-        if engine_id is not None:
-            try:
+        engine_id = self.request.QUERY_PARAMS.get('engine_id', None)
+        try:
+            if engine_id is not None:
                 queryset = models.Plan.objects.filter(engine_type=models.Engine.objects.get(id=engine_id).engine_type)
-            except:
-                pass
+        except:
+            pass
     
-        serializer = PlanSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-
+        return queryset
