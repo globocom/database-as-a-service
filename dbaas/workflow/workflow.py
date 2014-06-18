bdad4fc1689e41cce29fbcc8e1e9dee4388ea8ef
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-06-18 15:12:09 -0300

    Insert step_counter into workflow_dict

--- dbaas/workflow/workflow.py
+++ dbaas/workflow/workflow.py
@@ -1,21 +1,33 @@
+# -*- coding: utf-8 -*-
 from django.utils.module_loading import import_by_path
+from notification.models import TaskHistory
+import logging
 
+LOG = logging.getLogger(__name__)
 
-def start_workflow(workflow_dict):
+def start_workflow(workflow_dict, task=None):
 	try :
-		step_counter = 0 
+		if not 'steps' in workflow_dict:
+		    return False
+		workflow_dict['step_counter'] = 0
 
 		for step in workflow_dict['steps']:
-			step_counter+=1
+			workflow_dict['step_counter']+=1
+
+			LOG.info("Step %s number %i" % (step, workflow_dict['step_counter']))
+
 			my_class = import_by_path(step)
 			my_instance = my_class()
 
+			if task:
+			    task.update_details(persist=True, details=str(my_instance))
+
 			if my_instance.do(workflow_dict)!=True:
-				raise Exception
-			
+			    raise Exception
+
 	except Exception, e:
 		print e
-		workflow_dict['steps'] = workflow_dict['steps'][:step_counter]
+		workflow_dict['steps'] = workflow_dict['steps'][:workflow_dict['step_counter']]
 		stop_workflow(workflow_dict)
 
 
