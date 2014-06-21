8d76cf01794350c1df1436d90fa1b95f5f4865d3
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-06-21 12:43:58 -0300

    Setup baseStep unicode

--- dbaas/workflow/steps/base.py
+++ dbaas/workflow/steps/base.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class BaseStep(object):
+    def __unicode__(self):
+    	return "I am a step"
     
     def do(self, workflow_dict):
     	raise NotImplementedError
