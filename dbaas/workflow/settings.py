87724787180e04a66e05932f2123fd0288651148
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-06-24 10:28:58 -0300

    Add create_nfs

--- dbaas/workflow/settings.py
+++ dbaas/workflow/settings.py
@@ -7,7 +7,8 @@ DEPLOY_MYSQL_WORKFLOW = (
 DEPLOY_VIRTUALMACHINE = (
             'workflow.steps.build_databaseinfra.BuildDatabaseInfra',
             'workflow.steps.create_virtualmachines.CreateVirtualMachine',
-            'workflow.steps.create_dns.CreateDns'
+            'workflow.steps.create_dns.CreateDns',
+            'workflow.steps.create_nfs.CreateNfs',
 
 
     )
