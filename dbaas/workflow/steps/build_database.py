376eb3085f08357554d06b4cc4ff87fe6a9fa768
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-06-25 17:15:02 -0300

    Add build database step

---
+++ dbaas/workflow/steps/build_database.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import logging
+from base import BaseStep
+from logical.models import Database
+
+
+LOG = logging.getLogger(__name__)
+
+
+class BuildDatabase(BaseStep):
+
+    def __unicode__(self):
+        return "Creating logical database..."
+
+    def do(self, workflow_dict):
+        try:
+
+            if not workflow_dict['team'] or not workflow_dict['description'] or not workflow_dict['databaseinfra']:
+                return False
+
+            LOG.info("Creating Database...")
+            database = Database.provision(name= workflow_dict['name'], databaseinfra= workflow_dict['databaseinfra'])
+            database.team = workflow_dict['team']
+            database.project = workflow_dict['project']
+            database.description = workflow_dict['description']
+            database.save()
+
+            workflow_dict['database'] = database
+
+            return True
+        except Exception, e:
+            print e
+            return False
+
+    def undo(self, workflow_dict):
+        try:
+
+            LOG.info("Destroying the database....")
+
+            workflow_dict['database'].delete()
+
+            return True
+        except Exception, e:
+            print e
+            return False
