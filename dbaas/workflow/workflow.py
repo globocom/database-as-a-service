e517aeb2211f1b28daf2015ba21d27b53efc1afa
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-06-25 18:10:51 -0300

    Add exception handling

--- dbaas/workflow/workflow.py
+++ dbaas/workflow/workflow.py
@@ -36,11 +36,18 @@ def start_workflow(workflow_dict, task=None):
 def stop_workflow(workflow_dict):
     LOG.info("Running undo...")
 
-    for step in workflow_dict['steps'][::-1]:
-        workflow_dict['step_counter'] -= 1
-        my_class = import_by_path(step)
-        my_instance = my_class()
+    try:
 
-        LOG.info("Step %i %s " %
-                (workflow_dict['step_counter'], str(my_instance)))
-        my_instance.undo(workflow_dict)
+        for step in workflow_dict['steps'][::-1]:
+            workflow_dict['step_counter'] -= 1
+            my_class = import_by_path(step)
+            my_instance = my_class()
+
+            LOG.info("Step %i %s " %
+                    (workflow_dict['step_counter'], str(my_instance)))
+            my_instance.undo(workflow_dict)
+
+        return True
+    except Exception, e:
+        print e
+        return False
