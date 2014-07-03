b518588b8c1e1a38018ce4ff88280b09cd07b946
Ricardo Dias <ricardo.gestec@corp.globo.com>
2014-07-03 20:14:43 -0300

    Seting DEPLOY_MONGO and DEPLOY_UNKNOWN

--- dbaas/workflow/settings.py
+++ dbaas/workflow/settings.py
@@ -15,7 +15,17 @@ DEPLOY_MYSQL = (
 
 
 DEPLOY_MONGO = (
+            'workflow.steps.build_databaseinfra.BuildDatabaseInfra',
+            'workflow.steps.create_virtualmachines.CreateVirtualMachine',
+            'workflow.steps.create_dns.CreateDns',
+            'workflow.steps.create_nfs.CreateNfs',
+            'workflow.steps.init_database.InitDatabase',
+            'workflow.steps.check_database_connection.CheckDatabaseConnection',
+            'workflow.steps.check_dns.CheckDns',
+            'workflow.steps.create_zabbix.CreateZabbix',
+            'workflow.steps.create_dbmonitor.CreateDbMonitor'
+)
 
-
+DEPLOY_UNKNOWN = (
 
 )
