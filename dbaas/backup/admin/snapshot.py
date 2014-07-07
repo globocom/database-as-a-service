795ea2b69a032ff90cf9e591452a802e4af6ce1a
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-07-07 17:32:24 -0300

    Customize admin interface

--- dbaas/backup/admin/snapshot.py
+++ dbaas/backup/admin/snapshot.py
@@ -9,15 +9,19 @@ LOG = logging.getLogger(__name__)
 
 
 class SnapshotAdmin(admin.ModelAdmin):
-    
+
     actions = None
-    
-    list_display = ("database_name", "instance", "start_at", "end_at", "purge_at", "type", "status", )
+
+    list_display = ("database_name", "instance", "start_at", "end_at", "purge_at", "type", "status", "environment")
     search_fields = ("database_name", "instance__dns", )
-    readonly_fields = ("database_name", "instance", "start_at", "end_at", "purge_at", "type", "status", "snapshopt_id", "snapshot_name", "export_path", "size", )
+    readonly_fields = ("database_name", "instance", "start_at", "end_at", "purge_at", "type", "status", "snapshopt_id", "snapshot_name", "export_path", "size", "environment")
     ordering = ["-start_at"]
 
     def has_delete_permission(self, request, obj=None):
         return False
     def has_add_permission(self, request, obj=None):
         return False
+
+    def get_changelist(self, request, **kwargs):
+        from .views.main import ChangeList
+        return ChangeList
