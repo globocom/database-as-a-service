04eee39390654ea6ca56447a95721471a97114b9
Ricardo Dias <ricardo@ricardodias.org>
2014-07-16 15:58:32 -0300

    Comment monitoring steps (temporarily)

--- dbaas/workflow/settings.py
+++ dbaas/workflow/settings.py
@@ -19,11 +19,11 @@ DEPLOY_MONGO = (
             'workflow.steps.create_virtualmachines.CreateVirtualMachine',
             'workflow.steps.create_dns.CreateDns',
             'workflow.steps.create_nfs.CreateNfs',
-            'workflow.steps.init_database.InitDatabase',
+            'workflow.steps.init_database_mongodb.InitDatabaseMongoDB',
             'workflow.steps.check_database_connection.CheckDatabaseConnection',
             'workflow.steps.check_dns.CheckDns',
-            'workflow.steps.create_zabbix.CreateZabbix',
-            'workflow.steps.create_dbmonitor.CreateDbMonitor'
+            #'workflow.steps.create_zabbix.CreateZabbix',
+            #'workflow.steps.create_dbmonitor.CreateDbMonitor'
 )
 
 DEPLOY_UNKNOWN = (
