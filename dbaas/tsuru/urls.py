da657245e1705ed75a06a6c0c8792d93f140eafb
Andrews Medina <andrewsmedina@gmail.com>
2014-10-29 14:39:42 -0200

    tsuru: updated api endpoints to works with tsuru 0.8.0+.

--- dbaas/tsuru/urls.py
+++ dbaas/tsuru/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from views import ListPlans, GetServiceStatus, GetServiceInfo, ServiceAdd, ServiceBind, ServiceUnbind
+from views import ListPlans, GetServiceStatus, GetServiceInfo, ServiceAdd, ServiceBind, ServiceRemove
 
 urlpatterns = patterns('tsuru.views',
     url(r'^resources/plans$', ListPlans.as_view()),
     url(r'^resources/(?P<database_name>\w+)/status$', GetServiceStatus.as_view()),
     url(r'^services/(?P<database_name>\w+)$', GetServiceInfo.as_view()),
     url(r'^resources$', ServiceAdd.as_view()),
-    url(r'^resources/(?P<database_name>\w+)$', ServiceBind.as_view()),
-    url(r'^resources/(?P<database_name>\w+)/hostname/(?P<unbind_ip>\w+)$', ServiceUnbind.as_view()),
+    url(r'^resources/(?P<database_name>\w+)$', ServiceRemove.as_view()),
+    url(r'^resources/(?P<database_name>\w+)/bind$', ServiceBind.as_view()),
 )
