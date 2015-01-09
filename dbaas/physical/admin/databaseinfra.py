f804695106d4873a5bcb981f4ecd9ef5694f7cce
Ricardo Dias <ricardo@ricardodias.org>
2015-01-09 18:48:58 -0200

    Add field database_type

--- dbaas/physical/admin/databaseinfra.py
+++ dbaas/physical/admin/databaseinfra.py
@@ -30,7 +30,9 @@ class DatabaseInfraOfferingInline(django_admin.TabularInline):
 
 class InstanceAdmin(django_admin.TabularInline):
     model = Instance
-    fields = ('hostname', 'dns', 'address', 'port', 'is_active', 'is_arbiter')
+    #fields = ('hostname', 'dns', 'address', 'port', 'is_active', 'is_arbiter')
+    #fields = ('hostname', 'dns', 'address', 'port', 'is_active', 'database_type')
+    fields = ('hostname', 'dns', 'address', 'port', 'is_active', 'is_arbiter', 'database_type')
     # max_num = 1
     # can_delete = False
     extra = 1
