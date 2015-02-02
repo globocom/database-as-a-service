cbacf000c3d3f6d104e59ea2df11e13a073a35f6
Felippe Raposo <raposo.felippe@gmail.com>
2015-02-02 14:30:02 -0200

    Remove old logic from clone and resize database

--- dbaas/logical/models.py
+++ dbaas/logical/models.py
@@ -303,31 +303,12 @@ class Database(BaseModel):
                 database.name, quarantine_time))
 
     @classmethod
-    @transaction.commit_manually
     def clone(cls, database, clone_name, plan, environment, user):
-        # try:
-        #    cloned_database = Database.objects.get(pk=database.pk)
-        #    cloned_database.name = clone_name
-        #    cloned_database.pk = None
-        #    if not databaseinfra:
-        #        cloned_database.databaseinfra = DatabaseInfra.best_for(database.plan, database.environment)
-        #    else:
-        #        cloned_database.databaseinfra = databaseinfra
-        #    cloned_database.save()
-        # except:
-        #    transaction.rollback()
-        #    raise
-        # else:
-        #    transaction.commit()
-        # call task
-        #    from notification.tasks import clone_database
-        #    result = clone_database.delay(database, cloned_database, user=user)
         from notification.tasks import clone_database
 
         result = clone_database.delay(database, clone_name, plan, environment, user=user)
 
     @classmethod
-    @transaction.commit_manually
     def resize(cls, database, cloudstackpack, user):
         from notification.tasks import resize_database
 
