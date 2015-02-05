c3e81aee9852a94befdec9d9b2d3f9cd3d7914e2
Felippe Raposo <raposo.felippe@gmail.com>
2015-02-05 10:51:28 -0200

    Change task to create a taskHistory object

--- dbaas/system/tasks.py
+++ dbaas/system/tasks.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from dbaas.celery import app
 from util.decorators import only_one
 from models import CeleryHealthCheck
-#from celery.utils.log import get_task_logger
-
-#LOG = get_task_logger(__name__)
+from notification.models import TaskHistory
 
 import logging
 LOG = logging.getLogger(__name__)
@@ -13,9 +10,17 @@ LOG = logging.getLogger(__name__)
 
 
 @app.task(bind=True)
+@only_one(key="celery_healthcheck_last_update", timeout=20)
 def set_celery_healthcheck_last_update(self):
-    
-    LOG.info("Setting Celery healthcheck last update")
-    CeleryHealthCheck.set_last_update()
-    
-    return
+    try:
+        task_history = TaskHistory.register(request=self.request, user=None)
+
+        LOG.info("Setting Celery healthcheck last update")
+        CeleryHealthCheck.set_last_update()
+
+        task_history.update_status_for(TaskHistory.STATUS_SUCCESS, details="Finished")
+    except Exception, e:
+        LOG.warn("Oopss...{}".format(e))
+        task_history.update_status_for(TaskHistory.STATUS_ERROR, details=e)
+    finally:
+        return
