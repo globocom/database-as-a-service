bc551c0d46c63ed688184e45e3211e29a30214d7
Felippe Raposo <raposo.felippe@gmail.com>
2015-02-09 12:21:29 -0200

    Add check db status step

---
+++ dbaas/workflow/steps/util/resize/check_database_status.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import logging
+from ...util.base import BaseStep
+
+LOG = logging.getLogger(__name__)
+
+
+class CheckDatabaseStatus(BaseStep):
+
+    def __unicode__(self):
+        return "Checking database status..."
+
+    def do(self, workflow_dict):
+        try:
+            if not 'database' in workflow_dict:
+                return False
+
+            if not 'databaseinfra' in workflow_dict:
+                workflow_dict['databaseinfra'] = workflow_dict['database'].databaseinfra
+
+            LOG.info("Getting driver class")
+            driver = workflow_dict['databaseinfra'].get_driver()
+
+            if driver.check_status():
+                LOG.info("Database is ok...")
+                workflow_dict['database'].status=1
+                workflow_dict['database'].save()
+                return True
+
+            return False
+        except Exception,e:
+            LOG.info("Error: {}".format(e))
+            pass
+
+    def undo(self, workflow_dict):
+        LOG.info("Nothing to do here...")
+        return True
