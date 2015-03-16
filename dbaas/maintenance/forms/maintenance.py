7c36b55f2c1f11b5bd1cdca1571bdcee86a1e76b
felippemr <raposo.felippe@gmail.com>
2015-03-16 11:58:35 -0300

    Remove unused commit from maintenance form

--- dbaas/maintenance/forms/maintenance.py
+++ dbaas/maintenance/forms/maintenance.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from django.utils.translation import ugettext_lazy as _
 import logging
 from django import forms
 from .. import models
