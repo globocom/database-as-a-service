32cf0799de1b44f6c408935cac3d8f4e0375e170
Ricardo Dias <ricardo@ricardodias.org>
2015-03-19 15:59:00 -0300

    Add field on maintenance model

---
+++ dbaas/maintenance/migrations/0010_auto__add_field_maintenance_query_error__add_field_maintenance_affecte.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Maintenance.query_error'
+        db.add_column(u'maintenance_maintenance', 'query_error',
+                      self.gf('django.db.models.fields.TextField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Maintenance.affected_hosts'
+        db.add_column(u'maintenance_maintenance', 'affected_hosts',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Maintenance.query_error'
+        db.delete_column(u'maintenance_maintenance', 'query_error')
+
+        # Deleting field 'Maintenance.affected_hosts'
+        db.delete_column(u'maintenance_maintenance', 'affected_hosts')
+
+
+    models = {
+        u'maintenance.hostmaintenance': {
+            'Meta': {'unique_together': "((u'host', u'maintenance'),)", 'object_name': 'HostMaintenance', 'index_together': "[[u'host', u'maintenance']]"},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'finished_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'host': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'host_maintenance'", 'to': u"orm['physical.Host']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_log': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'maintenance': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'maintenance'", 'to': u"orm['maintenance.Maintenance']"}),
+            'rollback_log': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'started_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '4'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'maintenance.maintenance': {
+            'Meta': {'object_name': 'Maintenance'},
+            'affected_hosts': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'celery_task_id': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'host_query': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_script': ('django.db.models.fields.TextField', [], {}),
+            'maximum_workers': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'query_error': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'rollback_script': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'scheduled_for': ('django.db.models.fields.DateTimeField', [], {'unique': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'physical.host': {
+            'Meta': {'object_name': 'Host'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'hostname': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'monitor_url': ('django.db.models.fields.URLField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['maintenance']
