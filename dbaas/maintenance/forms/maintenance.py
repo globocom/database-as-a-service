86ff6720c2378d03855f2e6602cd64621e1ce6a5
felippemr <raposo.felippe@gmail.com>
2015-03-25 18:44:02 -0300

    Validate hostsid input

--- dbaas/maintenance/forms/maintenance.py
+++ dbaas/maintenance/forms/maintenance.py
@@ -3,13 +3,16 @@ from __future__ import absolute_import, unicode_literals
 import logging
 from django import forms
 from .. import models
+from ..validators import validate_hosts_ids
 
 
 LOG = logging.getLogger(__name__)
 
 class MaintenanceForm(forms.ModelForm):
+    hostsid = forms.CharField(widget=forms.TextInput,
+        validators= [validate_hosts_ids])
     class Meta:
         model = models.Maintenance
         fields = ( "description", "scheduled_for", "main_script", "rollback_script",
          "maximum_workers", "status", "celery_task_id",)
-    
+
---
+++ dbaas/maintenance/validators/__init__.py
@@ -0,0 +1,27 @@
+from django.core.exceptions import ValidationError
+from physical import models
+from collections import Counter
+from django.core import validators
+
+
+def validate_hosts_ids(hosts_ids):
+
+    validators.validate_comma_separated_integer_list(hosts_ids)
+
+    hosts_ids = [int(x) for x in hosts_ids.split(',')]
+    hosts_ids_set = Counter(set(hosts_ids))
+
+    hosts_ids = Counter(hosts_ids)
+
+    if hosts_ids_set != hosts_ids:
+        raise ValidationError(u'Some ids are repeated')
+
+    real_hosts = Counter(models.Host.objects.filter(id__in=hosts_ids,
+        ).values_list('id', flat=True))
+
+    diff = real_hosts - hosts_ids
+    diff2 = hosts_ids - real_hosts
+
+    if diff or diff2:
+        f_diff = diff|diff2
+        raise ValidationError(u'Some ids do not exist: {}'.format(f_diff.keys()))
