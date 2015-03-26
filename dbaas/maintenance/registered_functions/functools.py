4b2646bb91fb888480c67ca10c2d9388f1c0ec31
felippemr <raposo.felippe@gmail.com>
2015-03-26 12:32:34 -0300

    Add get_function function

--- dbaas/maintenance/registered_functions/functools.py
+++ dbaas/maintenance/registered_functions/functools.py
@@ -9,6 +9,18 @@ def get_registered_functions():
             if is_mod_function(current_module,
                 func) and func.__name__ not in['is_mod_function','get_registered_functions'])
 
+def get_function(func_doc):
+    current_module = sys.modules[__name__]
+    func_list =  list((func for func in current_module.__dict__.itervalues()
+            if is_mod_function(current_module,func) and func.__doc__ ==func_doc))
+
+    try:
+        func_list = func_list[0]
+    except IndexError, e:
+        return []
+
+    return func_list
+
 
 def return_host_id_str(host_id):
     """Get HostId String"""
