2523f263d8d013e71fcf9a9cb3a6b9fcce2d55f1
felippemr <raposo.felippe@gmail.com>
2015-04-22 11:57:28 -0300

    Fix region_migration models indetation

--- dbaas/region_migration/models.py
+++ dbaas/region_migration/models.py
@@ -11,15 +11,15 @@ from .migration_steps import get_engine_steps
 
 LOG = logging.getLogger(__name__)
 
+
 class DatabaseRegionMigration(BaseModel):
-    database =  models.ForeignKey(Database, verbose_name=("Database"), null= False, unique= True)
+    database = models.ForeignKey(Database, verbose_name=("Database"), null=False, unique=True)
     current_step = models.PositiveSmallIntegerField(verbose_name=_("Current Step"), null=False,
-        blank= False, default=0)
+                                                    blank=False, default=0)
     next_step = models.PositiveSmallIntegerField(verbose_name=_("Next Step"), null=True,
-        blank= False)
+                                                 blank=False)
 
     def __unicode__(self):
-        #return '{} on step {}'.format(self.database, self.current_step)
         return '{}'.format(self.database)
 
     def get_steps(self,):
@@ -39,11 +39,12 @@ class DatabaseRegionMigration(BaseModel):
             return get_engine_steps(self.database.engine_type)[self.next_step].description
         else:
             return ""
-    
+
     class Meta:
         permissions = (
             ("view_databaseregiomigration", "Can view database region migration"),
         )
+
 
 class DatabaseRegionMigrationDetail(BaseModel):
     WAITING = 0
@@ -60,25 +61,25 @@ class DatabaseRegionMigrationDetail(BaseModel):
         (REVOKED, 'Revoked')
     )
     database_region_migration = models.ForeignKey(DatabaseRegionMigration,
-        related_name="details", null=False)
-    step = models.PositiveSmallIntegerField(verbose_name=_("Step"), null=False,
-        blank= False)
+                                                  related_name="details",
+                                                  null=False)
+    step = models.PositiveSmallIntegerField(verbose_name=_("Step"),
+                                            null=False,
+                                            blank=False)
     scheduled_for = models.DateTimeField(verbose_name=_("Schedule for"), null=False)
     started_at = models.DateTimeField(verbose_name=_("Started at"), null=True, blank=True)
-    finished_at = models.DateTimeField(verbose_name=_("Finished at"),null=True, blank=True)
+    finished_at = models.DateTimeField(verbose_name=_("Finished at"), null=True, blank=True)
     created_by = models.CharField(verbose_name=_("Created by"), max_length=255, null=False,
-        blank=False)
+                                  blank=False)
     revoked_by = models.CharField(verbose_name=_("Revoked by"), max_length=255, null=True,
-        blank=True)
+                                  blank=True)
     status = models.IntegerField(choices=MAINTENANCE_STATUS, default=WAITING)
-    log = models.TextField(verbose_name=_("Log"),null=False, blank=False)
-
+    log = models.TextField(verbose_name=_("Log"), null=False, blank=False)
 
     class Meta:
         unique_together = (
             ('database_region_migration', 'step', 'scheduled_for',)
         )
-
 
     def __unicode__(self):
         return " Detail for %s" % (self.database_region_migration)
