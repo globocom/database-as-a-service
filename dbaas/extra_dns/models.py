6034c3520962600fc564c8eb168ce6043677d66f
felippemr <raposo.felippe@gmail.com>
2015-07-08 11:16:43 -0300

    Add extra dns signals to insert and remove dbmonitor's reference

--- dbaas/extra_dns/models.py
+++ dbaas/extra_dns/models.py
@@ -3,6 +3,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from util.models import BaseModel
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+from dbaas_dbmonitor.provider import DBMonitorProvider
 import logging
 
 LOG = logging.getLogger(__name__)
@@ -24,3 +27,31 @@ class ExtraDns(BaseModel):
 
     def __unicode__(self):
         return u"Extra dns: {} for database : {}".format(self.dns, self.database)
+
+
+@receiver(post_save, sender=ExtraDns)
+def extra_dns_post_save(sender, **kwargs):
+    """
+        extra dns post save signal. Inserts extra dns on dbmonitor's database
+    """
+    LOG.debug("extra_dns post-save triggered")
+
+    extra_dns = kwargs.get("instance")
+    is_new = kwargs.get("created")
+
+    if is_new:
+        database = extra_dns.database
+        DBMonitorProvider().insert_extra_dns(database=database, extra_dns=extra_dns.dns)
+
+
+@receiver(post_delete, sender=ExtraDns)
+def extra_dns_post_delete(sender, **kwargs):
+    """
+        extra dns post delete signal. Delete extra dns on dbmonitor's database
+    """
+    LOG.debug("extra_dns post-delete triggered")
+
+    extra_dns = kwargs.get("instance")
+    database = extra_dns.database
+
+    DBMonitorProvider().remove_extra_dns(database=database, extra_dns=extra_dns.dns)
