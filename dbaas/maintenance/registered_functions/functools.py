1733c4c62483adb5757db61e0f5beea83e1ee0fb
Ricardo Dias <ricardo@ricardodias.org>
2015-07-14 18:46:46 -0300

    New functions to mantainance

--- dbaas/maintenance/registered_functions/functools.py
+++ dbaas/maintenance/registered_functions/functools.py
@@ -1,26 +1,30 @@
-import sys, inspect
+import sys
+import inspect
 import logging
 LOG = logging.getLogger(__name__)
+
 
 def _is_mod_function(mod, func):
     return inspect.isfunction(func) and inspect.getmodule(func) == mod
 
+
 def _get_key(item):
     return item[1]
+
 
 def _get_registered_functions():
     current_module = sys.modules[__name__]
     function_list = ((func.__name__, func.__doc__) for func in current_module.__dict__.itervalues()
-            if _is_mod_function(current_module,
-                func) and not func.__name__.startswith('_'))
+                     if _is_mod_function(current_module,
+                                         func) and not func.__name__.startswith('_'))
 
     return sorted(function_list, key=_get_key)
 
 
 def _get_function(func_name):
     current_module = sys.modules[__name__]
-    func_list =  list((func for func in current_module.__dict__.itervalues()
-            if _is_mod_function(current_module,func) and func.__name__ ==func_name))
+    func_list = list((func for func in current_module.__dict__.itervalues()
+                      if _is_mod_function(current_module, func) and func.__name__ == func_name))
 
     try:
         func_list = func_list[0]
@@ -37,11 +41,19 @@ def get_hostmane(host_id):
     host = Host.objects.get(id=host_id)
     return host.hostname
 
+
+def get_hostaddress(host_id):
+    """Return HOST_ADDRESS"""
+    from physical.models import Host
+    host = Host.objects.get(id=host_id)
+    return host.address
+
+
 def get_infra_name(host_id):
     """Return DATABASE_INFRA_NAME"""
     from physical.models import Host
     host = Host.objects.filter(id=host_id,
-        ).select_related('instance').select_related('databaseinfra')
+                               ).select_related('instance').select_related('databaseinfra')
 
     try:
         host = host[0]
@@ -49,16 +61,16 @@ def get_infra_name(host_id):
         LOG.warn("Host id does not exists: {}. {}".format(host_id, e))
         return None
 
-
     return host.instance_set.all()[0].databaseinfra.name
+
 
 def get_database_name(host_id):
     """Return DATABASE_NAME"""
     from physical.models import Host
     host = Host.objects.filter(id=host_id,
-        ).select_related('instance',
-        ).select_related('databaseinfra',
-        ).select_related('database',)
+                               ).select_related('instance',
+                                                ).select_related('databaseinfra',
+                                                                 ).select_related('database',)
 
     try:
         host = host[0]
@@ -69,7 +81,8 @@ def get_database_name(host_id):
     try:
         database = host.instance_set.all()[0].databaseinfra.databases.all()[0]
     except IndexError, e:
-        LOG.warn("There is not a database on this host: {}. {}".format(host_id, e))
+        LOG.warn(
+            "There is not a database on this host: {}. {}".format(host_id, e))
         return None
 
     return database.name
@@ -79,14 +92,13 @@ def get_infra_user(host_id):
     """Return DATABASE_INFRA_USER"""
     from physical.models import Host
     host = Host.objects.filter(id=host_id,
-        ).select_related('instance').select_related('databaseinfra')
+                               ).select_related('instance').select_related('databaseinfra')
 
     try:
         host = host[0]
     except IndexError, e:
         LOG.warn("Host id does not exists: {}. {}".format(host_id, e))
         return None
-
 
     return host.instance_set.all()[0].databaseinfra.user
 
@@ -95,14 +107,13 @@ def get_infra_password(host_id):
     """Return DATABASE_INFRA_PASSWORD"""
     from physical.models import Host
     host = Host.objects.filter(id=host_id,
-        ).select_related('instance').select_related('databaseinfra')
+                               ).select_related('instance').select_related('databaseinfra')
 
     try:
         host = host[0]
     except IndexError, e:
         LOG.warn("Host id does not exists: {}. {}".format(host_id, e))
         return None
-
 
     return host.instance_set.all()[0].databaseinfra.password
 
@@ -121,11 +132,12 @@ def get_host_user(host_id):
     try:
         host_attr = host.cs_host_attributes.all()[0]
     except IndexError, e:
-        LOG.warn("Host id does not own a cs_host_attr: {}. {}".format(host_id, e))
+        LOG.warn(
+            "Host id does not own a cs_host_attr: {}. {}".format(host_id, e))
         return None
 
-
     return host_attr.vm_user
+
 
 def get_host_password(host_id):
     """Return HOST_PASSWORD"""
@@ -141,9 +153,9 @@ def get_host_password(host_id):
     try:
         host_attr = host.cs_host_attributes.all()[0]
     except IndexError, e:
-        LOG.warn("Host id does not own a cs_host_attr: {}. {}".format(host_id, e))
+        LOG.warn(
+            "Host id does not own a cs_host_attr: {}. {}".format(host_id, e))
         return None
-
 
     return host_attr.vm_password
 
@@ -152,9 +164,9 @@ def get_engine_type_name(host_id):
     """Return ENGINE_TYPE"""
     from physical.models import Host
     host = Host.objects.filter(id=host_id,
-        ).select_related('instance',
-        ).select_related('databaseinfra',
-        )
+                               ).select_related('instance',
+                                                ).select_related('databaseinfra',
+                                                                 )
 
     try:
         host = host[0]
@@ -162,5 +174,21 @@ def get_engine_type_name(host_id):
         LOG.warn("Host id does not exists: {}. {}".format(host_id, e))
         return None
 
-
     return host.instance_set.all()[0].databaseinfra.engine.name
+
+
+def get_max_database_size(host_id):
+    """Return MAX_DATABASE_SIZE"""
+    from physical.models import Host
+    host = Host.objects.filter(id=host_id,
+                               ).select_related('instance',
+                                                ).select_related('databaseinfra',
+                                                                 ).select_related('plan')
+
+    try:
+        host = host[0]
+    except IndexError, e:
+        LOG.warn("Host id does not exists: {}. {}".format(host_id, e))
+        return None
+
+    return host.instance_set.all()[0].databaseinfra.plan.max_db_size
