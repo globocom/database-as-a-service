83c0d919129676c8eda37c76e76bc62240e50608
Ricardo Dias <ricardo@ricardodias.org>
2015-07-23 17:28:20 -0300

    New function check_replication_and_switch

--- dbaas/drivers/base.py
+++ dbaas/drivers/base.py
@@ -206,6 +206,16 @@ class BaseDriver(object):
     def start_slave(self, instance):
         pass
 
+    def check_replication_and_switch(self, instance, attempts=21):
+        from time import sleep
+        for attempt in range(0, attempts):
+            if self.is_replication_ok(instance):
+                self.switch_master()
+                return
+            LOG.info("Waiting 10s to check replication...")
+            sleep(10)
+        raise Exception("Could not switch master because of replication's delay")
+
 
 class DatabaseStatus(object):
 
