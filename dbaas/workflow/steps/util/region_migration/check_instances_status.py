cf7086620df23d8af15f7c9898edf39f64965549
felippemr <raposo.felippe@gmail.com>
2015-08-18 16:52:00 -0300

    Add step to check instances status

--- dbaas/workflow/steps/util/region_migration/check_instances_status.py
+++ dbaas/workflow/steps/util/region_migration/check_instances_status.py
@@ -3,17 +3,31 @@ import logging
 from util import full_stack
 from workflow.steps.util.base import BaseStep
 from workflow.exceptions.error_codes import DBAAS_0020
+from drivers.base import ConnectionError
 
 LOG = logging.getLogger(__name__)
 
 
-class DecreaseTTL(BaseStep):
+class CheckInstancesStatus(BaseStep):
 
     def __unicode__(self):
         return "Checking instances status..."
 
     def do(self, workflow_dict):
         try:
+            databaseinfra = workflow_dict['databaseinfra']
+            driver = databaseinfra.get_driver()
+            for instance in driver.get_database_instances():
+                msg = "Instance({}) is down".format(instance)
+                exception_msg = Exception(msg)
+                try:
+                    status = driver.check_status(instance)
+                except ConnectionError:
+                    raise exception_msg
+                else:
+                    if status is False:
+                        raise exception_msg
+
             return True
         except Exception:
             traceback = full_stack()
