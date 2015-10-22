116e5727bae7e5bf12c9b2c90e10898d7eb9cf8e
felippemr <raposo.felippe@gmail.com>
2015-10-22 16:16:01 -0200

    Add AnalyseSerice api

---
+++ dbaas/services/analyzing/integration/analyze.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import requests
+from services.analyzing.exceptions.analyze import ServiceNotAvailable
+
+
+class AnalyzeService(object):
+    def __init__(self, endpoint, healh_check_route, healh_check_string):
+        self.endpoint = endpoint
+        self.healh_check_route = healh_check_route
+        self.healh_check_string = healh_check_string
+
+        if self.__service_is_not__available():
+            raise ServiceNotAvailable("Service not Working")
+
+    def __service_is_not__available(self,):
+        healh_check_endpoint = self.endpoint + self.healh_check_route
+        response = requests.get(healh_check_endpoint)
+
+        return not response.content == self.healh_check_string
+
+    def run(self, **kwargs):
+        response = requests.post(self.endpoint, data=kwargs)
+        return response.json()
