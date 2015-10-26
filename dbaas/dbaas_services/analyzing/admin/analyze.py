2dd0af2c9859c51e66594166137e8a279ea6fbc9
felippemr <raposo.felippe@gmail.com>
2015-10-26 16:08:49 -0200

    Remove analyzed_at from search fields

--- dbaas/dbaas_services/analyzing/admin/analyze.py
+++ dbaas/dbaas_services/analyzing/admin/analyze.py
@@ -8,7 +8,7 @@ from dbaas_services.analyzing.forms import AnalyzeRepositoryForm
 class AnalyzeRepositoryAdmin(admin.DjangoServicesAdmin):
     form = AnalyzeRepositoryForm
     service_class = AnalyzeRepositoryService
-    search_fields = ("analyzed_at", "database_name", "engine_name",
+    search_fields = ("database_name", "engine_name",
                      "environment_name", "instance_name",)
     list_filter = ("analyzed_at", "memory_alarm", "cpu_alarm")
     list_display = ("analyzed_at", "database_name", "engine_name",
