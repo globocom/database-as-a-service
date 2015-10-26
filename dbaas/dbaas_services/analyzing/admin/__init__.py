16830fd406503bdb75241df4229d3a18ac455384
felippemr <raposo.felippe@gmail.com>
2015-10-26 10:33:13 -0200

    Fix relative import

--- dbaas/dbaas_services/analyzing/admin/__init__.py
+++ dbaas/dbaas_services/analyzing/admin/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.contrib import admin
-from .. import models
-from .analyze import AnalyzeRepositoryAdmin
+from dbaas_services.analyzing import models
+from dbaas_services.analyzing.admin.analyze import AnalyzeRepositoryAdmin
 
 admin.site.register(models.AnalyzeRepository, AnalyzeRepositoryAdmin)
