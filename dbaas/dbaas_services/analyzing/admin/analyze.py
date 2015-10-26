021225cbce30b70c350133f5ae3cae9409bdd6ae
felippemr <raposo.felippe@gmail.com>
2015-10-26 16:07:22 -0200

    Add filters to analyzing admin

--- dbaas/dbaas_services/analyzing/admin/analyze.py
+++ dbaas/dbaas_services/analyzing/admin/analyze.py
@@ -8,6 +8,9 @@ from dbaas_services.analyzing.forms import AnalyzeRepositoryForm
 class AnalyzeRepositoryAdmin(admin.DjangoServicesAdmin):
     form = AnalyzeRepositoryForm
     service_class = AnalyzeRepositoryService
+    search_fields = ("analyzed_at", "database_name", "engine_name",
+                     "environment_name", "instance_name",)
+    list_filter = ("analyzed_at", "memory_alarm", "cpu_alarm")
     list_display = ("analyzed_at", "database_name", "engine_name",
                     "environment_name", "instance_name", "cpu_alarm",
                     "memory_alarm")
