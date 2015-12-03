d89e250414ebc916630e74d994c1675ae5ab9465
felippemr <raposo.felippe@gmail.com>
2015-12-03 14:40:02 -0200

    Change dashboard index

--- dbaas/dashboard/search_indexes.py
+++ dbaas/dashboard/search_indexes.py
@@ -6,7 +6,7 @@ class DatabaseIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(document=True, use_template=True)
     name = indexes.CharField(model_attr='name')
     team = indexes.CharField(model_attr='team')
-    project = indexes.CharField(model_attr='project', null=True)
+    project = indexes.CharField(model_attr='project', default="")
     databaseinfra = indexes.CharField(model_attr='databaseinfra',)
 
     def prepare_databaseinfra(self, obj):
