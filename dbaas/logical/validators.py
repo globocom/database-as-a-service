82d37302b567366c840eed9ec43b3ddb0526d4e5
felippemr <raposo.felippe@gmail.com>
2016-03-24 12:55:35 -0300

    Improve database validator

--- dbaas/logical/validators.py
+++ dbaas/logical/validators.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
 from logical.models import Database
-from django.core.exceptions import ValidationError
 from django.core.exceptions import ObjectDoesNotExist
 from system.models import Configuration
 
 
-def validate_evironment(database_name, environment_name):
+def database_name_evironment_constraint(database_name, environment_name):
     try:
-        database = Database.objects.get(database_name)
+        database = Database.objects.get(name=database_name)
     except ObjectDoesNotExist:
-        pass
+        return False
     else:
         dev_envs = Configuration.get_by_name_as_list('dev_envs')
         new_db_env_is_not_dev = environment_name not in dev_envs
@@ -19,7 +17,4 @@ def validate_evironment(database_name, environment_name):
         db_env_is_prod = database.environment.name in prod_envs
 
         if new_db_env_is_not_dev and db_env_is_prod:
-            raise ValidationError(
-                _('%(database_name)s already exists in production!'),
-                params={'database_name': database_name},
-            )
+            return True
