bd33ce2ba10a364e6e5949e9aa1b1389093c2583
tayanemoura <tayanemoura@id.uff.br>
2016-04-27 12:51:39 -0300

    add pagination to dashboard

--- dbaas/dashboard/views.py
+++ dbaas/dashboard/views.py
@@ -2,7 +2,7 @@ import logging
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
-
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from physical.models import DatabaseInfra
 from logical.models import Database
 
@@ -13,14 +13,32 @@ LOG = logging.getLogger(__name__)
 def dashboard(request):
     env_id = request.GET.get('env_id')
     engine_type = request.GET.get('engine_type')
+    dbinfra_list = DatabaseInfra.objects.all().order_by('name')
+    url_par = "?"
+    if env_id or engine_type:
+        url_par = "?"
+        if env_id:
+            url_par += "env_id=" + str(env_id) + "&"
+            dbinfra_list = dbinfra_list.filter(environment__id=env_id)
+        if engine_type:
+            url_par += "engine_type=" + str(engine_type) + "&"
+            dbinfra_list = dbinfra_list.filter(engine__engine_type__name=engine_type)
+    #else:
+    #    url_par = "?"
 
-    dbinfra = DatabaseInfra.objects.all()
-    if engine_type:
-        dbinfra = dbinfra.filter(engine__engine_type__name=engine_type)
-    if env_id:
-        dbinfra = dbinfra.filter(environment__id=env_id)
+    paginator = Paginator(dbinfra_list,100)
 
-    return render_to_response("dashboard/dashboard.html", {'dbinfra': dbinfra}, context_instance=RequestContext(request))
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+
+    try:
+        dbinfra = paginator.page(page)
+
+    except(EmptyPage, InvalidPage):
+        dbinfra = paginator.page(paginator.num_pages)
+    return render_to_response("dashboard/dashboard.html", {'dbinfra': dbinfra, 'url_par': url_par}, context_instance=RequestContext(request))
 
 
 @login_required
