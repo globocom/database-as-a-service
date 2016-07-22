0633b3d177143dd41ab44d9c532f15c0a8a21fd5
otherpirate <mauro_murari@hotmail.com>
2016-07-22 17:51:23 -0300

    Prepare FaaS code to be used in all engines

--- dbaas/workflow/steps/mongodb/deploy/create_nfs.py
+++ dbaas/workflow/steps/mongodb/deploy/create_nfs.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 from util import full_stack, get_credentials_for
-from dbaas_nfsaas.dbaas_api import DatabaseAsAServiceApi
-from dbaas_nfsaas.faas_provider import Provider
-from dbaas_nfsaas.models import HostAttr
-from dbaas_credentials.models import CredentialType
 from ...util.base import BaseStep
+from ...util.nfsaas_utils import create_disk, delete_disk
 from ....exceptions.error_codes import DBAAS_0009
 
 LOG = logging.getLogger(__name__)
@@ -16,42 +13,23 @@ class CreateNfs(BaseStep):
     def __unicode__(self):
         return "Requesting NFS export..."
 
-    def _get_faas_provider(self, workflow_dict):
-        faas_credentials = get_credentials_for(
-            environment=workflow_dict['environment'],
-            credential_type=CredentialType.FAAS
-        )
-        dbaas_api = DatabaseAsAServiceApi(credentials=faas_credentials)
-        return Provider(dbaas_api, HostAttr)
-
     def do(self, workflow_dict):
         try:
             workflow_dict['disks'] = []
 
             for instance in workflow_dict['instances']:
-                host = instance.hostname
-
                 if instance.is_arbiter:
                     LOG.info("Do not create NFS disk for Arbiter...")
                     continue
 
-                LOG.info("Creating nfsaas disk...")
-                provider = self._get_faas_provider(workflow_dict)
-                disk = provider.create_export(
-                    host=host,
-                    size_kb=workflow_dict['plan'].disk_offering.size_kb
+                disk = create_disk(
+                    workflow_dict['environment'], instance.hostname,
+                    workflow_dict['plan']
                 )
+                if not disk:
+                    return False
+
                 workflow_dict['disks'].append(disk)
-
-                LOG.info(
-                    "Creating nfsaas access to disk: {}".format(
-                        disk.nfsaas_path_host
-                    )
-                )
-
-                provider.create_access(
-                    export_path=disk.nfsaas_path_host, address=host.address
-                )
 
             return True
 
@@ -65,12 +43,9 @@ class CreateNfs(BaseStep):
 
     def undo(self, workflow_dict):
         try:
-            provider = self._get_faas_provider(workflow_dict)
             for host in workflow_dict['hosts']:
-                LOG.info("Destroying NFS export...")
-                for disk in HostAttr.objects.filter(host=host):
-                    if not provider.delete_export(disk.nfsaas_path_host):
-                        return False
+                if not delete_disk(workflow_dict['environment'], host):
+                    return False
 
             return True
         except Exception:
