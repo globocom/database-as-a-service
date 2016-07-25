eb04bb61d984c354ad389fce72c7423dbf9d56d4
otherpirate <mauro_murari@hotmail.com>
2016-07-25 11:53:28 -0300

    Create and remove snapshots

--- dbaas/workflow/steps/util/nfsaas_utils.py
+++ dbaas/workflow/steps/util/nfsaas_utils.py
@@ -19,7 +19,7 @@ def get_faas_provider(environment):
 
 
 def create_disk(enviroment, host, plan):
-    provider = get_faas_provider(enviroment)
+    provider = get_faas_provider(environment=enviroment)
 
     LOG.info("Creating NFS disk...")
     disk = provider.create_export(
@@ -37,10 +37,32 @@ def create_disk(enviroment, host, plan):
 
 
 def delete_disk(environment, host):
-    provider = get_faas_provider(environment)
+    provider = get_faas_provider(environment=environment)
 
     LOG.info("Destroying NFS export...")
     for disk in HostAttr.objects.filter(host=host):
-        if not provider.delete_export(disk.nfsaas_path_host):
+        if not provider.delete_export(path=disk.nfsaas_path_host):
             return False
     return True
+
+
+def delete_export(environment, export_path):
+    provider = get_faas_provider(environment=environment)
+    return provider.delete_export(path=export_path)
+
+
+def create_snapshot(environment, host):
+    provider = get_faas_provider(environment=environment)
+    disk = HostAttr.objects.get(host=host)
+    result = provider.create_snapshot(export_path=disk.nfsaas_path_host)
+    return result['snapshot']
+
+
+def delete_snapshot(snapshot):
+    provider = get_faas_provider(
+        environment=snapshot.instance.databaseinfra.environment
+    )
+    disk = HostAttr.objects.get(nfsaas_path=snapshot.export_path)
+    return provider.delete_snapshot(
+        export_path=disk.nfsaas_path_host, snapshot_id=snapshot.snapshopt_id
+    )
