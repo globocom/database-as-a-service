4d784092d3a163bfef6e72e80cdafe5da6d49df4
otherpirate <mauro_murari@hotmail.com>
2016-07-26 12:23:36 -0300

    Restore snapshot/backup with FaaS

--- dbaas/workflow/steps/util/nfsaas_utils.py
+++ dbaas/workflow/steps/util/nfsaas_utils.py
@@ -1,8 +1,10 @@
 import logging
+from dbaas_cloudstack.models import HostAttr as CsHostAttr
 from dbaas_credentials.models import CredentialType
 from dbaas_nfsaas.models import HostAttr
 from dbaas_nfsaas.dbaas_api import DatabaseAsAServiceApi
 from dbaas_nfsaas.faas_provider import Provider
+from dbaas_nfsaas.util import delete_all_disk_files
 from util import get_credentials_for
 
 LOG = logging.getLogger(__name__)
@@ -53,7 +55,7 @@ def delete_export(environment, export_path):
 
 def create_snapshot(environment, host):
     provider = get_faas_provider(environment=environment)
-    disk = HostAttr.objects.get(host=host)
+    disk = HostAttr.objects.get(host=host, is_active=True)
     result = provider.create_snapshot(export_path=disk.nfsaas_path_host)
     return result['snapshot']
 
@@ -65,4 +67,35 @@ def delete_snapshot(snapshot):
     disk = HostAttr.objects.get(nfsaas_path=snapshot.export_path)
     return provider.delete_snapshot(
         export_path=disk.nfsaas_path_host, snapshot_id=snapshot.snapshopt_id
+    )
+
+
+def restore_snapshot(environment, export_id, snapshot_id):
+    provider = get_faas_provider(environment=environment)
+    disk = HostAttr.objects.get(nfsaas_export_id=export_id)
+
+    return provider.restore_snapshot(
+        export_path=disk.nfsaas_path_host, snapshot_id=snapshot_id
+    )
+
+
+def restore_wait_for_finished(environment, job_id):
+    provider = get_faas_provider(environment=environment)
+    return provider.wait_for_job_finished(restore_job=job_id)
+
+
+def create_access(environment, export_path, host):
+    provider = get_faas_provider(environment=environment)
+    return provider.create_access(
+        export_path=export_path, address=host.address
+    )
+
+
+def clean_unused_data(export_id, host):
+    disk = HostAttr.objects.get(nfsaas_export_id=export_id)
+    cs_host_attr = CsHostAttr.objects.get(host=host)
+
+    delete_all_disk_files(
+        disk.nfsaas_path, disk.nfsaas_path_host,
+        disk.nfsaas_export_id, disk.host, cs_host_attr
     )
