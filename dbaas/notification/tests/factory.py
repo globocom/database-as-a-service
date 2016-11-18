b67a7807faf89cbcbc2a0cef0fb7f7705aab6078
otherpirate <mauro_murari@hotmail.com>
2016-11-18 16:10:23 -0200

    Using constant to RUNNING status in TaskHistoryFactory

--- dbaas/notification/tests/factory.py
+++ dbaas/notification/tests/factory.py
@@ -10,4 +10,4 @@ class TaskHistoryFactory(factory.DjangoModelFactory):
     task_name = factory.Sequence(lambda n: 'task_name_{0}'.format(n))
     task_id = factory.Sequence(lambda n: 'task_id_{0}'.format(n))
     user = factory.Sequence(lambda n: 'user_{0}'.format(n))
-    task_status = "PENDING"
+    task_status = models.TaskHistory.STATUS_PENDING
