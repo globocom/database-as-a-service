d83448d79f16246e54b89e59db4bd7f17e5a4b0d
otherpirate <mauro_murari@hotmail.com>
2017-01-11 15:14:31 -0200

    Disabled/Enable dbmonitor for instance on database upgrade

--- dbaas/workflow/steps/util/upgrade/db_monitor.py
+++ dbaas/workflow/steps/util/upgrade/db_monitor.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from util import build_context_script, exec_remote_command
-from dbaas_cloudstack.models import HostAttr, CloudStackPack
+from dbaas_dbmonitor.provider import DBMonitorProvider
 from workflow.steps.util.base import BaseInstanceStep
 
 
@@ -8,6 +7,7 @@ class DBMonitorStep(BaseInstanceStep):
 
     def __init__(self, instance):
         super(DBMonitorStep, self).__init__(instance)
+        self.provider = DBMonitorProvider()
 
     def do(self):
         raise NotImplementedError
@@ -19,16 +19,16 @@ class DBMonitorStep(BaseInstanceStep):
 class DisableMonitoring(DBMonitorStep):
 
     def __unicode__(self):
-        return "Disabling DB Montinor..."
+        return "Disabling DB Monitor..."
 
     def do(self):
-        pass
+        self.provider.disabled_dbmonitor_monitoring_instance(self.instance)
 
 
 class EnableMonitoring(DBMonitorStep):
 
     def __unicode__(self):
-        return "Enabling DB Montinor..."
+        return "Enabling DB Monitor..."
 
     def do(self):
-        pass
+        self.provider.enabled_dbmonitor_monitoring_instance(self.instance)
