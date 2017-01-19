79622c2e267e736d9949ad55e024331496879899
otherpirate <mauro_murari@hotmail.com>
2017-01-19 15:26:37 -0200

    Checking exist database zabbix monitoring before destroy

--- dbaas/workflow/steps/util/upgrade/zabbix.py
+++ dbaas/workflow/steps/util/upgrade/zabbix.py
@@ -35,10 +35,17 @@ class DestroyAlarms(ZabbixStep):
         return "Destroying Zabbix alarms..."
 
     def do(self):
-        self.zabbix_provider.delete_instance_monitors(
+        monitors = self.zabbix_provider.get_host_triggers(
             self.instance.hostname.hostname
         )
-        self.zabbix_provider.delete_instance_monitors(self.instance.dns)
+        if monitors:
+            self.zabbix_provider.delete_instance_monitors(
+                self.instance.hostname.hostname
+            )
+
+        monitors = self.zabbix_provider.get_host_triggers(self.instance.dns)
+        if monitors:
+            self.zabbix_provider.delete_instance_monitors(self.instance.dns)
 
 
 class CreateAlarms(ZabbixStep):
