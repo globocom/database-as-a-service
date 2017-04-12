fa9bbb1f522a985597b186cff2152cfe3de190f9
Elesbom <rafael.goncalves@corp.globo.com>
2017-04-12 17:15:43 -0300

    Create structure to run tests with docker-compose.

---
+++ tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create DATABASE
+echo "create database IF NOT EXISTS dbaas;" | mysql -h db -u root -p123
+
+# Create user on mongo
+python add_user_admin_on_mongo.py
+
+# Run tests
+make test
