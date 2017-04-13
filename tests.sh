57696543384dd615d49438cd8ce72020940b0763
Elesbom <rafael.goncalves@corp.globo.com>
2017-04-13 10:54:14 -0300

    Ru tests with docker

--- tests.sh
+++ tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create DATABASE
-echo "create database IF NOT EXISTS dbaas;" | mysql -h db -u root -p123
+echo "create database IF NOT EXISTS dbaas;" | mysql -h mysqldb56 -u root -p123
 
 # Create user on mongo
 python add_user_admin_on_mongo.py
 
 # Run tests
-make test
+make test_with_docker
