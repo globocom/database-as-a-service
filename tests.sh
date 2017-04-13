bb8d474b9658ed67e729795be9a1f26d591376d6
Elesbom <rafael.goncalves@corp.globo.com>
2017-04-13 15:03:19 -0300

    Add port on mysql servico on compose conf

--- tests.sh
+++ tests.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+echo -n 'waiting mysql start'
+while ! mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD -e "SHOW DATABASES"
+do
+  echo -n .
+  sleep 1
+done
+
+echo 'MYSQL STARTED!!!!'
 
 # Create DATABASE
-echo "create database IF NOT EXISTS dbaas;" | mysql -h mysqldb56 -u root -p123
+echo "create database IF NOT EXISTS dbaas;" | mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD
 
 # Create user on mongo
 python add_user_admin_on_mongo.py
 
 # Run tests
 make test
+# bash
