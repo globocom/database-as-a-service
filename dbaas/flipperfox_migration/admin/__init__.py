6c4971c553dd1f3e101b903240ed1c1cb090b150
Ricardo Dias <ricardo@ricardodias.org>
2017-05-15 11:46:32 -0300

    Remove flipperfox_migration scripts

--- dbaas/flipperfox_migration/admin/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from django.contrib import admin
-from ..models import DatabaseFlipperFoxMigration, DatabaseFlipperFoxMigrationDetail
-from .databaseflipperfoxmigration import DatabaseFlipperFoxMigrationAdmin
-from .databaseflipperfoxmigrationdetail import DatabaseFlipperFoxMigrationDetailAdmin
-
-admin.site.register(DatabaseFlipperFoxMigration, DatabaseFlipperFoxMigrationAdmin)
-admin.site.register(DatabaseFlipperFoxMigrationDetail,
-                    DatabaseFlipperFoxMigrationDetailAdmin)
--- dbaas/flipperfox_migration/forms/__init__.py
+++
@@ -1,1 +0,0 @@
-from .databaseflipperfoxmigrationdetail import DatabaseFlipperFoxMigrationDetailForm
