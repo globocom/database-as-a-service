00be14988a5c9b2bd98202c99be64d9beb5c4641
otherpirate <mauro_murari@hotmail.com>
2017-05-26 15:22:36 -0300

    Removing field available_size from admin/form of DiskOffering

--- dbaas/physical/forms/disk_offerring.py
+++ dbaas/physical/forms/disk_offerring.py
@@ -8,24 +8,10 @@ class DiskOfferingForm(forms.ModelForm):
         label='Size GB', validators=[MinValueValidator(0.1)]
     )
 
-    available_size_gb = forms.FloatField(
-        label='Available Size GB', validators=[MinValueValidator(0.1)]
-    )
-
     class Meta:
         model = DiskOffering
-        fields = ("name", "size_gb", "available_size_gb")
+        fields = ("name", "size_gb")
 
     def __init__(self, *args, **kwargs):
         super(DiskOfferingForm, self).__init__(*args, **kwargs)
         self.initial['size_gb'] = self.instance.size_gb()
-        self.initial['available_size_gb'] = self.instance.available_size_gb()
-
-    def clean(self):
-        size_gb = self.cleaned_data.get("size_gb")
-        available_size_gb = self.cleaned_data.get("available_size_gb")
-        if available_size_gb > size_gb:
-            message = "The available size should be lower than disk size."
-            self._errors["available_size_gb"] = self.error_class([message])
-
-        return self.cleaned_data
