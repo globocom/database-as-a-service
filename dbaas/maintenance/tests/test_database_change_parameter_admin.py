e23713ec64d0a2960e88e49b782cd3dfbc582c5d
Ricardo Dias <ricardo@ricardodias.org>
2017-05-30 11:39:04 -0300

    new maintenance model

--- dbaas/maintenance/tests/test_database_change_parameter_admin.py
+++ dbaas/maintenance/tests/test_database_change_parameter_admin.py
@@ -2,23 +2,22 @@
 from __future__ import absolute_import, unicode_literals
 from django.test import TestCase
 from django.contrib import admin
-from ..admin.database_upgrade import DatabaseUpgradeAdmin
-from ..models import DatabaseUpgrade
-from .factory import DatabaseUpgradeFactory
+from ..admin.database_change_parameter import DatabaseChangeParameterAdmin
+from ..models import DatabaseChangeParameter
+from .factory import DatabaseChangeParameterFactory
 
 
 SEARCH_FIELDS = ("database__name", "task__id", "task__task_id")
 LIST_FILTER = [
-    "database__team", "source_plan", "target_plan", "source_plan__engine",
-    "status",
+    "database__team", "status",
 ]
 LIST_DISPLAY = (
-    "database", "database_team", "source_plan", "target_plan",
+    "database", "database_team",
     "current_step", "friendly_status", "maintenance_action", "link_task",
     "started_at", "finished_at"
 )
 READONLY_FIELDS = (
-    "database", "source_plan", "target_plan", "link_task", "started_at",
+    "database", "link_task", "started_at",
     "finished_at", "current_step", "status", "maintenance_action"
 )
 EXCLUDE = ("task", "can_do_retry")
@@ -28,16 +27,16 @@ LIST_SELECT_RELATED = None
 NO_ACTION = 'N/A'
 
 
-class DatabaseUpgradeTestCase(TestCase):
+class DatabaseChangeParameterTestCase(TestCase):
 
     def setUp(self):
-        self.database_upgrade = DatabaseUpgradeFactory()
-        self.admin = DatabaseUpgradeAdmin(
-            DatabaseUpgrade, admin.sites.AdminSite()
+        self.database_change_parameter = DatabaseChangeParameterFactory()
+        self.admin = DatabaseChangeParameterAdmin(
+            DatabaseChangeParameter, admin.sites.AdminSite()
         )
 
     def tearDown(self):
-        self.database_upgrade.delete()
+        self.database_change_parameter.delete()
 
     def test_search_fields(self):
         self.assertEqual(SEARCH_FIELDS, self.admin.search_fields)
@@ -70,67 +69,67 @@ class DatabaseUpgradeTestCase(TestCase):
         self.assertFalse(self.admin.has_delete_permission(None))
 
     def test_friendly_status_waiting(self):
-        self.database_upgrade.status = DatabaseUpgrade.WAITING
-        status_html = self.admin.friendly_status(self.database_upgrade)
+        self.database_change_parameter.status = DatabaseChangeParameter.WAITING
+        status_html = self.admin.friendly_status(self.database_change_parameter)
         self.assertIn('label-warning', status_html)
         self.assertIn('Waiting', status_html)
 
     def test_friendly_status_running(self):
-        self.database_upgrade.status = DatabaseUpgrade.RUNNING
-        status_html = self.admin.friendly_status(self.database_upgrade)
+        self.database_change_parameter.status = DatabaseChangeParameter.RUNNING
+        status_html = self.admin.friendly_status(self.database_change_parameter)
         self.assertIn('label-success', status_html)
         self.assertIn('Running', status_html)
 
     def test_friendly_status_error(self):
-        self.database_upgrade.status = DatabaseUpgrade.ERROR
-        status_html = self.admin.friendly_status(self.database_upgrade)
+        self.database_change_parameter.status = DatabaseChangeParameter.ERROR
+        status_html = self.admin.friendly_status(self.database_change_parameter)
         self.assertIn('label-important', status_html)
         self.assertIn('Error', status_html)
 
     def test_friendly_status_success(self):
-        self.database_upgrade.status = DatabaseUpgrade.SUCCESS
-        status_html = self.admin.friendly_status(self.database_upgrade)
+        self.database_change_parameter.status = DatabaseChangeParameter.SUCCESS
+        status_html = self.admin.friendly_status(self.database_change_parameter)
         self.assertIn('label-info', status_html)
         self.assertIn('Success', status_html)
 
     def test_database_team(self):
-        database_team = self.database_upgrade.database.team.name
-        admin_team = self.admin.database_team(self.database_upgrade)
+        database_team = self.database_change_parameter.database.team.name
+        admin_team = self.admin.database_team(self.database_change_parameter)
         self.assertEqual(database_team, admin_team)
 
     def test_link_task(self):
-        admin_task = self.admin.link_task(self.database_upgrade)
-        self.assertIn(str(self.database_upgrade.task.id), admin_task)
+        admin_task = self.admin.link_task(self.database_change_parameter)
+        self.assertIn(str(self.database_change_parameter.task.id), admin_task)
 
     def test_maintenance_action(self):
-        self.database_upgrade.status = DatabaseUpgrade.ERROR
-        url = self.database_upgrade.database.get_upgrade_retry_url()
+        self.database_change_parameter.status = DatabaseChangeParameter.ERROR
+        url = self.database_change_parameter.database.get_change_parameters_retry_url()
 
-        button = self.admin.maintenance_action(self.database_upgrade)
+        button = self.admin.maintenance_action(self.database_change_parameter)
         self.assertIn(url, button)
 
     def test_maintenance_action_without_error_and_cannot_do_retry(self):
-        self.database_upgrade.status = DatabaseUpgrade.SUCCESS
-        self.database_upgrade.can_do_retry = False
-        button = self.admin.maintenance_action(self.database_upgrade)
+        self.database_change_parameter.status = DatabaseChangeParameter.SUCCESS
+        self.database_change_parameter.can_do_retry = False
+        button = self.admin.maintenance_action(self.database_change_parameter)
         self.assertEqual(NO_ACTION, button)
 
     def test_maintenance_action_with_error_and_cannot_do_retry(self):
-        self.database_upgrade.status = DatabaseUpgrade.ERROR
-        self.database_upgrade.can_do_retry = False
-        button = self.admin.maintenance_action(self.database_upgrade)
+        self.database_change_parameter.status = DatabaseChangeParameter.ERROR
+        self.database_change_parameter.can_do_retry = False
+        button = self.admin.maintenance_action(self.database_change_parameter)
         self.assertEqual(NO_ACTION, button)
 
     def test_maintenance_action_without_error_and_can_do_retry(self):
-        self.database_upgrade.status = DatabaseUpgrade.SUCCESS
-        self.database_upgrade.can_do_retry = True
-        button = self.admin.maintenance_action(self.database_upgrade)
+        self.database_change_parameter.status = DatabaseChangeParameter.SUCCESS
+        self.database_change_parameter.can_do_retry = True
+        button = self.admin.maintenance_action(self.database_change_parameter)
         self.assertEqual(NO_ACTION, button)
 
     def test_maintenance_action_with_error_and_can_do_retry(self):
-        self.database_upgrade.status = DatabaseUpgrade.ERROR
-        self.database_upgrade.can_do_retry = True
+        self.database_change_parameter.status = DatabaseChangeParameter.ERROR
+        self.database_change_parameter.can_do_retry = True
 
-        url = self.database_upgrade.database.get_upgrade_retry_url()
-        button = self.admin.maintenance_action(self.database_upgrade)
+        url = self.database_change_parameter.database.get_change_parameters_retry_url()
+        button = self.admin.maintenance_action(self.database_change_parameter)
         self.assertIn(url, button)
