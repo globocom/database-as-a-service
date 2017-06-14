829da308565409954d2d7af63b03f5e55e878da0
Ricardo Dias <ricardo@ricardodias.org>
2017-06-14 10:03:25 -0300

    Add filter: dynamic

--- dbaas/physical/admin/parameter.py
+++ dbaas/physical/admin/parameter.py
@@ -7,6 +7,6 @@ from ..service.parameter import ParameterService
 class ParameterAdmin(admin.DjangoServicesAdmin):
     service_class = ParameterService
     search_fields = ("name",)
-    list_filter = ("engine_type",)
+    list_filter = ("engine_type", "dynamic", )
     list_display = ("name", "engine_type", "dynamic", "class_path")
     save_on_top = True
