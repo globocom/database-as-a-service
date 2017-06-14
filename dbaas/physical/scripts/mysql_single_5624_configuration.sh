424246a75b8e5d40b2a11cc5e6867d03e9deb3a5
Ricardo Dias <ricardo@ricardodias.org>
2017-06-14 10:06:48 -0300

    Replace hard coded parameter for dbaas configuration

--- dbaas/physical/scripts/mysql_single_5624_configuration.sh
+++ dbaas/physical/scripts/mysql_single_5624_configuration.sh
@@ -18,33 +18,35 @@ createconfigdbfile()
 syslog
 
 [mysqld]
-binlog_format=row
-performance_schema=OFF
-transaction_isolation = READ-COMMITTED
+
+binlog_format                    = {{ configuration.binlog_format.value }}
+performance_schema               = {{ configuration.performance_schema.value }}
+transaction_isolation            = {{ configuration.transaction_isolation.value }}
+
 port                              = 3306
 socket                          = /var/lib/mysql/mysql.sock
 datadir                         = /data/data
 tmpdir                          = /data/tmp/
 
-skip_external_locking
-skip_name_resolve
+skip_external_locking           = {{ configuration.skip_external_locking.value }}
+skip_name_resolve               = {{ configuration.skip_name_resolve.value }}
 
-default_storage_engine          = innodb
+default_storage_engine          = {{ configuration.default_storage_engine.value }}
+default_tmp_storage_engine      = {{ configuration.default_tmp_storage_engine.value }}
 
+character_set_server            = {{ configuration.character_set_server.value }}
 
-character_set_server            = utf8
+max_connections                 = {{ configuration.max_connections.value }}
+max_connect_errors              = {{ configuration.max_connect_errors.value }}
 
-max_connections                 = 1000
-max_connect_errors              = 999999
+thread_cache_size               = {{ configuration.thread_cache_size.value }}
+table_open_cache                = {{ configuration.table_open_cache.value }}
 
-thread_cache_size               = 32
-table_open_cache                = 4096
-
-query_cache_type                = 1
+query_cache_type                = {{ configuration.query_cache_type.value }}
 query_cache_size                = {{ configuration.query_cache_size.value }}
 
-thread_stack                    = 192K
-thread_concurrency              = 16
+thread_stack                    = {{ configuration.thread_stack.value }}
+thread_concurrency              = {{ configuration.thread_concurrency.value }}
 
 max_allowed_packet              = {{ configuration.max_allowed_packet.value }}
 sort_buffer_size                = {{ configuration.sort_buffer_size.value }}
@@ -53,18 +55,19 @@ tmp_table_size                  = {{ configuration.tmp_table_size.value }}
 max_heap_table_size             = {{ configuration.max_heap_table_size.value }}
 
 # Binary Logging
-sync_binlog                     = 1
-expire_logs_days                = 3
-max_binlog_size                 = {{ configuration.max_binlog_size.value }}
 log_bin                         = /data/repl/mysql-bin
 relay_log                       = /data/repl/mysql-relay
+sync_binlog                     = {{ configuration.sync_binlog.value }}
+expire_logs_days                = {{ configuration.expire_logs_days.value }}
+max_binlog_size                 = {{ configuration.max_binlog_size.value }}
+log_slave_updates               = {{ configuration.log_slave_updates.value }}
 log_slave_updates
 
 
 # Slow Query Logging
-long_query_time                 = 1
 slow_query_log_file             = /data/logs/mysql-slow.log
-slow_query_log
+long_query_time                 = {{ configuration.long_query_time.value }}
+slow_query_log                  = {{ configuration.slow_query_log.value }}
 
 
 # MyISAM
@@ -72,29 +75,25 @@ key_buffer_size                 = {{ configuration.key_buffer_size.value }}
 myisam_sort_buffer_size         = {{ configuration.myisam_sort_buffer_size.value }}
 read_buffer_size                = {{ configuration.read_buffer_size.value }}
 read_rnd_buffer_size            = {{ configuration.read_rnd_buffer_size.value }}
+
 # InnoDB
 innodb_data_home_dir            = /data/data
-innodb_data_file_path           = ibdata1:10M:autoextend
-innodb_autoextend_increment     = 8
-innodb_file_per_table
-
-innodb_buffer_pool_size         = {{ configuration.innodb_buffer_pool_size.value }}
-
 innodb_log_group_home_dir       = /data/data
-innodb_log_files_in_group       = 3
+innodb_data_file_path           = ibdata1:10M:autoextend
+innodb_autoextend_increment     = {{ configuration.innodb_autoextend_increment.value }}
+innodb_file_per_table           = {{ configuration.innodb_file_per_table.value }}
+innodb_buffer_pool_size         = {{ configuration.innodb_buffer_pool_size.value }}
+innodb_log_files_in_group       = {{ configuration.innodb_log_files_in_group.value }}
 innodb_log_file_size            = {{ configuration.innodb_log_file_size.value }}
 innodb_log_buffer_size          = {{ configuration.innodb_log_buffer_size.value }}
+innodb_lock_wait_timeout        = {{ configuration.innodb_lock_wait_timeout.value }}
+innodb_flush_log_at_trx_commit  = {{ configuration.innodb_flush_log_at_trx_commit.value }}
+innodb_flush_method             = {{ configuration.innodb_flush_method.value }}
+innodb_thread_concurrency       = {{ configuration.innodb_thread_concurrency.value }}
+innodb_max_dirty_pages_pct      = {{ configuration.innodb_max_dirty_pages_pct.value }}
+innodb_max_purge_lag            = {{ configuration.innodb_max_purge_lag.value }}
 
-innodb_lock_wait_timeout        = 50
-
-innodb_flush_log_at_trx_commit  = 1
-innodb_flush_method             = O_DIRECT
-innodb_file_io_threads          = 4
-innodb_thread_concurrency       = 16
-innodb_max_dirty_pages_pct      = 90
-innodb_max_purge_lag            = 0
-
-explicit_defaults_for_timestamp = TRUE
+explicit_defaults_for_timestamp = {{ configuration.explicit_defaults_for_timestamp.value }}
 
 EOF_DBAAS
 ) > /etc/my.cnf
