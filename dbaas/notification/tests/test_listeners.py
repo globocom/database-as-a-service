8865d36f78622bc32d730ceb3c67f57bdf7add18
Elesbom <rafael.goncalves@corp.globo.com>
2017-08-02 17:47:37 -0300

    Do logic for save information if the task was read or not

--- dbaas/notification/tests/test_listeners.py
+++ dbaas/notification/tests/test_listeners.py
@@ -24,7 +24,8 @@ class SaveTaskTestCase(TestCase):
             u'task_status': 'SUCCESS',
             u'user': 'admin', u'arguments': self.fake_task.arguments,
             u'updated_at': 1501177162,
-            u'is_new': 1
+            u'is_new': 1,
+            u'read': 0,
         }
 
     def _simulate_signal(self):
@@ -90,3 +91,36 @@ class SaveTaskTestCase(TestCase):
 
         args = mock_conn.hmset.call_args[0]
         self.assertEqual(args[1]['is_new'], 1)
+
+    def test_read_1_when_status_equal(self, mock_conn):
+        '''
+        If the status stored on database is the same of new value
+        read must be the old value
+        '''
+
+        mock_conn = mock_conn()
+        self.default_expected_params['read'] = 1
+        mock_conn.hgetall.return_value = self.default_expected_params
+
+        self._simulate_signal()
+
+        args = mock_conn.hmset.call_args[0]
+        self.assertEqual(args[1]['read'], 1)
+
+    def test_read_0_when_status_different(self, mock_conn):
+        '''
+        If the status stored on database is different of new value
+        read must be 0
+        '''
+
+        mock_conn = mock_conn()
+        self.default_expected_params.update({
+            'read': 0,
+            'task_status': 'OTHER'
+        })
+        mock_conn.hgetall.return_value = self.default_expected_params
+
+        self._simulate_signal()
+
+        args = mock_conn.hmset.call_args[0]
+        self.assertEqual(args[1]['read'], 0)
