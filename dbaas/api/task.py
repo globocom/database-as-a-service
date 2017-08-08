f2424a98af9b822101e4902f2d23afc53612ce5d
Elesbom <rafael.goncalves@corp.globo.com>
2017-08-08 15:10:24 -0300

    Add task name on tasks api

--- dbaas/api/task.py
+++ dbaas/api/task.py
@@ -8,7 +8,18 @@ from notification.models import TaskHistory
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TaskHistory
-        fields = ('id', 'task_id', 'task_status', 'db_id', 'object_class', 'object_id', 'updated_at', 'user', 'created_at')
+        fields = (
+            'id',
+            'task_id',
+            'task_status',
+            'db_id',
+            'object_class',
+            'object_id',
+            'updated_at',
+            'user',
+            'created_at',
+            'task_name'
+        )
 
     def get_id(self, obj):
         return obj.task_id
@@ -25,7 +36,14 @@ class TaskAPI(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     # queryset = TaskHistory.objects.all().order_by('-
     filter_backends = (filters.OrderingFilter,)
-    filter_fields = ('task_id', 'task_status', 'object_class', 'object_id', 'updated_at', 'created_at')
+    filter_fields = (
+        'task_id',
+        'task_status',
+        'object_class',
+        'object_id',
+        'updated_at',
+        'created_at'
+    )
     ordering_fields = ('created_at', 'updated_at', 'id')
     ordering = ('-created_at',)
     datetime_fields = ('created_at', 'updated_at')
