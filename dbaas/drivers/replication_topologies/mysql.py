91eecd39c390890a3f3d47be7b2aa5e5df5fdebf
otherpirate <mauro_murari@hotmail.com>
2017-08-23 15:00:46 -0300

    Adding step to create mysql bing log file position in database restore

--- dbaas/drivers/replication_topologies/mysql.py
+++ dbaas/drivers/replication_topologies/mysql.py
@@ -72,6 +72,7 @@ class MySQLFoxHA(MySQLSingle):
         return (
             'workflow.steps.mysql.restore_snapshot.restore_snapshot.RestoreSnapshot',
             'workflow.steps.util.restore_snapshot.grant_nfs_access.GrantNFSAccess',
+            'workflow.steps.mysql.restore_snapshot.make_export_snapshot.MySQLSaveBinlogPosition',
             'workflow.steps.mysql.restore_snapshot.stop_database.StopDatabase',
             'workflow.steps.mysql.restore_snapshot.umount_data_volume.UmountDataVolume',
             'workflow.steps.util.restore_snapshot.update_fstab.UpdateFstab',
