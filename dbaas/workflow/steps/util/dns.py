eb12b3e42c325dba8fa9dec46c2b703e14d36873
otherpirate <mauro_murari@hotmail.com>
2017-09-04 14:22:43 -0300

    New infra provider, check dns propagation

--- dbaas/workflow/steps/util/dns.py
+++ dbaas/workflow/steps/util/dns.py
@@ -1,6 +1,8 @@
-from dbaas_dnsapi.models import HOST, INSTANCE
+from dbaas_credentials.models import CredentialType
+from dbaas_dnsapi.models import HOST, INSTANCE, DatabaseInfraDNSList
 from dbaas_dnsapi.provider import DNSAPIProvider
 from dbaas_dnsapi.utils import add_dns_record
+from util import get_credentials_for, check_nslookup
 from base import BaseInstanceStep
 
 
@@ -9,6 +11,10 @@ class DNSStep(BaseInstanceStep):
     def __init__(self, instance):
         super(DNSStep, self).__init__(instance)
         self.provider = DNSAPIProvider
+
+    @property
+    def credentials(self):
+        return get_credentials_for(self.environment, CredentialType.DNSAPI)
 
     def do(self):
         raise NotImplementedError
@@ -114,3 +120,20 @@ class CreateDNS(DNSStep):
             databaseinfra=self.infra,
             ip=self.instance.address
         )
+
+
+class CheckIsReady(DNSStep):
+
+    def __unicode__(self):
+        return "Waiting for DNS..."
+
+    def do(self):
+        if self.instance.id != self.infra.instances.first().id:
+            return
+
+        check_dns = self.credentials.get_parameter_by_name('check_dns')
+        if str(check_dns).lower() != 'true':
+            return
+
+        for dns in DatabaseInfraDNSList.objects.filter(databaseinfra=self.infra):
+            check_nslookup(dns.dns, self.credentials.project)
