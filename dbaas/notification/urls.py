fdbf23e182627160c781511fbcb6643c14fef7aa
Elesbom <rafael.goncalves@corp.globo.com>
2017-09-26 11:41:03 -0300

    Change notification regex url

--- dbaas/notification/urls.py
+++ dbaas/notification/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns(
     url(r"^tasks_waiting/$", views.waiting_tasks_api, name="notification:tasks_waiting"),
     url(r"^database_tasks/(?P<database_id>\d+)/?$", views.database_tasks, name="notification:database_tasks"),
     # TODO: see why namespace notification: not work on templatetag {% url %}
-    url(r"^(?P<username>[\w\-\_\.]*)/user_tasks/?$", views.UserTasks.as_view(), name="notification_user_tasks"),
+    url(r"^(?P<username>.*)/user_tasks/?$", views.UserTasks.as_view(), name="notification_user_tasks"),
 
 )
