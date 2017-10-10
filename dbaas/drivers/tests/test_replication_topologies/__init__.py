e4df407d7678166cc606af17601ede96b5c8e3f1
Ricardo Dias <ricardo@ricardodias.org>
2017-10-10 11:59:59 -0300

    automatic tests

--- dbaas/drivers/tests/test_replication_topologies/__init__.py
+++ dbaas/drivers/tests/test_replication_topologies/__init__.py
@@ -220,6 +220,37 @@ class AbstractReplicationTopologySettingsTestCase(TestCase):
             )
         }]
 
+    def _get_reinstallvm_steps(self):
+        return [{
+            'Disable monitoring and alarms': (
+                'workflow.steps.util.zabbix.DisableAlarms',
+                'workflow.steps.util.db_monitor.DisableMonitoring',
+            ),
+        }] + [{
+            'Reinstall VM': (
+                'workflow.steps.util.vm.ChangeMaster',
+                'workflow.steps.util.database.Stop',
+                'workflow.steps.util.vm.Stop',
+                'workflow.steps.util.vm.ReinstallTemplate',
+                'workflow.steps.util.vm.Start',
+                'workflow.steps.util.vm.WaitingBeReady',
+                'workflow.steps.util.vm.UpdateOSDescription',
+                'workflow.steps.util.plan.Initialization',
+                'workflow.steps.util.plan.Configure',
+                'workflow.steps.util.pack.Configure',
+                'workflow.steps.util.database.Start',
+                'workflow.steps.util.database.CheckIsUp',
+            ),
+        }] + self._get_reinstallvm_steps_final()
+
+    def _get_reinstallvm_steps_final(self):
+        return [{
+            'Enabling monitoring and alarms': (
+                'workflow.steps.util.db_monitor.EnableMonitoring',
+                'workflow.steps.util.zabbix.EnableAlarms',
+            ),
+        }]
+
     @skip_unless_not_abstract
     def test_deploy_settings(self):
         self.assertEqual(
@@ -295,4 +326,11 @@ class AbstractReplicationTopologySettingsTestCase(TestCase):
         self.assertEqual(
             self._get_switch_write_instance_steps(),
             self.replication_topology.get_switch_write_instance_steps()
+        )
+
+    @skip_unless_not_abstract
+    def test_reinstallvm_settings(self):
+        self.assertEqual(
+            self._get_reinstallvm_steps(),
+            self.replication_topology.get_reinstallvm_steps()
         )
