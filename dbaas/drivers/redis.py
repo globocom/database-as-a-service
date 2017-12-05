0725af8385ae468c6f80237406d469c9967649f1
Elesbom <rafael.goncalves@corp.globo.com>
2017-12-05 17:57:15 -0200

    Add update_infra_instances_used_size for redis driver

--- dbaas/drivers/redis.py
+++ dbaas/drivers/redis.py
@@ -170,6 +170,27 @@ class Redis(BaseDriver):
 
         return dbs_names
 
+    def update_infra_instances_used_size(self):
+
+        result = {
+            'updated': [],
+            'error': []
+        }
+
+        for instance in self.get_database_instances():
+            with self.redis(instance=instance) as client:
+                if instance.status == Instance.ALIVE:
+                    database_info = client.info()
+                    instance.used_size_in_bytes = database_info.get(
+                        'used_memory', 0
+                    )
+                    instance.save()
+                    result['updated'].append(instance)
+                else:
+                    result['error'].append(instance)
+
+        return result
+
     def info(self):
         infra_status = DatabaseInfraStatus(
             databaseinfra_model=self.databaseinfra
