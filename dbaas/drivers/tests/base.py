6c8496436c539bb6dceea552966169e2343ec2f1
Elesbom <rafael.goncalves@corp.globo.com>
2017-12-08 14:04:40 -0200

    Refactor redis driver test

--- dbaas/drivers/tests/base.py
+++ dbaas/drivers/tests/base.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import os
-from unittest import TestCase
-from mock import MagicMock
+from mock import patch, MagicMock
 
 from django.conf import settings
+from django.test import TestCase
 
 from physical.tests import factory as factory_physical
 from physical.models import Instance
@@ -12,6 +12,7 @@ from logical.models import Database
 from drivers.mysqldb import MySQL
 from drivers.mongodb import MongoDB
 from drivers.redis import Redis
+from dbaas.tests.helpers import InstanceHelper, UsedAndTotalValidator
 
 
 class FakeDriverClient(object):
@@ -44,7 +45,7 @@ class FakeDriverClient(object):
                     {'command': lambda self, string: {'totalSize': 40}})()
 
 
-class BaseDriverTestCase(TestCase):
+class BaseDriverTest(object):
 
     host = None
     port = None
@@ -54,19 +55,24 @@ class BaseDriverTestCase(TestCase):
     instance_type = None
     driver_class = None
     driver_client_lookup = ''
+    instances_quantity = 1
+    instance_helper = InstanceHelper
 
     def setUp(self):
         host = self.host or '127.0.0.1'
         port = self.port or 3306
-        self.endpoint = "{}:{}".format(host, port)
+        self.infra_endpoint = '{}:{}'.format(host, port)
         self.databaseinfra = factory_physical.DatabaseInfraFactory(
-            password=self.db_password, endpoint=self.endpoint,
+            password=self.db_password, endpoint="{}:{}".format(host, port),
             engine__engine_type__name=self.engine_name, user=self.db_user
         )
-        self.instance = factory_physical.InstanceFactory(
-            databaseinfra=self.databaseinfra, port=port,
-            instance_type=self.instance_type, address=host
+        self.instances = self.instance_helper.create_instances_by_quant(
+            infra=self.databaseinfra, port=self.port, qt=self.instances_quantity,
+            total_size_in_bytes=0, used_size_in_bytes=0,
+            instance_type=self.instance_type
         )
+        self.instance = self.instances[0]
+        self.instance_endpoint = "{}:{}".format(self.instance.address, self.instance.port)
         self.driver = self.driver_class(databaseinfra=self.databaseinfra)
         self._driver_client = None
 
@@ -76,7 +82,6 @@ class BaseDriverTestCase(TestCase):
             self.databaseinfra.delete()
         if self._driver_client:
             self.driver_client.close()
-        # self.driver = self.databaseinfra = self._driver_client = None
 
     @property
     def driver_client(self):
@@ -88,53 +93,9 @@ class BaseDriverTestCase(TestCase):
         return self._driver_client
 
 
-class BaseUsedAndTotalTestCase(BaseDriverTestCase):
+class BaseMysqlDriverTestCase(BaseDriverTest, TestCase):
 
-    def setUp(self):
-        super(BaseUsedAndTotalTestCase, self).setUp()
-        self.instance.address = '127.0.0.1'
-        self.instance.save()
-        self.driver.check_instance_is_master = MagicMock(
-            side_effect=self._check_instance_is_master
-        )
-
-    def _check_instance_is_master(self, instance):
-
-        n = int(instance.address.split('.')[-1]) - 1
-
-        return n % 2 == 0
-
-    def _create_more_instances(self, qt=1, total_size_in_bytes=50,
-                               used_size_in_bytes=25):
-
-        def _create(n):
-            n += 2
-            return factory_physical.InstanceFactory(
-                databaseinfra=self.databaseinfra,
-                address='127.{0}.{0}.{0}'.format(n), port=self.port,
-                instance_type=self.instance_type,
-                total_size_in_bytes=total_size_in_bytes,
-                used_size_in_bytes=used_size_in_bytes
-            )
-
-        return map(_create, range(qt))
-
-    def _change_instance_type(self, instances, instance_type):
-        for instance in instances:
-            instance.instance_type = instance_type
-            instance.save()
-
-    def _validate_instances_sizes(self, instances=None, expected_used_size=40, expected_total_size=90):
-        if instances is None:
-            instances = self.databaseinfra.instances.filter(instance_type=self.instance_type)
-        for instance in instances:
-            self.assertEqual(instance.used_size_in_bytes, expected_used_size)
-            self.assertEqual(instance.total_size_in_bytes, expected_total_size)
-
-
-class BaseMysqlDriverTestCase(BaseDriverTestCase):
-
-    host = settings.DB_HOST
+    host = '127.0.0.1'
     port = 3306
     db_user = 'root'
     db_password = settings.DB_PASSWORD
@@ -144,7 +105,7 @@ class BaseMysqlDriverTestCase(BaseDriverTestCase):
     driver_client_lookup = '__mysql_client__'
 
 
-class BaseMongoDriverTestCase(BaseDriverTestCase):
+class BaseMongoDriverTestCase(BaseDriverTest, TestCase):
 
     host = os.getenv('TESTS_MONGODB_HOST', '127.0.0.1')
     port = os.getenv('TESTS_MONGODB_PORT', '27017')
@@ -154,7 +115,7 @@ class BaseMongoDriverTestCase(BaseDriverTestCase):
     driver_client_lookup = '__mongo_client__'
 
 
-class BaseRedisDriverTestCase(BaseDriverTestCase):
+class BaseRedisDriverTestCase(BaseDriverTest, TestCase):
 
     host = '127.0.0.1'
     port = settings.REDIS_PORT
@@ -163,3 +124,86 @@ class BaseRedisDriverTestCase(BaseDriverTestCase):
     instance_type = 4
     driver_class = Redis
     driver_client_lookup = '__redis_client__'
+
+
+class BaseSingleInstanceUpdateSizesTest(BaseDriverTest):
+
+    instances_quantity = 1
+    validator_helper = UsedAndTotalValidator
+
+    def setUp(self):
+        super(BaseSingleInstanceUpdateSizesTest, self).setUp()
+        self.driver.check_instance_is_master = MagicMock(
+            side_effect=self.instance_helper.check_instance_is_master
+        )
+
+    def test_instance_alive(self):
+        self.driver.databaseinfra.get_parameter_value_by_parameter_name = MagicMock(return_value=90)
+        result = self.driver.update_infra_instances_sizes()
+        self.validator_helper.instances_sizes(self.driver.get_database_instances())
+        self.assertListEqual(result['updated'], [self.instances[0]])
+        self.assertEqual(result['error'], [])
+
+    def test_instance_dead(self):
+        self.instance_helper.kill_instances(self.instances)
+        result = self.driver.update_infra_instances_sizes()
+        self.validator_helper.instances_sizes(
+            self.driver.get_database_instances(),
+            expected_used_size=0,
+            expected_total_size=0
+        )
+        self.assertListEqual(result['error'], [self.instances[0]])
+        self.assertEqual(result['updated'], [])
+
+
+class BaseHAInstanceUpdateSizesTest(BaseDriverTest):
+
+    validator_helper = UsedAndTotalValidator
+    instances_quantity = 4
+    secondary_instance_quantity = None
+    secondary_instance_type = None
+    dead_instance_quantity = 1
+
+    def setUp(self):
+        super(BaseHAInstanceUpdateSizesTest, self).setUp()
+        if self.secondary_instance_quantity is not None:
+            self.instance_helper.change_instances_type(
+                self.instances[-self.secondary_instance_quantity:],
+                self.secondary_instance_type
+            )
+        self.driver.databaseinfra.get_parameter_value_by_parameter_name = MagicMock(return_value=90)
+
+    def test_instance_alive(self):
+        result = self.driver.update_infra_instances_sizes()
+        database_instances = self.driver.get_database_instances()
+        self.validator_helper.instances_sizes(
+            database_instances
+        )
+
+        updated_instances = result['updated']
+
+        self.assertListEqual(updated_instances, database_instances)
+        self.assertEqual(result['error'], [])
+
+    def test_instance_dead(self):
+        self.instance_helper.kill_instances(self.instances[self.dead_instance_quantity:])
+        result = self.driver.update_infra_instances_sizes()
+
+        all_instances = self.databaseinfra.instances.filter(
+            instance_type=self.instance_type
+        )
+        alive_instances = all_instances.filter(status=Instance.ALIVE)
+        dead_instances = all_instances.exclude(status=Instance.ALIVE)
+
+        self.validator_helper.instances_sizes(alive_instances)
+        self.validator_helper.instances_sizes(
+            dead_instances,
+            expected_used_size=0,
+            expected_total_size=0
+        )
+
+        updated_instances = result['updated']
+        error_instances = result['error']
+
+        self.assertListEqual(error_instances, list(dead_instances))
+        self.assertListEqual(updated_instances, list(alive_instances))
