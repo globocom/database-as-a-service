7c2512b7201592196c7227c7c12a40e16284adcf
Ricardo Dias <ricardo@ricardodias.org>
2018-01-10 09:55:15 -0200

    Add redis parameters

--- dbaas/physical/configurations.py
+++ dbaas/physical/configurations.py
@@ -255,6 +255,36 @@ class ConfigurationRedis(ConfigurationBase):
         return self.get_parameter(parameter_name, default)
 
     @property
+    def repl_ping_slave_period(self):
+        parameter_name = inspect.stack()[0][3]
+        default = 10
+        return self.get_parameter(parameter_name, default)
+
+    @property
+    def repl_timeout(self):
+        parameter_name = inspect.stack()[0][3]
+        default = 60
+        return self.get_parameter(parameter_name, default)
+
+    @property
+    def repl_disable_tcp_nodelay(self):
+        parameter_name = inspect.stack()[0][3]
+        default = 'no'
+        return self.get_parameter(parameter_name, default)
+
+    @property
+    def repl_backlog_size(self):
+        parameter_name = inspect.stack()[0][3]
+        default = 1048576
+        return self.get_parameter(parameter_name, default)
+
+    @property
+    def repl_backlog_ttl(self):
+        parameter_name = inspect.stack()[0][3]
+        default = 3600
+        return self.get_parameter(parameter_name, default)
+
+    @property
     def cluster_enabled(self):
         return 'no'
 
