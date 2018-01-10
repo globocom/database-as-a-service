d2cc4ad4dfc32de4f8a379dae24d045a8bde3ac3
Ricardo Dias <ricardo@ricardodias.org>
2018-01-10 17:56:32 -0200

    get_configuration and set_configuration for Redis special parameter

--- dbaas/drivers/redis.py
+++ dbaas/drivers/redis.py
@@ -302,10 +302,31 @@ class Redis(BaseDriver):
             )
 
         with self.redis(instance) as client:
-            return client.config_get()
+            config = client.config_get()
+
+            if 'client-output-buffer-limit' in config:
+                config_COBL = config['client-output-buffer-limit']
+                config_COBL_normal = config_COBL.split("normal ")[1].split(" slave")[0]
+                config_COBL_slave = config_COBL.split("slave ")[1].split(" pubsub")[0]
+                config_COBL_pubsub = config_COBL.split("pubsub ")[1]
+                config['client-output-buffer-limit-normal'] = config_COBL_normal
+                config['client-output-buffer-limit-slave'] = config_COBL_slave
+                config['client-output-buffer-limit-pubsub'] = config_COBL_pubsub
+
+            return config
 
     def set_configuration(self, instance, name, value):
         with self.redis(instance) as client:
+            if name == 'client-output-buffer-limit-normal':
+                name = 'client-output-buffer-limit'
+                value = 'normal {}'.format(value)
+            elif name == 'client-output-buffer-limit-slave':
+                name = 'client-output-buffer-limit'
+                value = 'slave {}'.format(value)
+            elif name == 'client-output-buffer-limit-pubsub':
+                name = 'client-output-buffer-limit'
+                value = 'pubsub {}'.format(value)
+
             client.config_set(name, value)
 
     def get_database_process_name(self):
