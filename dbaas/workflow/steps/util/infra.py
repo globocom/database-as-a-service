486866e07cb4087e8d4b4df7c29ca35e7b207c29
Elesbom <rafael.goncalves@corp.globo.com>
2018-01-16 14:10:47 -0200

    Remove useless if

--- dbaas/workflow/steps/util/infra.py
+++ dbaas/workflow/steps/util/infra.py
@@ -38,9 +38,8 @@ class Offering(Update):
         self.infra_offering.offering = offering
         self.infra_offering.save()
 
-        if self.instance.is_database:
-            self.instance.hostname.offering = offering
-            self.instance.hostname.save()
+        self.instance.hostname.offering = offering
+        self.instance.hostname.save()
 
     def do(self):
         self.change_infra_offering(self.target_offering)
