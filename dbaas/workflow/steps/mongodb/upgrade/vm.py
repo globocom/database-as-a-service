05798cdf8818365b77e7954cc9cc82712e330e6f
otherpirate <mauro_murari@hotmail.com>
2018-01-22 09:51:32 -0200

    Restore new upgrade mongodb generic step

---
+++ dbaas/workflow/steps/mongodb/upgrade/vm.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from util import build_context_script, exec_remote_command
+from workflow.steps.util.vm import VmStep
+from workflow.steps.util import test_bash_script_error
+from workflow.steps.mongodb.util import build_change_release_alias_script
+
+
+class ChangeBinaryBase(VmStep):
+
+    def __script(self):
+        return test_bash_script_error() + build_change_release_alias_script()
+
+
+    def change_binary(self, release):
+        context_dict = {
+            'TARGET_PATH': '/usr/local/',
+            'MONGODB_RELEASE_FOLDER': 'mongodb-linux-x86_64-rhel62-{}'.format(
+                release
+            ),
+        }
+
+        script = build_context_script(
+            context_dict, self.__script()
+        )
+
+        output = {}
+        return_code = exec_remote_command(
+            self.host.address, self.host_cs.vm_user, self.host_cs.vm_password,
+            script, output
+        )
+        if return_code != 0:
+            raise EnvironmentError(
+                'Could change binary {}: {}'.format(return_code, output)
+            )
+
+
+class ChangeBinaryTo32(ChangeBinaryBase):
+
+    def __unicode__(self):
+        return "Changing binary to 3.2..."
+
+    def do(self):
+        self.change_binary('3.2.11')
+
+
+class ChangeBinaryTo34(ChangeBinaryBase):
+
+    def __unicode__(self):
+        return "Changing binary to 3.4..."
+
+    def do(self):
+        self.change_binary('3.4.1')
