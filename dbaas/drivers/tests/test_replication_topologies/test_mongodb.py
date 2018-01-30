a70065ada6ec0ac510d030a7ed103e515d7cfc4b
otherpirate <mauro_murari@hotmail.com>
2018-01-30 19:01:20 -0200

    MongoDB ReplicaSet use database create with retry

--- dbaas/drivers/tests/test_replication_topologies/test_mongodb.py
+++ dbaas/drivers/tests/test_replication_topologies/test_mongodb.py
@@ -155,3 +155,40 @@ class TestMongoDBReplicaset(AbstractBaseMondodbTestCase):
 
             )
         }] + self._get_change_parameter_steps_final()
+
+    def _get_deploy_settings(self):
+        return [{
+            'Creating virtual machine': (
+                'workflow.steps.util.vm.CreateVirtualMachineNewInfra',
+            )}, {
+            'Creating dns': (
+                'workflow.steps.util.dns.CreateDNS',
+            )}, {
+            'Creating disk': (
+                'workflow.steps.util.disk.CreateExport',
+            )}, {
+            'Waiting VMs': (
+                'workflow.steps.util.vm.WaitingBeReady',
+                'workflow.steps.util.vm.UpdateOSDescription'
+            )}, {
+            'Configuring database': (
+                'workflow.steps.util.plan.InitializationForNewInfra',
+                'workflow.steps.util.plan.ConfigureForNewInfra',
+                'workflow.steps.util.database.Start',
+            )}, {
+            'Check Database': (
+                'workflow.steps.util.plan.StartReplicationNewInfra',
+                'workflow.steps.util.database.CheckIsUp',
+                'workflow.steps.util.infra.UpdateEndpoint',
+            )}, {
+            'Check DNS': (
+                'workflow.steps.util.dns.CheckIsReady',
+            )}, {
+            'Creating Database': (
+                'workflow.steps.util.database.Create',
+            )}, {
+            'Creating monitoring and alarms': (
+                'workflow.steps.util.zabbix.CreateAlarms',
+                'workflow.steps.util.db_monitor.CreateInfraMonitoring',
+            )
+        }]
