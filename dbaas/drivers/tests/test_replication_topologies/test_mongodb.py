35b755f5f0708bc97f3533704e20fd3e11e7b86a
otherpirate <mauro_murari@hotmail.com>
2018-01-31 12:06:58 -0200

    MongoDB ReplicaSet restore using retry

--- dbaas/drivers/tests/test_replication_topologies/test_mongodb.py
+++ dbaas/drivers/tests/test_replication_topologies/test_mongodb.py
@@ -193,3 +193,40 @@ class TestMongoDBReplicaset(AbstractBaseMondodbTestCase):
                 'workflow.steps.util.db_monitor.CreateInfraMonitoring',
             )
         }]
+
+    def get_restore_snapshot_steps(self):
+        return [{
+            'Disable monitoring': (
+                'workflow.steps.util.zabbix.DisableAlarms',
+                'workflow.steps.util.db_monitor.DisableMonitoring',
+            )}, {
+            'Restoring': (
+                'workflow.steps.util.disk.RestoreSnapshot',
+            )}, {
+            'Stopping datbase': (
+                'workflow.steps.util.database.Stop',
+                'workflow.steps.util.database.CheckIsDown',
+            )}, {
+            'Configuring': (
+                'workflow.steps.util.disk.AddDiskPermissionsRestoredDisk',
+                'workflow.steps.util.disk.UnmountOldestExportRestore',
+                'workflow.steps.util.disk.MountNewerExportRestore',
+                'workflow.steps.util.disk.ConfigureFstabRestore',
+                'workflow.steps.util.disk.CleanDataMongoDB',
+                'workflow.steps.util.plan.ConfigureRestore',
+            )}, {
+            'Starting database': (
+                'workflow.steps.util.database.Start',
+            )}, {
+            'Check database': (
+                'workflow.steps.util.database.CheckIsUp',
+            )}, {
+            'Old data': (
+                'workflow.steps.util.disk.BackupRestore',
+                'workflow.steps.util.disk.UpdateRestore',
+            )}, {
+            'Enabling monitoring': (
+                'workflow.steps.util.db_monitor.EnableMonitoring',
+                'workflow.steps.util.zabbix.EnableAlarms',
+            )
+        }]
