fd605a7e1ecc5eee06f17bc4937eae46bb4f8d1b
anitavincent <paes.anita@gmail.com>
2018-04-11 14:40:16 -0300

    Add new page detailing offered plans

---
+++ dbaas/physical/templatetags/filter_plans.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('plans/plans_table.html')
+def show_plans(environment, engine):
+
+    active_plans = environment.active_plans().filter(engine=engine)
+
+    return {
+        'selected_plans': active_plans,
+        'env_name': environment.name,
+        'eng_name': engine.full_name
+    }
--- dbaas/physical/views.py
+++ dbaas/physical/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 from django.http import HttpResponse
-from physical.models import Environment
+from django.shortcuts import render_to_response
+from physical.models import Environment, Plan, Engine
 
 def engines_by_env(self, environment_id):
     environment = Environment.objects.get(id=environment_id)
@@ -16,3 +17,19 @@ def engines_by_env(self, environment_id):
         "engines": engines
     })
     return HttpResponse(response_json, content_type="application/json")
+
+def plans_details(self):
+
+    context = {}
+
+
+    all_environments = Environment.objects.all()
+    all_engines = Engine.objects.all()
+
+    context["environments"] = all_environments
+    context["engines"] = all_engines
+
+    return render_to_response(
+        "plans/plans_details.html",
+        context
+    )
