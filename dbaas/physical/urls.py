3fa2bcafa0353cac7fdbbf2506cd7d2469b34e8b
otherpirate <mauro_murari@hotmail.com>
2018-04-30 14:30:34 -0300

    Code review: pep8, remove useless code

--- dbaas/physical/urls.py
+++ dbaas/physical/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 
+
 urlpatterns = patterns(
     'physical.views',
     url(r'^engines_by_env/(?P<environment_id>\d+)/$', "engines_by_env"),
