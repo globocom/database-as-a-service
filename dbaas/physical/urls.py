fb3591545d6c7fcdf835606d0a02bc9ad54696fe
anitavincent <paes.anita@gmail.com>
2018-04-30 13:41:21 -0300

    Remove unwanted layout version

--- dbaas/physical/urls.py
+++ dbaas/physical/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'physical.views',
     url(r'^engines_by_env/(?P<environment_id>\d+)/$', "engines_by_env"),
-    url(r'^plans_details/(?P<template_id>\d+)/$', "plans_details"),
+    url(r'^plans_details/$', "plans_details"),
 )
