9b629576f68d2340eb6b123ccbbf0cf39f32f16d
Ricardo Dias <ricardo@ricardodias.org>
2018-07-16 13:39:23 -0300

    remove mount from initialization - mount now is a step

--- dbaas/physical/scripts/mongodb_initialization.sh
+++ dbaas/physical/scripts/mongodb_initialization.sh
@@ -9,31 +9,6 @@ die_if_error()
     fi
 }
 
-mountdatadisk()
-{
-    if [ "{{DATABASERULE}}" != "ARBITER" ]; then
-        echo ""; echo $(date "+%Y-%m-%d %T") "- Mounting data disk"
-        sed '/data/d' "/etc/fstab"
-        echo "{{EXPORTPATH}}    /data nfs defaults,bg,intr,nolock 0 0" >> /etc/fstab
-        die_if_error "Error setting fstab"
-
-        if mount | grep /data > /dev/null; then
-            umount /data
-            die_if_error "Error umount /data"
-        fi
-        mount /data
-        die_if_error "Error mounting /data"
-
-        wcl=$(mount -l | grep data | grep nfs | wc -l)
-        if [ "$wcl" -eq 0 ]
-        then
-            echo "Could not mount /data"
-            exit 100
-        fi
-    else
-        echo ""; echo $(date "+%Y-%m-%d %T") "- Arbiter does not mount data disk"
-    fi
-}
 
 movedatabase()
 {
@@ -59,10 +34,6 @@ filerpermission()
     chmod g+x /data
     die_if_error "Error changing datadir permission"
 }
-
-if [ "{{DATABASERULE}}" != "ARBITER" ]; then
-    mountdatadisk
-fi
 
 if [ "{{MOVE_DATA}}" != "True" ]; then
   movedatabase
--- dbaas/physical/scripts/mysql_initialization.sh
+++ dbaas/physical/scripts/mysql_initialization.sh
@@ -9,28 +9,6 @@ die_if_error()
     fi
 }
 
-mountdatadisk()
-{
-    echo ""; echo $(date "+%Y-%m-%d %T") "- Mounting data disk"
-    sed '/data/d' "/etc/fstab"
-    echo "{{EXPORTPATH}}    /data nfs defaults,bg,intr,nolock 0 0" >> /etc/fstab
-    die_if_error "Error setting fstab"
-
-    if mount | grep /data > /dev/null; then
-        umount /data
-        die_if_error "Error umount /data"
-    fi
-    mount /data
-    die_if_error "Error mounting /data"
-
-    wcl=$(mount -l | grep data | grep nfs | wc -l)
-    if [ "$wcl" -eq 0 ]
-    then
-        echo "Could not mount /data"
-        exit 100
-    fi
-}
-
 movedatabase()
 {
     echo ""; echo $(date "+%Y-%m-%d %T") "- Moving the database"
@@ -47,8 +25,6 @@ filepermission()
     chmod g+x /data
     die_if_error "Error changing datadir permission"
 }
-
-mountdatadisk
 
 if [ "{{MOVE_DATA}}" != "True" ]; then
     movedatabase
