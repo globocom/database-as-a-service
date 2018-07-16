2f8992ba77888f4e6489d722b2934bcf4b5caea7
Ricardo Dias <ricardo@ricardodias.org>
2018-07-16 13:39:23 -0300

    remove mount from initialization - mount now is a step

--- dbaas/physical/scripts/redis_initialization.sh
+++ dbaas/physical/scripts/redis_initialization.sh
@@ -9,26 +9,8 @@ die_if_error()
     fi
 }
 
-mountdatadisk()
+filerpermission()
 {
-    echo ""; echo $(date "+%Y-%m-%d %T") "- Mounting data disk"
-    sed '/data/d' "/etc/fstab"
-    echo "{{EXPORTPATH}}    /data nfs defaults,bg,intr,nolock 0 0" >> /etc/fstab
-    die_if_error "Error setting fstab"
-
-    if mount | grep /data > /dev/null; then
-        umount /data
-        die_if_error "Error umount /data"
-    fi
-    mount /data
-    die_if_error "Error mounting /data"
-
-    wcl=$(mount -l | grep data | grep nfs | wc -l)
-    if [ "$wcl" -eq 0 ]
-    then
-        echo "Could not mount /data"
-        exit 100
-    fi
     mkdir -p /data/data
     die_if_error "Error creating data dir"
     chown redis:redis /data
@@ -41,26 +23,6 @@ mountdatadisk()
     die_if_error "Error changing datadir permission"
 }
 
-mountdatadisk_sentinel()
-{
-    echo ""; echo $(date "+%Y-%m-%d %T") "- Mounting data disk"
-    mkdir -p /data/data
-    die_if_error "Error creating data dir"
-    chown redis:redis /data
-    die_if_error "Error changing datadir permission"
-    chown -R redis:redis /data/data
-    die_if_error "Error changing datadir permission"
-    chmod g+r /data
-    die_if_error "Error changing datadir permission"
-    chmod g+x /data
-    die_if_error "Error changing datadir permission"
-}
-
-
-{% if ONLY_SENTINEL %}
-    mountdatadisk_sentinel
-{% else %}
-    mountdatadisk
-{% endif %}
+filerpermission
 
 exit 0
