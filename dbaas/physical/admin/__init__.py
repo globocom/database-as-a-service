68349aa18226b208a579b83b29a85e6803cca39f
Elesbom <rafael.goncalves@corp.globo.com>
2018-07-19 17:53:15 -0300

    Create Offering Admin

--- dbaas/physical/admin/__init__.py
+++ dbaas/physical/admin/__init__.py
@@ -10,12 +10,14 @@ from .environment import EnvironmentAdmin
 from .replication_topology import ReplicationTopologyAdmin
 from .disk_offering import DiskOfferingAdmin
 from .parameter import ParameterAdmin
+from .offering import OfferingAdmin
 
 admin.site.register(models.DatabaseInfra, DatabaseInfraAdmin)
 admin.site.register(models.Engine, EngineAdmin)
 admin.site.register(models.EngineType, EngineTypeAdmin)
 admin.site.register(models.Plan, PlanAdmin)
 admin.site.register(models.Host, HostAdmin)
+admin.site.register(models.Offering, OfferingAdmin)
 admin.site.register(models.Environment, EnvironmentAdmin)
 admin.site.register(models.ReplicationTopology, ReplicationTopologyAdmin)
 admin.site.register(models.DiskOffering, DiskOfferingAdmin)
