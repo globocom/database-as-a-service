cc7a8da30e071ba3b9c167ffea85c6b7803c2117
otherpirate <mauro_murari@hotmail.com>
2018-07-27 15:00:15 -0300

    Remove OLD restore_snapshot method

--- dbaas/drivers/replication_topologies/base.py
+++ dbaas/drivers/replication_topologies/base.py
@@ -53,18 +53,7 @@ class BaseTopology(object):
         )}]
 
     def get_restore_snapshot_steps(self):
-        return (
-            'workflow.steps.util.restore_snapshot.restore_snapshot.RestoreSnapshot',
-            'workflow.steps.util.restore_snapshot.grant_nfs_access.GrantNFSAccess',
-            'workflow.steps.util.restore_snapshot.stop_database.StopDatabase',
-            'workflow.steps.util.restore_snapshot.umount_data_volume.UmountDataVolume',
-            'workflow.steps.util.restore_snapshot.update_fstab.UpdateFstab',
-            'workflow.steps.util.restore_snapshot.mount_data_volume.MountDataVolume',
-            'workflow.steps.util.restore_snapshot.start_database.StartDatabase',
-            'workflow.steps.util.restore_snapshot.make_export_snapshot.MakeExportSnapshot',
-            'workflow.steps.util.restore_snapshot.update_dbaas_metadata.UpdateDbaaSMetadata',
-            'workflow.steps.util.restore_snapshot.clean_old_volumes.CleanOldVolumes',
-        )
+        raise NotImplementedError
 
     def get_upgrade_steps_initial_description(self):
         return 'Disable monitoring and alarms'
