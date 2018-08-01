050c720a03ea4af99aa10b8cde39cb7eabc0842f
Elesbom <rafael.goncalves@corp.globo.com>
2018-08-01 13:07:40 -0300

    Fix migrate and put query on the end

--- dbaas/migrate.py
+++ dbaas/migrate.py
@@ -39,7 +39,7 @@ for cs_off in CloudStackOffering.objects.all():
         off = Offering.objects.get(**params)
     except Offering.DoesNotExist:
         params.update({
-            'name': cs_off.name.replace(" (weaker)", "")
+            'name': cs_off.name.replace("- weaker", "")
         })
         off = Offering.objects.create(**params)
 
@@ -50,3 +50,17 @@ for plan_attr in PlanAttr.objects.all():
     plan = plan_attr.plan
     migrate_off(plan_attr.get_stronger_offering(), plan)
     migrate_off(plan_attr.get_weaker_offering(), plan, strong=False)
+
+print """
+Exec this queries on PROD
+ALTER TABLE physical_host ADD cs_offering VARCHAR(255);
+
+UPDATE physical_host AS ph
+  LEFT JOIN  `dbaas_cloudstack_cloudstackoffering` as cso ON ph.offering_id = cso.id
+  SET cs_offering = CONCAT(cso.cpus, "-", cso.memory_size_mb);
+
+UPDATE physical_host AS ph, `physical_offering` AS po
+SET ph.offering_id = po.id
+WHERE po.cpus = SUBSTRING_INDEX(ph.cs_offering, '-', 1) AND po.memory_size_mb = SUBSTRING_INDEX(ph.cs_offering, '-', -1);
+
+"""
