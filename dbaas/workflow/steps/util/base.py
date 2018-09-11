630ec3fbe572ab62d6e9ab0352273a3ec795b8e2
elesbom <rafael.goncalves@corp.globo.com>
2018-09-11 16:36:12 -0300

    Create ACLFromHell client

--- dbaas/workflow/steps/util/base.py
+++ dbaas/workflow/steps/util/base.py
@@ -155,7 +155,6 @@ class HostProviderClient(object):
             )
         return self._credential
 
-
     def get_vm_by_host(self, host):
         api_host_url = '/{}/{}/host/{}'.format(
             self.credential.project,
@@ -179,3 +178,123 @@ class HostProviderClient(object):
         if resp.ok:
             data = resp.json()
             return data.get('offering_id')
+
+
+class ACLFromHellClient(object):
+
+    def __init__(self, env):
+        self.environment = env
+        self._credential = None
+
+    @property
+    def credential(self):
+        if not self._credential:
+            try:
+                self._credential = get_credentials_for(
+                    self.environment, CredentialType.ACLFROMHELL
+                )
+            except IndexError:
+                raise Exception(
+                    "Credential ACLFROMHELL for env {} not found".format(
+                        self.environment.name
+                    )
+                )
+
+        return self._credential
+
+    def _request(self, action, url, **kw):
+        return action(
+            url,
+            auth=(self.credential.user, self.credential.password),
+            **kw
+        )
+
+    def get_rule(self, database, app_name=None):
+        params = {
+            'metadata.owner': 'dbaas',
+            'metadata.service-name': self.credential.project,
+            'metadata.instance-name': database.name,
+        }
+        if app_name:
+            params.update({'source.tsuruapp.appname': app_name})
+
+        LOG.debug("Tsuru get rule for {} params:{}".format(
+            database.name, params))
+
+        return self._request(
+            requests.get,
+            self.credential.endpoint,
+            params=params,
+        )
+
+    def add_acl(self, database, app_name, hostname):
+        infra = database.infra
+        driver = infra.get_driver()
+
+        payload = {
+            "source": {
+                "tsuruapp": {
+                    "appname": app_name
+                }
+            },
+            "destination": {
+                "externaldns": {
+                    "name": hostname,
+                    "ports": map(
+                        lambda p: {
+                            'protocol': 'tcp',
+                            'port': p
+                        },
+                        driver.ports
+                    )
+                }
+            },
+            "target": "accept",
+            "metadata": {
+                'owner': 'dbaas',
+                "service-name": self.credential.project,
+                "instance-name": database.name
+            }
+        }
+
+        LOG.debug("Tsuru Add ACL: payload for host {}:{}".format(
+            hostname, payload))
+        resp = self._request(
+            requests.post,
+            self.credential.endpoint,
+            json=payload,
+        )
+        if not resp.ok:
+            msg = "Error bind {} database on {} environment: {}".format(
+                database.name, self.environment, resp.content
+            )
+            LOG.debug(msg)
+        LOG.debug("Tsuru Add ACL Status for host {}: {}".format(
+            hostname, resp.status_code
+        ))
+
+        return resp
+
+    def remove_acl(self, database, app_name):
+
+        resp = self.get_rule(database, app_name)
+        if not resp.ok:
+            msg = "Rule not found for {}.".format(
+                database.name)
+            LOG.debug(msg)
+
+        rules = resp.json()
+        for rule in rules:
+            rule_id = rule.get('RuleID')
+            host = rule.get('Destination', {}).get('ExternalDNS', {}).get('Name')
+            if rule_id:
+                LOG.debug('Tsuru Unbind App removing rule for {}'.format(host))
+                resp = self._request(
+                    requests.delete,
+                    '{}/{}'.format(self.credential.endpoint, rule_id)
+                )
+                if not resp.ok:
+                    msg = "Error on delete rule {} for {}.".format(
+                        rule_id, host)
+                    LOG.debug(msg)
+        return None
