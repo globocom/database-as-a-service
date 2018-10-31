47a0d4bb37cec737d516718eb04985904cc0db91
Ricardo Dias <ricardo@ricardodias.org>
2018-10-31 08:44:41 -0300

    Add metric_collector field

--- dbaas/physical/models.py
+++ dbaas/physical/models.py
@@ -211,6 +211,9 @@ class Script(BaseModel):
         max_length=300, help_text="File path", null=True, blank=True
     )
 
+    metric_collector = models.CharField(
+        max_length=300, help_text="File path", null=True, blank=True)
+
     def _get_content(self, file_name):
         path = file_name
         if not os.path.exists(path):
@@ -236,6 +239,9 @@ class Script(BaseModel):
     def start_replication_template(self):
         return self._get_content(self.start_replication)
 
+    @property
+    def metric_collector_template(self):
+        return self._get_content(self.metric_collector)
 
 class ReplicationTopology(BaseModel):
 
