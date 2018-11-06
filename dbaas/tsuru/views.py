e35256b25fe47b22e670c4619dba6660205fd622
elesbom <rafael.goncalves@corp.globo.com>
2018-11-06 16:12:13 -0200

    Add Action put to service remove

--- dbaas/tsuru/views.py
+++ dbaas/tsuru/views.py
@@ -467,6 +467,59 @@ class ServiceRemove(APIView):
     renderer_classes = (JSONRenderer, JSONPRenderer)
     model = Database
 
+    def put(self, request, database_name, format=None):
+        data = request.DATA
+        user = data['user']
+        team = data['team']
+        plan = data['plan']
+        env = get_url_env(request)
+
+        UserMiddleware.set_current_user(request.user)
+        env = get_url_env(request)
+        try:
+            database = get_database(database_name, env)
+        except IndexError as e:
+            msg = "Database id provided does not exist {} in {}.".format(
+                database_name, env)
+            return log_and_response(
+                msg=msg, e=e, http_status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        try:
+            dbaas_user = AccountUser.objects.get(email=user)
+        except ObjectDoesNotExist as e:
+            msg = "User does not exist."
+            return log_and_response(
+                msg=msg, e=e, http_status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        except MultipleObjectsReturned as e:
+            msg = "There are multiple user for {} email.".format(user)
+            return log_and_response(
+                msg=msg, e=e, http_status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+        try:
+            dbaas_team = Team.objects.get(name=team)
+        except ObjectDoesNotExist as e:
+            msg = "Team does not exist."
+            return log_and_response(
+                msg=msg, e=e, http_status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+        try:
+            dbaas_user.team_set.get(name=dbaas_team.name)
+        except ObjectDoesNotExist as e:
+            msg = "The user is not on {} team.".format(dbaas_team.name)
+            return log_and_response(
+                msg=msg, e=e, http_status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+        LOG.debug('Tsuru Service Remove plan {}'.format(plan))
+
+        database.team = dbaas_team
+        database.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     def delete(self, request, database_name, format=None):
         UserMiddleware.set_current_user(request.user)
         env = get_url_env(request)
