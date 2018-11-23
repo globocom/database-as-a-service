13d460aea6fcefcb4d7250afc21e722249b11d06
otherpirate <mauro_murari@hotmail.com>
2018-11-23 11:45:33 -0200

    Add step instance not eligible to be master in Redis HA topologies

--- dbaas/drivers/replication_topologies/redis.py
+++ dbaas/drivers/replication_topologies/redis.py
@@ -266,48 +266,52 @@ class RedisSentinel(BaseRedis):
             )
         }]
 
+    def get_host_migrate_steps_cleaning_up(self):
+        return (
+            'workflow.steps.util.volume_provider.DestroyOldEnvironment',
+            'workflow.steps.util.host_provider.DestroyVirtualMachineMigrate',
+            'workflow.steps.redis.upgrade.sentinel.ResetAllSentinel',
+        )
+
+    def get_base_host_migrate_steps(self):
+        return (
+            'workflow.steps.util.vm.ChangeMaster',
+            'workflow.steps.util.database.CheckIfSwitchMaster',
+            'workflow.steps.util.host_provider.CreateVirtualMachineMigrate',
+            'workflow.steps.util.volume_provider.NewVolume',
+            'workflow.steps.util.vm.WaitingBeReady',
+            'workflow.steps.util.vm.UpdateOSDescription',
+            'workflow.steps.util.volume_provider.MountDataVolume',
+            'workflow.steps.util.plan.Initialization',
+            'workflow.steps.util.plan.Configure',
+            'workflow.steps.util.database.Start',
+            'workflow.steps.util.database.CheckIsUp',
+            'workflow.steps.util.vm.CheckAccessToMaster',
+            'workflow.steps.util.vm.CheckAccessFromMaster',
+            'workflow.steps.util.acl.ReplicateAclsMigrate',
+            'workflow.steps.redis.upgrade.sentinel.ResetAllSentinel',
+            'workflow.steps.util.database.SetSlave',
+            'workflow.steps.util.database.WaitForReplication',
+            'workflow.steps.redis.horizontal_elasticity.database.SetNotEligible',
+            'workflow.steps.util.dns.ChangeEndpoint',
+            'workflow.steps.util.dns.CheckIsReady',
+            'workflow.steps.util.zabbix.DestroyAlarms',
+            'workflow.steps.util.zabbix.CreateAlarms',
+            'workflow.steps.util.disk.ChangeSnapshotOwner',
+        )
+
     def get_host_migrate_steps(self):
         return [{
-            'Changing master': (
-                'workflow.steps.util.vm.ChangeMaster',
-                'workflow.steps.util.database.CheckIfSwitchMaster',
-            )}, {
-            'Creating new infra': (
-                'workflow.steps.util.host_provider.CreateVirtualMachineMigrate',
-                'workflow.steps.util.volume_provider.NewVolume',
-                'workflow.steps.util.vm.WaitingBeReady',
-                'workflow.steps.util.vm.UpdateOSDescription',
-                'workflow.steps.util.volume_provider.MountDataVolume',
-            )}, {
-            'Configuring new host': (
-                'workflow.steps.util.plan.Initialization',
-                'workflow.steps.util.plan.Configure',
-                'workflow.steps.util.database.Start',
-                'workflow.steps.util.database.CheckIsUp',
-            )}, {
-            'Checking access': (
-                'workflow.steps.util.vm.CheckAccessToMaster',
-                'workflow.steps.util.vm.CheckAccessFromMaster',
-                'workflow.steps.util.acl.ReplicateAclsMigrate',
-            )}, {
-            'Configuring sentinel': (
-                'workflow.steps.redis.upgrade.sentinel.ResetAllSentinel',
-                'workflow.steps.util.database.SetSlave',
-                'workflow.steps.util.database.WaitForReplication',
-            )}, {
-            'Configuring DNS': (
-                'workflow.steps.util.dns.ChangeEndpoint',
-                'workflow.steps.util.dns.CheckIsReady',
-            )}, {
-            'Configure Monitors': (
-                'workflow.steps.util.zabbix.DestroyAlarms',
-                'workflow.steps.util.zabbix.CreateAlarms',
-            )}, {
-            'Cleaning up': (
-                'workflow.steps.util.disk.ChangeSnapshotOwner',
-                'workflow.steps.util.host_provider.DestroyVirtualMachineMigrate',
-                'workflow.steps.redis.upgrade.sentinel.ResetAllSentinel',
-            )
+            'Migrating':
+                self.get_base_host_migrate_steps() +
+                self.get_host_migrate_steps_cleaning_up()
+        }]
+
+    def get_database_migrate_steps(self):
+        return [{
+            'Migrating': self.get_base_host_migrate_steps()
+        }, {
+            'Cleaning up': self.get_host_migrate_steps_cleaning_up()
         }]
 
 
@@ -414,48 +418,52 @@ class RedisCluster(BaseRedis):
             )
         }]
 
+    def get_host_migrate_steps_cleaning_up(self):
+        return (
+            'workflow.steps.redis.cluster.RemoveNode',
+            'workflow.steps.redis.cluster.CheckClusterStatus',
+            'workflow.steps.util.volume_provider.DestroyOldEnvironment',
+            'workflow.steps.util.host_provider.DestroyVirtualMachineMigrate',
+        )
+
+    def get_base_host_migrate_steps(self):
+        return (
+            'workflow.steps.util.vm.ChangeMaster',
+            'workflow.steps.util.database.CheckIfSwitchMaster',
+            'workflow.steps.util.host_provider.CreateVirtualMachineMigrate',
+            'workflow.steps.util.volume_provider.NewVolume',
+            'workflow.steps.util.vm.WaitingBeReady',
+            'workflow.steps.util.vm.UpdateOSDescription',
+            'workflow.steps.util.volume_provider.MountDataVolume',
+            'workflow.steps.util.plan.Initialization',
+            'workflow.steps.util.plan.Configure',
+            'workflow.steps.util.database.Start',
+            'workflow.steps.util.database.CheckIsUp',
+            'workflow.steps.util.vm.CheckAccessToMaster',
+            'workflow.steps.util.vm.CheckAccessFromMaster',
+            'workflow.steps.util.acl.ReplicateAclsMigrate',
+            'workflow.steps.redis.cluster.AddSlaveNode',
+            'workflow.steps.redis.horizontal_elasticity.database.SetNotEligible',
+            'workflow.steps.redis.cluster.CheckClusterStatus',
+            'workflow.steps.util.dns.ChangeEndpoint',
+            'workflow.steps.util.dns.CheckIsReady',
+            'workflow.steps.util.zabbix.DestroyAlarms',
+            'workflow.steps.util.zabbix.CreateAlarms',
+            'workflow.steps.util.disk.ChangeSnapshotOwner',
+        )
+
     def get_host_migrate_steps(self):
         return [{
-            'Changing master': (
-                'workflow.steps.util.vm.ChangeMaster',
-                'workflow.steps.util.database.CheckIfSwitchMaster',
-            )}, {
-            'Creating new infra': (
-                'workflow.steps.util.host_provider.CreateVirtualMachineMigrate',
-                'workflow.steps.util.volume_provider.NewVolume',
-                'workflow.steps.util.vm.WaitingBeReady',
-                'workflow.steps.util.vm.UpdateOSDescription',
-                'workflow.steps.util.volume_provider.MountDataVolume',
-            )}, {
-            'Configuring new host': (
-                'workflow.steps.util.plan.Initialization',
-                'workflow.steps.util.plan.Configure',
-                'workflow.steps.util.database.Start',
-                'workflow.steps.util.database.CheckIsUp',
-            )}, {
-            'Checking access': (
-                'workflow.steps.util.vm.CheckAccessToMaster',
-                'workflow.steps.util.vm.CheckAccessFromMaster',
-                'workflow.steps.util.acl.ReplicateAclsMigrate',
-            )}, {
-            'Configuring Cluster': (
-                'workflow.steps.redis.cluster.AddSlaveNode',
-                'workflow.steps.redis.cluster.CheckClusterStatus',
-            )}, {
-            'Configuring DNS': (
-                'workflow.steps.util.dns.ChangeEndpoint',
-                'workflow.steps.util.dns.CheckIsReady',
-            )}, {
-            'Configure Monitors': (
-                'workflow.steps.util.zabbix.DestroyAlarms',
-                'workflow.steps.util.zabbix.CreateAlarms',
-            )}, {
-            'Cleaning up': (
-                'workflow.steps.util.disk.ChangeSnapshotOwner',
-                'workflow.steps.redis.cluster.RemoveNode',
-                'workflow.steps.redis.cluster.CheckClusterStatus',
-                'workflow.steps.util.host_provider.DestroyVirtualMachineMigrate',
-            )
+            'Migrating':
+                self.get_base_host_migrate_steps() +
+                self.get_host_migrate_steps_cleaning_up()
+        }]
+
+    def get_database_migrate_steps(self):
+        return [{
+            'Migrating': self.get_base_host_migrate_steps()
+        }, {
+            'Cleaning up': self.get_host_migrate_steps_cleaning_up()
         }]
 
     @property
