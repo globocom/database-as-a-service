41eab9dc208156279fb77ddd6aad2f5544003fe1
Ricardo Dias <ricardo@ricardodias.org>
2018-11-30 15:23:22 -0200

    add try/except to fix test

--- dbaas/dbaas/views.py
+++ dbaas/dbaas/views.py
@@ -9,16 +9,19 @@ def external_links(request):
     iaas_status = Configuration.get_by_name('iaas_status')
     iaas_quota = Configuration.get_by_name('iaas_quota')
 
-    credential = get_credentials_for(
-        environment=Environment.objects.first(),
-        credential_type=CredentialType.GRAFANA
-    )
-
-    sofia_dashboard = "{}/{}?var-datasource={}".format(
-        credential.endpoint,
-        credential.get_parameter_by_name('sofia_dbaas_dashboard'),
-        credential.get_parameter_by_name('datasource')
+    try:
+        credential = get_credentials_for(
+            environment=Environment.objects.first(),
+            credential_type=CredentialType.GRAFANA
         )
+
+        sofia_dashboard = "{}/{}?var-datasource={}".format(
+            credential.endpoint,
+            credential.get_parameter_by_name('sofia_dbaas_dashboard'),
+            credential.get_parameter_by_name('datasource')
+        )
+    except IndexError:
+        sofia_dashboard = ""
 
     return {'iaas_status': iaas_status,
             'iaas_quota': iaas_quota,
