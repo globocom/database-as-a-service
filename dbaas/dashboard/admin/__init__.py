987b771eb33aaa95c5cf03e1c84ff98f0cbc3678
Ricardo Dias <ricardo@ricardodias.org>
2018-11-30 11:21:36 -0200

    Add dbaas dashboard on Custom Views

--- dbaas/dashboard/admin/__init__.py
+++ dbaas/dashboard/admin/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from ..views import dashboard
+from ..views import dashboard, sofia_dashboard
 
 import admin
 admin.site.register_view('/dashboard/', view=dashboard)
+admin.site.register_view('/dashboard/sofia_dashboard/',
+                         name='Sofia Dashboard',
+                         view=sofia_dashboard)
