db3c577cb4bfba9f04aa4d59c3198fdbf1a8b985
Ricardo Dias <ricardo@ricardodias.org>
2019-01-12 09:27:04 -0200

    create vip on networkapi using vip-provider

--- dbaas/workflow/steps/util/network.py
+++ dbaas/workflow/steps/util/network.py
@@ -7,7 +7,6 @@ from dbaas_networkapi.provider import NetworkProvider
 from dbaas_networkapi.utils import get_vip_ip_from_databaseinfra
 from base import BaseInstanceStep
 from workflow.steps.util.base import HostProviderClient
-from workflow.steps.util.vip_provider import CreateVip as VipProviderCreateVip
 
 
 
@@ -15,20 +14,8 @@ class Network(BaseInstanceStep):
 
     def __init__(self, instance):
         super(Network, self).__init__(instance)
-        self._api = None
-        self._provider = None
-
-    @property
-    def api(self):
-        if not self._api:
-            self._api = DatabaseAsAServiceApi(self.infra)
-        return self._api
-
-    @property
-    def provider(self):
-        if not self._provider:
-            self._provider = NetworkProvider(self.api)
-        return self._provider
+        self.api = DatabaseAsAServiceApi(self.infra)
+        self.provider = NetworkProvider(self.api)
 
     def do(self):
         raise NotImplementedError
@@ -73,41 +60,21 @@ class CreateVip(Network):
     def is_valid(self):
         return self.instance == self.infra.instances.first()
 
-    @property
-    def is_aws(self):
-        create_vip = VipProviderCreateVip(self.instance)
-        try:
-            create_vip.provider.credential
-        except:
-            return False
-        else:
-            return True
-
     def do(self):
         if not self.is_valid:
             return
 
-        if self.is_aws:
-            aws_step = VipProviderCreateVip(self.instance)
-            aws_step.do()
-        else:
-            vip = self.provider.create_vip(self.equipments, 3306, self.vip_dns)
-            dns = add_dns_record(self.infra, self.infra.name, vip.vip_ip, FOXHA, is_database=False)
+        vip = self.provider.create_vip(self.equipments, 3306, self.vip_dns)
+        dns = add_dns_record(self.infra, self.infra.name, vip.vip_ip, FOXHA, is_database=False)
 
-            self.infra.endpoint = "{}:{}".format(vip.vip_ip, 3306)
-            self.infra.endpoint_dns = "{}:{}".format(dns, 3306)
-            self.infra.save()
+        self.infra.endpoint = "{}:{}".format(vip.vip_ip, 3306)
+        self.infra.endpoint_dns = "{}:{}".format(dns, 3306)
+        self.infra.save()
 
     def undo(self):
-        if self.is_aws:
-            if not self.is_valid:
-                return
-            aws_step = VipProviderCreateVip(self.instance)
-            aws_step.undo()
+        try:
+            vip_ip = get_vip_ip_from_databaseinfra(self.infra)
+        except ObjectDoesNotExist:
+            return
         else:
-            try:
-                vip_ip = get_vip_ip_from_databaseinfra(self.infra)
-            except ObjectDoesNotExist:
-                return
-            else:
-                self.provider.delete_vip(vip_ip)
+            self.provider.delete_vip(vip_ip)
