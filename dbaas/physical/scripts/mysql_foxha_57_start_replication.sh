af2edeaf8b5ff59f051d2b2b7730293e1c2de615
Ricardo Dias <ricardo@ricardodias.org>
2019-01-28 19:07:30 -0200

    scripts for mysql57

---
+++ dbaas/physical/scripts/mysql_foxha_57_start_replication.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+die_if_error()
+{
+    local err=$?
+    if [ "$err" != "0" ]; then
+        echo "$*"
+        exit $err
+    fi
+}
+
+setreplication()
+{
+    echo ""; echo $(date "+%Y-%m-%d %T") "- Setting the database replication"
+    mysql -p{{DBPASSWORD}} <<EOF_DBAAS
+    CHANGE MASTER TO
+    MASTER_HOST='{{IPMASTER}}',
+    MASTER_USER='{{REPLICA_USER}}',
+    MASTER_PASSWORD='{{REPLICA_PASSWORD}}',
+    MASTER_LOG_FILE='mysql-bin.000003',
+    MASTER_LOG_POS=154;
+    exit
+EOF_DBAAS
+    die_if_error "Error setting replication"
+}
+
+startreplication()
+{
+    echo ""; echo $(date "+%Y-%m-%d %T") "- Starting the database replication"
+    mysql -p{{DBPASSWORD}}<<EOF_DBAAS
+    start slave;
+    exit
+EOF_DBAAS
+    die_if_error "Error starting slave"
+}
+
+startheartbeatdaemon()
+{
+    echo ""; echo $(date "+%Y-%m-%d %T") "- Starting heartbeat daemon"
+    /etc/init.d/pt-heartbeat start
+    #die_if_error "Error starting heartbeat"
+}
+
+setreplication
+startreplication
+startheartbeatdaemon
+
+exit 0
