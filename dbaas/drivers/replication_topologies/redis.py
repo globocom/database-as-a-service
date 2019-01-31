d8d89cb4ce63e999131d7180615fc816d8f845f8
elesbom <rafael.goncalves@corp.globo.com>
2019-01-31 15:29:19 -0200

    Remove legacy code for redis

--- dbaas/drivers/replication_topologies/redis.py
+++ dbaas/drivers/replication_topologies/redis.py
@@ -4,29 +4,6 @@ from physical.models import Instance
 
 
 class BaseRedis(BaseTopology):
-    def deploy_first_steps(self):
-        return (
-            'workflow.steps.redis.deploy.build_databaseinfra.BuildDatabaseInfra',
-            'workflow.steps.redis.deploy.create_virtualmachines.CreateVirtualMachine',
-            'workflow.steps.redis.deploy.create_dns.CreateDns',
-            'workflow.steps.util.deploy.create_nfs.CreateNfs',
-            'workflow.steps.redis.deploy.init_database.InitDatabaseRedis',
-            'workflow.steps.util.deploy.check_database_connection.CheckDatabaseConnection',
-            'workflow.steps.util.deploy.check_dns.CheckDns',
-            'workflow.steps.util.deploy.start_monit.StartMonit',
-        )
-
-    def deploy_last_steps(self):
-        return (
-            'workflow.steps.util.deploy.build_database.BuildDatabase',
-            'workflow.steps.util.deploy.check_database_binds.CheckDatabaseBinds',
-        )
-
-    def get_clone_steps(self):
-        return self.deploy_first_steps() + self.deploy_last_steps() + (
-            'workflow.steps.redis.clone.clone_database.CloneDatabase',
-        ) + self.monitoring_steps()
-
     def get_upgrade_steps_extra(self):
         return (
             'workflow.steps.util.volume_provider.MountDataVolume',
