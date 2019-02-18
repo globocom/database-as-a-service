30d76f18bcc1d7841d29dfaeb9d39a0548bbdc32
Ricardo Dias <ricardo@ricardodias.org>
2019-02-18 16:35:42 -0300

    add organition on team list_display

--- dbaas/account/admin/team.py
+++ dbaas/account/admin/team.py
@@ -32,7 +32,7 @@ class RoleListFilter(SimpleListFilter):
 
 class TeamAdmin(admin.ModelAdmin):
 
-    list_display = ["name", "role", "database_limit", "email"]
+    list_display = ["name", "role", "database_limit", "email", "organization"]
     filter_horizontal = ['users']
     list_filter = (RoleListFilter, )
     search_fields = ('name',)
