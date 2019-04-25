30f71eb07e17bc0d37ca021b91282205a4df1b3f
Diego Lima <diego.lima@corp.globo.com>
2019-04-25 10:38:05 -0300

    Create MONGO_SERVER_SELECTION_DEFAULT_TIMEOUT variable

--- dbaas/drivers/mongodb.py
+++ dbaas/drivers/mongodb.py
@@ -22,6 +22,7 @@ LOG = logging.getLogger(__name__)
 
 CLONE_DATABASE_SCRIPT_NAME = "mongodb_clone.sh"
 MONGO_CONNECTION_DEFAULT_TIMEOUT = 5
+MONGO_SERVER_SELECTION_DEFAULT_TIMEOUT = 5
 
 
 class MongoDB(BaseDriver):
@@ -157,9 +158,12 @@ class MongoDB(BaseDriver):
             # mongo uses timeout in mili seconds
             connection_timeout_in_miliseconds = Configuration.get_by_name_as_int(
                 'mongo_connect_timeout', default=MONGO_CONNECTION_DEFAULT_TIMEOUT) * 1000
+            
+            server_selection_timeout_in_seconds = Configuration.get_by_name_as_int(
+                'mongo_server_selection_timeout', default=MONGO_SERVER_SELECTION_DEFAULT_TIMEOUT)
 
             client = pymongo.MongoClient(
-                connection_address, connectTimeoutMS=connection_timeout_in_miliseconds)
+                connection_address, connectTimeoutMS=connection_timeout_in_miliseconds, serverSelectionTimeoutMS=server_selection_timeout_in_seconds)
             if (not instance) or (instance and instance.instance_type != instance.MONGODB_ARBITER):
                 if self.databaseinfra.user and self.databaseinfra.password:
                     LOG.debug('Authenticating databaseinfra %s',
