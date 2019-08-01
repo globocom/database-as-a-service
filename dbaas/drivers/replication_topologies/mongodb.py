3730c586ed7eda839268587878a2753a574e3af2
Ricardo Dias <ricardo@ricardodias.org>
2019-08-01 09:28:52 -0300

    fix name

--- dbaas/drivers/replication_topologies/mongodb.py
+++ dbaas/drivers/replication_topologies/mongodb.py
@@ -122,7 +122,7 @@ class MongoDBSingle(BaseTopology):
                 'workflow.steps.util.metric_collector.StopTelegraf',
                 'workflow.steps.util.database.Stop',
                 'workflow.steps.util.database.StopRsyslog',
-                'workflow.stps.util.database.CheckIsDown',
+                'workflow.steps.util.database.CheckIsDown',
                 'workflow.steps.util.volume_provider.AddAccessNewVolume',
                 'workflow.steps.util.volume_provider.MountDataLatestVolume',
                 'workflow.steps.util.volume_provider.CopyPermissions',
