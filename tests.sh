dce86fff923d8a57309fac3aeaa1a736237b4881
elesbom <rafael.goncalves@corp.globo.com>
2019-09-27 14:52:19 -0300

    Comment mysql

--- tests.sh
+++ tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-which mysql
-ls
-echo -n 'waiting mysql start'
-while ! mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD -e "SHOW DATABASES"
-do
-  echo -n .
-  sleep 1
-done
-
-ps -ef | grep mysql
-echo 'MYSQL STARTED!!!!'
-
-# Create DATABASE
-echo "create database IF NOT EXISTS dbaas;" | mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD
+# which mysql
+# ls
+# echo -n 'waiting mysql start'
+# while ! mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD -e "SHOW DATABASES"
+# do
+#   echo -n .
+#   sleep 1
+# done
+#
+# ps -ef | grep mysql
+# echo 'MYSQL STARTED!!!!'
+#
+# # Create DATABASE
+# echo "create database IF NOT EXISTS dbaas;" | mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD
 
 # Create user on mongo
 python add_user_admin_on_mongo.py
