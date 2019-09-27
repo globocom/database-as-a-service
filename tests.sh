8c199e83a84970db70cb76c88c6c38847237c462
elesbom <rafael.goncalves@corp.globo.com>
2019-09-27 14:44:08 -0300

    test docker

--- tests.sh
+++ tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+which mysql
+ls
 echo -n 'waiting mysql start'
-while ! docker exec mysql56 mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD -e "SHOW DATABASES"
+while ! mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD -e "SHOW DATABASES"
 do
   echo -n .
   sleep 1
@@ -10,7 +12,7 @@ ps -ef | grep mysql
 echo 'MYSQL STARTED!!!!'
 
 # Create DATABASE
-echo "create database IF NOT EXISTS dbaas;" | docker exec mysql56 mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD
+echo "create database IF NOT EXISTS dbaas;" | mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD
 
 # Create user on mongo
 python add_user_admin_on_mongo.py
