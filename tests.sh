428588ae95e2baa7b5e8b718368aaa82cf96a176
elesbom <rafael.goncalves@corp.globo.com>
2019-09-27 14:21:59 -0300

    Use mysql from docker

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -n 'waiting mysql start'
-while ! /usr/local/mysql/bin/mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD -e "SHOW DATABASES"
+while ! docker exec mysql56 mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD -e "SHOW DATABASES"
 do
   echo -n .
   sleep 1
@@ -10,7 +10,7 @@ ps -ef | grep mysql
 echo 'MYSQL STARTED!!!!'
 
 # Create DATABASE
-echo "create database IF NOT EXISTS dbaas;" | /usr/local/mysql/bin/mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD
+echo "create database IF NOT EXISTS dbaas;" | docker exec mysql56 mysql -h $DBAAS_DATABASE_HOST -uroot -p$DBAAS_DATABASE_PASSWORD
 
 # Create user on mongo
 python add_user_admin_on_mongo.py
