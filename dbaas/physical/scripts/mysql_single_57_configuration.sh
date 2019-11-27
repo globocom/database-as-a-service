b9cd31d87d54f3259778610719ffee56feeb9004
Eliakin Costa <eliakim170@gmail.com>
2019-11-27 17:46:30 -0300

    Adding sql_mode to MySQL Single 5.7 configuration

--- dbaas/physical/scripts/mysql_single_57_configuration.sh
+++ dbaas/physical/scripts/mysql_single_57_configuration.sh
@@ -61,6 +61,8 @@ wait_timeout                    = {{ configuration.wait_timeout.value }}
 interactive_timeout             = {{ configuration.interactive_timeout.value }}
 log_bin_trust_function_creators = {{ configuration.log_bin_trust_function_creators.value }}
 
+sql_mode                        = {{ configuration.sql_mode.value }}
+
 # Binary Logging
 log_bin                         = /data/repl/mysql-bin
 relay_log                       = /data/repl/mysql-relay
@@ -137,7 +139,7 @@ createconfigdbrsyslogfile()
 \$InputRunFileMonitor
 
 \$ModLoad imfile
-\$InputFileName /data/logs/mysql-slow.log 
+\$InputFileName /data/logs/mysql-slow.log
 \$InputFileTag mysql-slow:
 \$InputFileStateFile mysql-slow-dbaas
 
