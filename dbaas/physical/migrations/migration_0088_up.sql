291b57700d82ac8f5ab068358473af9c9a43a73e
Eliakin Costa <eliakim170@gmail.com>
2019-12-03 13:51:44 -0300

    Adding default to sql_mode migrations script

--- dbaas/physical/migrations/migration_0088_up.sql
+++ dbaas/physical/migrations/migration_0088_up.sql
@@ -5,7 +5,7 @@ INSERT INTO physical_parameter (created_at, updated_at, engine_type_id,
 SELECT now(), now(), physical_enginetype.id,
     'sql_mode', 1, null,
     'Modes affect the SQL syntax MySQL supports and the data validation checks it performs. This makes it easier to use MySQL in different environments and to use MySQL together with other database servers.',
-    '', 'string'
+    'default', 'string'
 from physical_enginetype where name = 'mysql' limit 1;
 
 INSERT INTO physical_replicationtopology_parameter (replicationtopology_id, parameter_id)
