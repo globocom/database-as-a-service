6c4a4c01ba0039d2b1a054df084ae676386dc060
Eliakin Costa <eliakim170@gmail.com>
2019-12-04 14:35:19 -0300

    Adding comments for better understanding

--- dbaas/backup/tasks.py
+++ dbaas/backup/tasks.py
@@ -209,6 +209,7 @@ def make_databases_backup(self):
     current_time = datetime.now()
     current_hour = current_time.hour
 
+    # Get all infra should have gotten a backup today until the current hour
     infras_with_backup_today = DatabaseInfra.objects.filter(
         instances__backup_instance__status=2,
         backup_hour__lt=current_hour,
@@ -216,15 +217,18 @@ def make_databases_backup(self):
         instances__backup_instance__end_at__month=current_time.month,
         instances__backup_instance__end_at__day=current_time.day).distinct()
 
+    # Get all infras with pending backups based on infras_with_backup_today
     infras_pending_backup = DatabaseInfra.objects.filter(
         backup_hour__lt=current_hour
     ).exclude(pk__in=[infra.pk for infra in infras_with_backup_today])
 
+    # Get all infras to backup on the current hour
     infras_current_hour = DatabaseInfra.objects.filter(
         plan__has_persistence=True,
         backup_hour=current_time.hour
     )
 
+    # Merging pending and current infras to backup list
     infras = infras_current_hour | infras_pending_backup
 
     for env_name in env_names_order:
