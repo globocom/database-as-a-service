6fa8c66804ede5006b55a3930e42178b8ed8c659
elesbom <rafael.goncalves@corp.globo.com>
2019-12-06 11:33:36 -0300

    Create periodic task check_ssl_expire_at

---
+++ dbaas/notification/tests/test_periodic_tasks.py
@@ -0,0 +1,71 @@
+from django.test import TestCase
+from mock import patch, MagicMock
+from datetime import date, timedelta
+
+from physical.tests import factory as factory_physical
+from physical.models import Instance
+from logical.tests import factory as factory_logical
+from dbaas.tests.helpers import InstanceHelper
+from notification.periodic_tasks import check_ssl_expire_at
+from maintenance.models import TaskSchedule
+
+
+@patch('notification.periodic_tasks.get_worker_name', new=MagicMock())
+@patch('notification.periodic_tasks.TaskHistory', new=MagicMock())
+class CheckSslExpireAt(TestCase):
+    instance_helper = InstanceHelper
+
+    def setUp(self):
+        self.today = date.today()
+        self.engine_type = factory_physical.EngineTypeFactory(
+            name='mysql'
+        )
+        self.engine = factory_physical.EngineFactory(
+            engine_type=self.engine_type
+        )
+        self.plan = factory_physical.PlanFactory(
+            engine=self.engine
+        )
+        self.databaseinfra = factory_physical.DatabaseInfraFactory(
+            name="__test__ mysqlinfra2",
+            user="root", password='fake_pass',
+            engine=self.engine,
+            plan=self.plan,
+            ssl_configured=True
+        )
+        self.hostname = factory_physical.HostFactory()
+        self.instances = self.instance_helper.create_instances_by_quant(
+            instance_type=Instance.MYSQL, qt=1,
+            infra=self.databaseinfra, hostname=self.hostname
+        )
+        self.instance = self.instances[0]
+        self.database = factory_logical.DatabaseFactory(
+            name='test_db_1',
+            databaseinfra=self.databaseinfra,
+        )
+        self.one_month_later = self.today + timedelta(days=30)
+
+    @patch('notification.periodic_tasks.TaskSchedule.objects.filter')
+    def test_dont_find_infras(self, filter_mock):
+        self.databaseinfra.ssl_configured = False
+        check_ssl_expire_at()
+        self.assertFalse(filter_mock.called)
+
+    @patch('notification.periodic_tasks.TaskSchedule.objects.create')
+    def test_already_have_task_scheduled(self, create_mock):
+        task_schedule = TaskSchedule()
+        task_schedule.database = self.database
+        task_schedule.scheduled_for = self.one_month_later
+        task_schedule.status = TaskSchedule.SCHEDULED
+        task_schedule.save()
+        check_ssl_expire_at()
+        self.assertFalse(create_mock.called)
+
+    def test_create_task_scheduled(self):
+        task_schedule = TaskSchedule.objects.filter(database=self.database)
+        self.hostname.ssl_expire_at = self.one_month_later
+        self.hostname.save()
+        self.assertEqual(task_schedule.count(), 0)
+        check_ssl_expire_at()
+        task_schedule = TaskSchedule.objects.filter(database=self.database)
+        self.assertEqual(task_schedule.count(), 1)
