20be2b1b8814e60f28ef9a9497fd4f687e4508aa
Eliakin Costa <eliakim170@gmail.com>
2019-12-11 15:20:01 -0300

    Adding available_plans_for_migration to the views

--- dbaas/logical/views.py
+++ dbaas/logical/views.py
@@ -927,6 +927,7 @@ def database_maintenance(request, context, database):
     context['available_patches'] = list(
         database.engine.available_patches(database)
     )
+    context['available_plans_for_migration'] = database.available_plans_for_migration()
 
     context['maintenance_windows'] = \
         [(hour, datetime.time(hour, 0).strftime('%H:%M - ' +
