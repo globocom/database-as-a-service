3e44f202b618ccf593923a0d4c837370543c7cfa
Eliakin Costa <eliakim170@gmail.com>
2020-01-07 15:58:59 -0300

    Adding new field current_database to DatabaseMigrateEngine maintenance model

--- dbaas/maintenance/models.py
+++ dbaas/maintenance/models.py
@@ -447,6 +447,11 @@ class DatabaseUpgrade(DatabaseMaintenanceTask):
 
 class DatabaseMigrateEngine(DatabaseUpgrade):
 
+    current_database = models.ForeignKey(
+        Database, verbose_name="Database",
+        null=False, unique=False, related_name="engine_migrations"
+    )
+
     def __unicode__(self):
         return "{} migrate engine".format(self.database.name)
 
