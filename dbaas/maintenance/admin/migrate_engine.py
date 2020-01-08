a8dc686c0387fce8b8531701969542ae9c332c92
Eliakin Costa <eliakim170@gmail.com>
2020-01-08 16:45:33 -0300

    Improvement - Retry only redirecting to Maintenance Tab
    -  Changing behavior as Retry from Managers is Legacy code
    -  Warning - Retry should be executed from Database Context

---
+++ dbaas/maintenance/admin/migrate_engine.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from django.utils.html import format_html
+from .database_maintenance_task import DatabaseMaintenanceTaskAdmin
+
+
+class DatabaseMigrateEngineAdmin(DatabaseMaintenanceTaskAdmin):
+
+    list_filter = [
+        "database__team", "source_plan", "target_plan", "source_plan__engine",
+        "status",
+    ]
+
+    list_display = (
+        "database", "database_team", "source_plan_name", "target_plan_name",
+        "current_step", "friendly_status", "maintenance_action", "link_task",
+        "started_at", "finished_at"
+    )
+
+    readonly_fields = (
+        "database", "source_plan", "source_plan_name", "target_plan",
+        "target_plan_name", "link_task", "started_at", "finished_at",
+        "current_step", "status", "maintenance_action"
+    )
+
+    def maintenance_action(self, maintenance_task):
+        if (not maintenance_task.is_status_error or
+                not maintenance_task.can_do_retry):
+            return 'N/A'
+
+        url = maintenance_task.database.get_migrate_engine_retry_url()
+        html = ("<a title='Retry' class='btn btn-info' "
+                "href='{}'>Retry</a>").format(url)
+        return format_html(html)
