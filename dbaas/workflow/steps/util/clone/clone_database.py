c36efa94b1e6a973909b674700899f76c649fcb1
elesbom <rafael.goncalves@corp.globo.com>
2020-01-30 17:12:52 -0300

    Create CloneDatabaseData Step

--- dbaas/workflow/steps/util/clone/clone_database.py
+++ dbaas/workflow/steps/util/clone/clone_database.py
@@ -1,51 +1,42 @@
 # -*- coding: utf-8 -*-
 import logging
-from util import full_stack, call_script
+from util import call_script
 from django.conf import settings
 from drivers import factory_for
 from notification.util import get_clone_args
-from workflow.steps.util.base import BaseStep
-from workflow.exceptions.error_codes import DBAAS_0017
+from workflow.steps.util.base import BaseInstanceStep
 
 LOG = logging.getLogger(__name__)
 
 
-class CloneDatabase(BaseStep):
+class CloneDatabaseData(BaseInstanceStep):
 
     def __unicode__(self):
         return "Replicating database data..."
 
-    def do(self, workflow_dict):
+    def do(self):
         try:
-
-            if 'databaseinfra' not in workflow_dict \
-                    or 'clone' not in workflow_dict:
-                return False
-
             args = get_clone_args(
-                workflow_dict['clone'], workflow_dict['database'])
-            script_name = factory_for(
-                workflow_dict['clone'].databaseinfra).clone()
+                self.step_manager.origin_database, self.database)
+            script_name = factory_for(self.infra).clone()
 
             return_code, output = call_script(
-                script_name, working_dir=settings.SCRIPTS_PATH, args=args, split_lines=False,)
+                script_name, working_dir=settings.SCRIPTS_PATH,
+                args=args, split_lines=False
+            )
 
             LOG.info("Script Output: {}".format(output))
             LOG.info("Return code: {}".format(return_code))
 
             if return_code != 0:
-                workflow_dict['exceptions']['traceback'].append(output)
+                raise Exception(output)
                 return False
 
             return True
-        except Exception:
-            traceback = full_stack()
-
-            workflow_dict['exceptions']['error_codes'].append(DBAAS_0017)
-            workflow_dict['exceptions']['traceback'].append(traceback)
+        except Exception as err:
+            raise Exception(err)
 
             return False
 
-    def undo(self, workflow_dict):
-        LOG.info("Nothing to do here...")
-        return True
+    def undo(self):
+        pass
