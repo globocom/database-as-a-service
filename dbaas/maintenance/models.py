3c33127f65aed8869d82b723035100409c102d65
Eliakin Costa <eliakim170@gmail.com>
2020-02-03 11:25:16 -0300

    Implementing AddInstancesToDatabase maintenance model

--- dbaas/maintenance/models.py
+++ dbaas/maintenance/models.py
@@ -969,6 +969,24 @@ class UpdateSsl(DatabaseMaintenanceTask):
                     pass
 
 
+class AddInstancesToDatabase(DatabaseMaintenanceTask):
+    task = models.ForeignKey(
+        TaskHistory, verbose_name="Task History",
+        null=False, related_name="add_instances_to_database_manager"
+    )
+    database = models.ForeignKey(
+        Database, verbose_name="Database",
+        null=False, unique=False,
+        related_name="add_instances_to_database_manager"
+    )
+    quantity = models.PositiveIntegerField(
+        verbose_name="Quantity", null=False, unique=False
+    )
+
+    def __unicode__(self):
+        return "Add instances to database: {}".format(self.database)
+
+
 simple_audit.register(Maintenance)
 simple_audit.register(HostMaintenance)
 simple_audit.register(MaintenanceParameters)
