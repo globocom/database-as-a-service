1d3e1fa96ef85192bc9bb6dbf7c5735c03fe59b2
Ricardo Dias <ricardo@ricardodias.org>
2020-02-13 14:15:51 -0300

    change label (plans to tasks)

--- dbaas/djcelery_dbaas/admin.py
+++ dbaas/djcelery_dbaas/admin.py
@@ -8,11 +8,11 @@ class PeriodicTaskDbaas(PeriodicTaskAdmin):
 
     def action_enable_plans(self, request, queryset):
         queryset.update(enabled=True)
-    action_enable_plans.short_description = "Enable selected plans"
+    action_enable_plans.short_description = "Enable selected tasks"
 
     def action_disable_plans(self, request, queryset):
         queryset.update(enabled=False)
-    action_disable_plans.short_description = "Disable selected plans"
+    action_disable_plans.short_description = "Disable selected tasks"
 
 admin.site.unregister(PeriodicTask)
 admin.site.register(PeriodicTask, PeriodicTaskDbaas)
