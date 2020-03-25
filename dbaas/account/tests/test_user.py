3f73ad95f2ddd72b936660e526c587af0cd3a8ad
elesbom <rafael.goncalves@corp.globo.com>
2020-03-25 19:44:11 -0300

    Change factoryBoy to model_mommy

--- dbaas/account/tests/test_user.py
+++ dbaas/account/tests/test_user.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+
 from django.test import TestCase
-from ..models import Team, Role, User, Organization
+
 from ..admin.user import TeamListFilter, UserAdmin
+from model_mommy import mommy
 
 
 class UserTest(TestCase):
@@ -13,30 +15,52 @@ class UserTest(TestCase):
     PASSWORD = "12345"
 
     def setUp(self):
-        self.role = Role.objects.get_or_create(name="fake_role")[0]
-        self.organization = Organization.objects.get_or_create(
-            name='fake_organization')[0]
+        # self.role = Role.objects.get_or_create(name="fake_role")[0]
+        self.role = mommy.make_recipe('account.role')
+        # self.organization = Organization.objects.get_or_create(
+        #     name='fake_organization')[0]
+        self.organization = mommy.make('Organization')
 
-        self.team_dba = Team.objects.get_or_create(
-            name="team_dba",
+        # self.team_dba = Team.objects.get_or_create(
+        #     name="team_dba",
+        #     role=self.role,
+        #     organization=self.organization
+        # )[0]
+        self.team_dba = mommy.make_recipe(
+            'account.team',
             role=self.role,
             organization=self.organization
-        )[0]
-        self.team_dev = Team.objects.get_or_create(
-            name="team_dev",
+        )
+        # self.team_dev = Team.objects.get_or_create(
+        #     name="team_dev",
+        #     role=self.role,
+        #     organization=self.organization
+        # )[0]
+        self.team_dev = mommy.make_recipe(
+            'account.team',
             role=self.role,
             organization=self.organization
-        )[0]
-        self.team_ops = Team.objects.get_or_create(
-            name="team_ops",
+        )
+        # self.team_ops = Team.objects.get_or_create(
+        #     name="team_ops",
+        #     role=self.role,
+        #     organization=self.organization
+        # )[0]
+        self.team_ops = mommy.make_recipe(
+            'account.team',
             role=self.role,
             organization=self.organization
-        )[0]
-        self.team_bkp = Team.objects.get_or_create(
-            name="team_bkp",
+        )
+        # self.team_bkp = Team.objects.get_or_create(
+        #     name="team_bkp",
+        #     role=self.role,
+        #     organization=self.organization
+        # )[0]
+        self.team_bkp = mommy.make_recipe(
+            'account.team',
             role=self.role,
             organization=self.organization
-        )[0]
+        )
 
         self.user_dev = User.objects.create_superuser(
             self.DEV_NAME,
