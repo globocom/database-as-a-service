2addaa28216206e9458becf327c700312e0a7afd
elesbom <rafael.goncalves@corp.globo.com>
2020-03-26 11:17:41 -0300

    Remove commented code

--- dbaas/notification/tests/test_periodic_tasks/test_send_mail_24hours_before_auto_task.py
+++ dbaas/notification/tests/test_periodic_tasks/test_send_mail_24hours_before_auto_task.py
@@ -6,11 +6,9 @@ from itertools import cycle
 from model_mommy import mommy
 from model_mommy.recipe import seq
 
-from physical.models import Instance
 from dbaas.tests.helpers import InstanceHelper
 from notification.tasks import send_mail_24hours_before_auto_task
-from maintenance.models import TaskSchedule
-from dbaas.tests.helpers import DatabaseHelper, InfraHelper, PlanHelper
+from dbaas.tests.helpers import DatabaseHelper
 
 
 __all__ = ('SendMailTestCase',)
@@ -128,102 +126,3 @@ class SendMailTestCase(TestCase):
                 'execution_warning': True
             }
         )
-
-    # @patch('notification.tasks.maintenance_models.TaskSchedule.objects.filter')
-    # def test_dont_find_infras(self, filter_mock):
-    #     self.databaseinfra.ssl_configured = False
-    #     self.databaseinfra.save()
-    #     check_ssl_expire_at()
-    #     self.assertFalse(filter_mock.called)
-    #
-    # @patch('notification.tasks.Configuration.get_by_name',
-    #        new=MagicMock(return_value='other_env'))
-    # @patch('notification.tasks.maintenance_models.TaskSchedule.objects.filter')
-    # def test_dont_find_infras_if_env_configured(self, filter_mock):
-    #     check_ssl_expire_at()
-    #     self.assertFalse(filter_mock.called)
-    #
-    # @patch('notification.tasks.maintenance_models.TaskSchedule.objects.create')
-    # @patch('maintenance.models.schedule_task_notification', new=MagicMock())
-    # def test_already_have_task_scheduled(self, create_mock):
-    #     task_schedule = TaskSchedule()
-    #     task_schedule.database = self.database
-    #     task_schedule.scheduled_for = self.one_month_later
-    #     task_schedule.status = TaskSchedule.SCHEDULED
-    #     task_schedule.save()
-    #     check_ssl_expire_at()
-    #     self.assertFalse(create_mock.called)
-    #
-    # def test_create_task_scheduled(self):
-    #     task_schedule = TaskSchedule.objects.filter(database=self.database)
-    #     self.hostname.ssl_expire_at = self.one_month_later
-    #     self.hostname.save()
-    #     self.assertEqual(task_schedule.count(), 0)
-    #     check_ssl_expire_at()
-    #     task_schedule = TaskSchedule.objects.filter(database=self.database)
-    #     self.assertEqual(task_schedule.count(), 1)
-    #
-    # def test_create_task_scheduled_percona(self):
-    #     self.engine_type.name = 'mysql_percona'
-    #     self.engine_type.save()
-    #     task_schedule = TaskSchedule.objects.filter(database=self.database)
-    #     self.hostname.ssl_expire_at = self.one_month_later
-    #     self.hostname.save()
-    #     self.assertEqual(task_schedule.count(), 0)
-    #     check_ssl_expire_at()
-    #     task_schedule = TaskSchedule.objects.filter(database=self.database)
-    #     self.assertEqual(task_schedule.count(), 1)
-    #
-    # @patch('notification.tasks.Configuration.get_by_name',
-    #        new=MagicMock(return_value='fake_env'))
-    # def test_create_task_scheduled_if_configured(self):
-    #     task_schedule = TaskSchedule.objects.filter(database=self.database)
-    #     self.hostname.ssl_expire_at = self.one_month_later
-    #     self.hostname.save()
-    #     self.assertEqual(task_schedule.count(), 0)
-    #     check_ssl_expire_at()
-    #     task_schedule = TaskSchedule.objects.filter(database=self.database)
-    #     self.assertEqual(task_schedule.count(), 1)
-    #
-    # def _fake_get_by_name(self, conf_name):
-    #     if conf_name == 'schedule_send_mail':
-    #         return 0
-    #     else:
-    #         return self.check_ssl_envs
-    #
-    # @patch('notification.tasks.Configuration.get_by_name')
-    # def test_create_task_scheduled_if_configured_multiple_envs(
-    #         self, get_by_name_mock):
-    #     self.check_ssl_envs = 'fake_env,another_env'
-    #     get_by_name_mock.side_effect = self._fake_get_by_name
-    #     environment, databaseinfra, hostname, database = self._create_database(
-    #         env_name='another_env',
-    #         infra_name='__test__ another_infra'
-    #     )
-    #     task_schedule = TaskSchedule.objects.filter(database=self.database)
-    #     self.hostname.ssl_expire_at = self.one_month_later
-    #     self.hostname.save()
-    #     hostname.ssl_expire_at = self.one_month_later
-    #     hostname.save()
-    #     self.assertEqual(task_schedule.count(), 0)
-    #     check_ssl_expire_at()
-    #     task_schedule = TaskSchedule.objects.all()
-    #     self.assertEqual(task_schedule.count(), 2)
-    #
-    # @patch('notification.tasks.date')
-    # def test_create_task_scheduled_next_maintenance_window(self, date_mock):
-    #     date_mock.today.return_value = FAKE_TODAY
-    #     self.databaseinfra.maintenance_window = 3
-    #     self.databaseinfra.maintenance_day = 5
-    #     self.databaseinfra.save()
-    #     check_ssl_expire_at()
-    #     task_schedule = TaskSchedule.objects.get(database=self.database)
-    #     self.assertEqual(
-    #         task_schedule.scheduled_for.weekday(),
-    #         4
-    #     )
-    #     self.assertEqual(
-    #         task_schedule.scheduled_for.date().strftime("%Y-%m-%d"),
-    #         "2019-12-27"
-    #     )
-    #     self.assertEqual(task_schedule.scheduled_for.hour, 3)
