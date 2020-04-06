5076c8ce33594058add5dbf2bf1ced9a7b29ff46
elesbom <rafael.goncalves@corp.globo.com>
2020-04-06 14:04:19 -0300

    Add if on snapshot steps on zone migrate

--- dbaas/workflow/tests/test_volume_provider_steps/test_take_snapshot_from_master.py
+++ dbaas/workflow/tests/test_volume_provider_steps/test_take_snapshot_from_master.py
@@ -18,6 +18,9 @@ class SMMSingleInstanceTestCase(SingleInstanceBaseTestCase):
         get_driver_mock().get_master_instance.return_value = (
             self.master_instance
         )
+        get_driver_mock().get_database_instances.return_value = [
+            self.master_instance
+        ]
         make_snapshot_mock.return_value = self.snapshot
         backup_group_mock.return_value = self.backup_group
         self.step.do()
@@ -38,6 +41,9 @@ class SMMHATestCase(HABaseTestCase):
         get_driver_mock().get_master_instance.return_value = (
             self.master_instance
         )
+        get_driver_mock().get_database_instances.return_value = [
+            self.master_instance, self.slave_instance
+        ]
         make_snapshot_mock.return_value = self.snapshot
         backup_group_mock.return_value = self.backup_group
         self.step.do()
@@ -45,3 +51,21 @@ class SMMHATestCase(HABaseTestCase):
             make_snapshot_mock.call_args[1]['target_volume'],
             self.master_volume
         )
+
+    @patch('physical.models.DatabaseInfra.get_driver')
+    @patch('backup.tasks.make_instance_snapshot_backup')
+    @patch('backup.tasks.BackupGroup')
+    def test_not_run_if_instance_not_database(self, backup_group_mock,
+                                              make_snapshot_mock,
+                                              get_driver_mock):
+        get_driver_mock().get_database_instances.return_value = [
+            self.master_instance
+        ]
+        try:
+            self.step.do()
+        except VolumeProviderSnapshotHasWarningStatusError:
+            self.fail(
+                ("do() raised VolumeProviderSnapshotHasWarningStatusError "
+                 "unexpectedly!")
+            )
+        self.assertFalse(make_snapshot_mock.called)
