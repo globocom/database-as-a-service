57e117e259ce27c9bff3f857c9697fed20975a65
Ricardo Dias <ricardo@ricardodias.org>
2020-04-27 13:42:16 -0300

    change mysql test

--- dbaas/workflow/steps/util/metric_collector.py
+++ dbaas/workflow/steps/util/metric_collector.py
@@ -41,8 +41,7 @@ class MetricsCollector(BaseInstanceStep):
             'PORT': self.instance.port,
             'USER': user,
             'PASSWORD': password,
-            'MYSQL': self.instance.instance_type in (
-                self.instance.MYSQL, self.instance.MYSQL_PERCONA),
+            'MYSQL': self.instance.is_mysql,
             'MONGODB': self.instance.instance_type == self.instance.MONGODB,
             'REDIS': self.instance.instance_type == self.instance.REDIS,
             'CREATE_TELEGRAF_CONFIG': create_telegraf_config,
