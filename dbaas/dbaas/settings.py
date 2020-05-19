059bfee99ed332777f5b372521f8e24e63599d18
elesbom <rafael.goncalves@corp.globo.com>
2020-05-19 14:38:56 -0300

    Put settings inseide string

--- dbaas/dbaas/settings.py
+++ dbaas/dbaas/settings.py
@@ -134,7 +134,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = os.getenv('DBAAS_DJ_SECRET_KEY')
+SECRET_KEY = "{}".format(os.getenv('DBAAS_DJ_SECRET_KEY'))
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
