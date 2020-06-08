1b4c86ba0937dcfb188f5c2c5c138d7167574c2b
Natália Viveiros <nvmoreira@id.uff.br>
2020-06-08 16:00:27 -0300

    test_make_databases_backup improved

--- dbaas/backup/tests/test_make_databases_backup.py
+++ dbaas/backup/tests/test_make_databases_backup.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from mock import patch
+from mock import patch, call
 from datetime import datetime
 
 from model_mommy import mommy
@@ -7,6 +7,15 @@ from model_mommy import mommy
 from backup.tasks import make_databases_backup
 from backup.models import Snapshot, BackupGroup
 from dbaas.tests.helpers import DatabaseHelper, InfraHelper
+
+
+FAKE_NOW = datetime(2020, 1, 1, 5, 10, 00)
+
+
+class FakeDatetime(datetime):
+    @staticmethod
+    def now():
+        return FAKE_NOW
 
 
 class TestMakeDatabasesBackup(TestCase):
@@ -43,18 +52,14 @@ class TestMakeDatabasesBackup(TestCase):
 
     @patch('backup.tasks.make_instance_snapshot_backup')
     @patch('backup.models.BackupGroup.save')
-    @patch('backup.tasks.get_now')
+    @patch('backup.tasks.datetime', FakeDatetime)
     @patch('notification.models.TaskHistory.register')
     @patch('backup.tasks.get_worker_name')
-    def test_only_backup_current_hour(
-        self, get_worker_name_mock, task_register, datetime_now,
-        save_backup_group, make_instance_snapshot_backup
+    def test_backup_current_hour(
+        self, get_worker_name_mock, task_register, save_backup_group,
+        make_instance_snapshot_backup
     ):
         get_worker_name_mock.return_value = 'test'
-        datetime_now.return_value = datetime(
-                year=self.year, month=self.month, day=self.day,
-                hour=self.backup_hour
-        )
         group = BackupGroup()
         save_backup_group.return_value = group
         make_instance_snapshot_backup.return_value.status.return_value = (
@@ -63,5 +68,68 @@ class TestMakeDatabasesBackup(TestCase):
 
         make_databases_backup()
         make_instance_snapshot_backup.assert_called_with(
-            instance=self.instance, error={}, group=group
+            current_hour=self.backup_hour, instance=self.instance, error={},
+            group=group
         )
+
+    # @patch('backup.tasks.make_instance_snapshot_backup')
+    # @patch('backup.models.BackupGroup.save')
+    # @patch('notification.models.TaskHistory.register')
+    # @patch('backup.tasks.get_worker_name')
+    # def test_current_hour_without_pending_backup(
+    #     self, get_worker_name_mock, task_register, save_backup_group,
+    #     make_instance_snapshot_backup
+    # ):
+    #     get_worker_name_mock.return_value = 'test'
+    #     group = BackupGroup()
+    #     save_backup_group.return_value = group
+    #     make_instance_snapshot_backup.return_value.status.return_value = (
+    #         Snapshot.SUCCESS
+    #     )
+
+    @patch('backup.tasks.make_instance_snapshot_backup')
+    @patch('backup.models.BackupGroup.save')
+    @patch('backup.tasks.datetime', FakeDatetime)
+    @patch('notification.models.TaskHistory.register')
+    @patch('backup.tasks.get_worker_name')
+    def test_current_hour_with_pending_backup(
+        self, get_worker_name_mock, task_register, save_backup_group,
+        make_instance_snapshot_backup
+    ):
+
+        infra_mock = self.infra
+        infra_mock.pk = None
+        infra_mock.name = 'pending_backup_test'
+        infra_mock.backup_hour = self.backup_hour - 1
+        infra_mock.environment = self.dev_env
+        infra_mock.save()
+        print(infra_mock.environment)
+
+        instance_mock = mommy.make(
+            'Instance', databaseinfra=infra_mock
+        )
+
+        get_worker_name_mock.return_value = 'test'
+        group = BackupGroup()
+        save_backup_group.return_value = group
+        make_instance_snapshot_backup.return_value.status.return_value = (
+            Snapshot.SUCCESS
+        )
+
+        make_databases_backup()
+        calls = [
+            call(
+                current_hour=self.backup_hour, instance=self.instance,
+                error={}, group=group
+            ),
+            call(
+                current_hour=self.backup_hour, instance=instance_mock,
+                error={}, group=group
+            )
+        ]
+        make_instance_snapshot_backup.assert_has_calls(calls)
+
+        # make_instance_snapshot_backup.assert_called_with(
+        #     current_hour=self.backup_hour, instance=self.instance, error={},
+        #     group=group
+        # )
