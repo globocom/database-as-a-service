cd7b702239c0f6aa9fd68fc081c722c9503836e1
elesbom <rafael.goncalves@corp.globo.com>
2020-06-19 11:15:48 -0300

    Create Config Map Step

--- dbaas/workflow/steps/util/k8s.py
+++ dbaas/workflow/steps/util/k8s.py
@@ -6,7 +6,10 @@ import yaml
 from django.template.loader import render_to_string
 
 from base import BaseInstanceStep
-from physical.models import Volume, Host
+from physical.models import Volume, Host, Offering
+from util import get_credentials_for
+from physical.configurations import configuration_factory
+from dbaas_credentials.models import CredentialType
 
 LOG = logging.getLogger(__name__)
 
@@ -46,6 +49,10 @@ class BaseK8SStep(BaseInstanceStep):
     @property
     def pod_name(self):
         return '{}-0'.format(self.statefulset_name)
+
+    @property
+    def config_map_name(self):
+        return 'configmap-{}'.format(self.hostname())
 
     @property
     def client_class_name(self):
@@ -181,27 +188,35 @@ class NewPodK8S(BaseK8SStep):
         return 'physical/scripts/k8s/pod.yaml'
 
     @property
+    def config_map_mount_path(self):
+        return '/mnt/config-map'
+
+    @property
     def context(self):
         return {
             'POD_NAME': self.statefulset_name,
             'LABEL_NAME': self.label_name,
             'SERVICE_NAME': self.service_name,
+            'INIT_CONTAINER_CREATE_CONFIG_COMMANDS': ' cp {0}/mongodb.conf /data/configdb'.format(self.config_map_mount_path),
+            'CONFIG_MAP_MOUNT_PATH': self.config_map_mount_path,
             'IMAGE_NAME': 'mongo',
             'IMAGE_TAG': '4.2',
             'CONTAINER_PORT': 27017,  # self.instance.port
             'VOLUME_NAME': 'data-volume',
-            'VOLUME_PATH': '/data/db',
+            'VOLUME_PATH_ROOT': '/data',
+            'VOLUME_PATH_DB': '/data/db',
+            'VOLUME_SUBPATH': 'db',
             'CPU': 100,
             'MEMORY': 200,
             'CPU_LIMIT': 200,
             'MEMORY_LIMIT': 400,
-            'VOLUME_CLAIM_NAME': self.volume_claim_name
+            'VOLUME_CLAIM_NAME': self.volume_claim_name,
+            'CONFIG_MAP_NAME': self.config_map_name
         }
 
     def do(self):
         if not self.instance.is_database:
             return
-
         self.client.create_namespaced_stateful_set(
             self.namespace, self.yaml_file
         )
@@ -276,3 +291,115 @@ class SetServiceEndpoint(BaseK8SStep):
         host.save()
         self.instance.hostname = host
         self.instance.save()
+
+
+class NewConfigMapK8S(BaseK8SStep):
+    def __unicode__(self):
+        return "Creating config map..."
+
+    @property
+    def database(self):
+        from logical.models import Database
+        if self.infra.databases.exists():
+            return self.infra.databases.first()
+        database = Database()
+        step_manager = self.infra.databases_create.last()
+        database.name = (step_manager.name
+                         if step_manager else self.step_manager.name)
+        return database
+
+    @property
+    def template_path(self):
+        return 'physical/scripts/k8s/config_map.yaml'
+
+    @property
+    def context(self):
+        return {
+            'CONFIG_MAP_NAME': self.config_map_name,
+            'CONFIG_MAP_LABEL': self.label_name,
+            'CONFIG_FILE_NAME': 'mongodb.conf',
+            'CONFIG_CONTENT': render_to_string(
+                'physical/scripts/database_config_files/mongodb_40.conf',
+                self.script_variables
+            )
+        }
+
+    @property
+    def script_variables(self):
+        variables = {
+            'DATABASENAME': self.database.name,
+            'DBPASSWORD': self.infra.password,
+            'HOST': self.host.hostname.split('.')[0],
+            'HOSTADDRESS': self.instance.address,
+            'ENGINE': self.plan.engine.engine_type.name,
+            'MOVE_DATA': (
+                bool(self.upgrade) or
+                bool(self.reinstall_vm) or
+                bool(self.engine_migration)
+            ),
+            'DRIVER_NAME': self.infra.get_driver().topology_name(),
+            'DISK_SIZE_IN_GB': (self.disk_offering.size_gb()
+                                if self.disk_offering else 8),
+            'ENVIRONMENT': self.environment,
+            'HAS_PERSISTENCE': self.infra.plan.has_persistence,
+            'IS_READ_ONLY': self.instance.read_only,
+            'SSL_CONFIGURED': self.infra.ssl_configured,
+        }
+
+        if self.infra.ssl_configured:
+            from workflow.steps.util.ssl import InfraSSLBaseName
+            from workflow.steps.util.ssl import InstanceSSLBaseName
+            infra_ssl = InfraSSLBaseName(self.instance)
+            instance_ssl = InstanceSSLBaseName(self.instance)
+            variables['INFRA_SSL_CA'] = infra_ssl.ca_file_path
+            variables['INFRA_SSL_CERT'] = infra_ssl.cert_file_path
+            variables['INFRA_SSL_KEY'] = infra_ssl.key_file_path
+            variables['INSTANCE_SSL_CA'] = instance_ssl.ca_file_path
+            variables['INSTANCE_SSL_CERT'] = instance_ssl.cert_file_path
+            variables['INSTANCE_SSL_KEY'] = instance_ssl.key_file_path
+
+        variables['configuration'] = self.get_configuration()
+        variables['GRAYLOG_ENDPOINT'] = self.get_graylog_config()
+
+        return variables
+
+    def get_graylog_config(self):
+        credential = get_credentials_for(
+            environment=self.environment,
+            credential_type=CredentialType.GRAYLOG
+        )
+        return credential.get_parameter_by_name('endpoint_log')
+
+    @property
+    def offering(self):
+        if self.resize:
+            return self.resize.target_offer
+
+        try:
+            return self.infra.offering
+        except Offering.DoesNotExist:
+            return self.instance.offering
+
+    def get_configuration(self):
+        try:
+            configuration = configuration_factory(
+                self.infra, self.offering.memory_size_mb
+            )
+        except NotImplementedError:
+            return None
+        else:
+            return configuration
+
+    def do(self):
+        if not self.instance.is_database:
+            return
+
+        self.client.create_namespaced_config_map(
+            self.namespace, self.yaml_file
+        )
+
+    def undo(self):
+        self.client.delete_namespaced_config_map(
+            self.config_map_name,
+            self.namespace
+        )
