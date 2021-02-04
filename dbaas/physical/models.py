8505fb5f642a8eb9477b7ca67e89ea354efbbcee
Ricardo Dias <ricardo@ricardodias.org>
2021-02-04 20:11:12 -0300

    Add GCP provider

--- dbaas/physical/models.py
+++ dbaas/physical/models.py
@@ -555,11 +555,13 @@ class Plan(BaseModel):
     PREPROVISIONED = 0
     CLOUDSTACK = 1
     AMAZON = 2
+    GCP = 3
 
     PROVIDER_CHOICES = (
         (PREPROVISIONED, 'Pre Provisioned'),
         (CLOUDSTACK, 'Cloud Stack'),
         (AMAZON, 'Amazon'),
+        (GCP, 'Google Cloud Platform'),
     )
 
     name = models.CharField(
