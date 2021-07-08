7d69218b2158ac1096a927c427c8695c7182e741
William Marquardt <william.marquardt@g.globo>
2021-07-08 19:04:04 -0300

    WIP dark mode css

--- dbaas/admin/static/admin/js/dark_mode.js
+++ dbaas/admin/static/admin/js/dark_mode.js
@@ -1,28 +1,31 @@
 var isDark = localStorage.getItem('darkmode');
+isDark = (isDark === "true" || isDark === true)
 
 var setDarkMode = function(){
     console.log('add dark')
     django.jQuery('body').addClass('dark')
+    django.jQuery('#dmodechckbox').attr('checked', 'checked');
 }
 
 var removeDarkMode = function(){
     console.log('remove dark')
     django.jQuery('body').removeClass('dark')
+    django.jQuery('#dmodechckbox').removeAttr('checked');
 }
-
 
 if (!isDark)
     isDark = false;
 
 if (isDark){
     setDarkMode();
+}else{
+    removeDarkMode();
 }
 
 localStorage.setItem('darkmode', isDark);
 
 
 django.jQuery(document).ready(function(){
-    django.jQuery('#dmodechckbox').attr('checked', 'checked');
     django.jQuery('#dmodechckbox').on("change", function(v){
         isDark = django.jQuery('#dmodechckbox').is(':checked');
         if (isDark){
@@ -30,6 +33,8 @@ django.jQuery(document).ready(function(){
         }else{
             removeDarkMode();
         }
+
+        console.log('setting', isDark)
         localStorage.setItem('darkmode', isDark);
     });
 })
