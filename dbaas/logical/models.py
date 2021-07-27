4933d2b60e71e2d0fa4fa7004fca6f9d0ed10ebd
William Marquardt <william.marquardt@g.globo>
2021-07-27 18:20:36 -0300

    only validate if class already has an environment

--- dbaas/logical/models.py
+++ dbaas/logical/models.py
@@ -166,6 +166,9 @@ class Database(BaseModel):
             in environemnt stage'''
         super(Database, self).validate_unique(*args, **kwargs)
 
+        if not self.environment_id:
+            return
+
         environment = Environment.objects.get(pk=self.environment_id)
         db_check = Database.objects.filter(
             name=self.name,
