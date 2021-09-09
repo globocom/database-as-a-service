d75f98669022987d629c5a1fb42b795d2ec2077b
William Marquardt <william.marquardt@g.globo>
2021-09-09 11:50:21 -0300

    improve error message on debug lib

--- dev/check_debug_lib.sh
+++ dev/check_debug_lib.sh
@@ -1,5 +1,6 @@
 if [ $# -eq 0 ]
   then
+    echo "Argument not found"
     exit 1
 fi
 
@@ -10,12 +11,14 @@ echo "Trying to enable debug mode to $1..."
 
 
 if [[ ! -d $DIR ]]; then
+    echo "Path not found: $DIR"
     exit 2
 fi
 
 cd $DIR
 
 if [[ ! -f $SETUP ]]; then
+    echo "file not found $DIR/$SETUP"
     exit 3
 fi
 
