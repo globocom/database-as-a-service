8ff6c66987d411091b40768b2b2ced4b0ea862b9
William Marquardt <william.marquardt@g.globo>
2021-09-16 23:38:18 -0300

    fix host migrate

--- dbaas/maintenance/admin/host_migrate.py
+++ dbaas/maintenance/admin/host_migrate.py
@@ -47,4 +47,97 @@ class HostMigrateAdmin(DatabaseMaintenanceTaskAdmin):
         )
     link_database_migrate.short_description = "Database Migrate"
 
-  
+    def maintenance_action(self, maintenance_task):
+        if not maintenance_task.is_status_error:
+            return 'N/A'
+
+        if not maintenance_task.can_do_retry:
+            return 'N/A'
+
+        url_retry = "/admin/maintenance/hostmigrate/{}/retry/".format(
+            maintenance_task.id
+        )
+        html_retry = ("<a title='Retry' class='btn btn-info' "
+                      "href='{}'>Retry</a>").format(url_retry)
+
+        url_rollback = "/admin/maintenance/hostmigrate/{}/rollback/".format(
+            maintenance_task.id
+        )
+        html_rollback = ("<a title='Rollback' class='btn btn-danger' "
+                         "href='{}'>Rollback</a>").format(url_rollback)
+
+        spaces = '&nbsp' * 3
+        html_content = '{}{}{}'.format(html_retry, spaces, html_rollback)
+        return format_html(html_content)
+
+    def get_urls(self):
+        base = super(HostMigrateAdmin, self).get_urls()
+        admin = patterns(
+            '',
+            url(
+                r'^/?(?P<host_migrate_id>\d+)/retry/$',
+                self.admin_site.admin_view(self.retry_view),
+                name="host_migrate_retry"
+            ),
+            url(
+                r'^/?(?P<host_migrate_id>\d+)/rollback/$',
+                self.admin_site.admin_view(self.rollback_view),
+                name="host_migrate_rollback"
+            ),
+        )
+        return admin + base
+
+    def retry_view(self, request, host_migrate_id):
+        retry_from = get_object_or_404(HostMigrate, pk=host_migrate_id)
+        success, redirect = self.check_status(request, retry_from, 'retry')
+        if not success:
+            return redirect
+        host = retry_from.host
+        database = host.instances.first().databaseinfra.databases.first()
+        TaskRegister.host_migrate(
+            retry_from.host, retry_from.zone, retry_from.environment,
+            request.user, database, retry_from.current_step,
+            step_manager=retry_from, zone_origin=retry_from.zone_origin
+        )
+        return self.redirect_to_database(retry_from)
+
+    def rollback_view(self, request, host_migrate_id):
+        rollback_from = get_object_or_404(HostMigrate, pk=host_migrate_id)
+        success, redirect = self.check_status(
+            request, rollback_from, 'rollback'
+        )
+        if not success:
+            return redirect
+        TaskRegister.host_migrate_rollback(rollback_from, request.user)
+        return self.redirect_to_database(rollback_from)
+
+    def check_status(self, request, host_migrate, operation):
+        success = True
+        if success and not host_migrate.is_status_error:
+            success = False
+            messages.add_message(
+                request, messages.ERROR,
+                "You can not do {} because current status is '{}'".format(
+                    operation, host_migrate.get_status_display()
+                ),
+            )
+
+        if success and not host_migrate.can_do_retry:
+            success = False
+            messages.add_message(
+                request, messages.ERROR,
+                "{} is disabled".format(operation.capitalize())
+            )
+
+        return success, HttpResponseRedirect(
+            reverse(
+                'admin:maintenance_hostmigrate_change', args=(host_migrate.id,)
+            )
+        )
+
+    def redirect_to_database(self, maintenance):
+        infra = maintenance.host.instances.first().databaseinfra
+        database = infra.databases.first()
+        return HttpResponseRedirect(reverse(
+            'admin:logical_database_migrate', kwargs={'id': database.id})
+        )
