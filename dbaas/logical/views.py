046deeb42573587b4cfc3411839027f880a446ec
Ricardo Dias <ricardo@ricardodias.org>
2021-11-02 10:45:30 -0300

    always show sofia metrics

--- dbaas/logical/views.py
+++ dbaas/logical/views.py
@@ -820,13 +820,6 @@ def database_metrics(request, context, database):
     else:
         context['second_source'] = 'sofia'
 
-    user = request.user
-    #if len(user.team_set.filter(organization__external=False)) > 0:
-    #    context['can_show_sofia_metrics'] = True
-    #else:
-    #    context['can_show_sofia_metrics'] = False
-    context['can_show_sofia_metrics'] = True
-
     context['hosts'] = []
     for host in Host.objects.filter(
             instances__databaseinfra=database.infra,
@@ -845,16 +838,8 @@ def database_metrics(request, context, database):
         hostname__hostname__contains=context['hostname'],
     ).first()
 
-    #organization = database.team.organization
-    #if organization and organization.external:
-    #    endpoint = organization.grafana_endpoint
-    #    datasource = organization.grafana_datasource
-    #else:
-    #    endpoint = credential.endpoint
-    #    datasource = credential.get_parameter_by_name('environment')
     endpoint = credential.endpoint
     datasource = credential.get_parameter_by_name('environment')
-
 
     engine_type = (
         database.engine_type if not database.engine_type == "mysql_percona"
@@ -873,12 +858,7 @@ def database_metrics(request, context, database):
         datasource
     )
 
-    #if organization and organization.external:
-    #    grafana_url_zabbix += "&orgId={}".format(organization.grafana_orgid)
-
     context['grafana_url_zabbix'] = grafana_url_zabbix
-
-    #print("grafana_url_zabbix:{}", grafana_url_zabbix)
 
     dashboard = credential.get_parameter_by_name(
                 'sofia_dbaas_database_dashboard'
