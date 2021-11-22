1e685ac651e6ff839df78a41e9a4a9f7e2c1ee15
Ricardo Dias <ricardo@ricardodias.org>
2021-11-22 17:34:33 -0300

    add env filter

--- dbaas/logical/views.py
+++ dbaas/logical/views.py
@@ -1104,7 +1104,7 @@ def database_resizes(request, context, database):
     if not disk_used_size_kb:
         disk_used_size_kb = database.used_size_in_kb
     context['disk_offerings'] = list(
-        DiskOffering.objects.filter(size_kb__gt=disk_used_size_kb)
+        database.environment.diskofferings.filter(size_kb__gt=disk_used_size_kb)
     )
     if database.infra.disk_offering not in context['disk_offerings']:
         context['disk_offerings'].insert(0, database.infra.disk_offering)
