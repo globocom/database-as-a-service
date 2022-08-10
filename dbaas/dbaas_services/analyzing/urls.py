958da5f93e24a376826e1ec10c4dc3ad0c66b651
Ylgner Becton <ylgner.becton@gmail.com>
2022-08-09 22:29:12 -0300

    feature: database report

--- dbaas/dbaas_services/analyzing/urls.py
+++ dbaas/dbaas_services/analyzing/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from views import SubUsedResourceReport
+from views import SubUsedResourceReport, DatabaseReport
 
 urlpatterns = [
     url(r'^analyzing/reports/$', SubUsedResourceReport.as_view(),
         name='sub_used_resource_report'),
+    url(r'^analyzing/report/$', DatabaseReport.as_view(),
+        name='database_report'),
 ]
