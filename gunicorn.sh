2da851f5cc4fcd5ac71a625e13c18ba8937e5c68
Marcelo RS Soares <marcelo.rsoares@g.globo>
2022-12-05 02:58:53 -0300

    Several small updates so the docker works better, requirements install everything, but there is still things to clean

--- gunicorn.sh
+++ gunicorn.sh
@@ -4,7 +4,12 @@
 
 
 cd /home/app/dbaas
-gunicorn dbaas.wsgi
+gunicorn dbaas.wsgi \
+    --bind 0.0.0.0:$PORT \
+    --workers $WORKERS \
+    --timeout $TIMEOUT \
+    --log-level=$DEBUG_LEVEL
+    
 
 # python manage.py run_gunicorn
 # python manage.py runserver 0.0.0.0:8000
