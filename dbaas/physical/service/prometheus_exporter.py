f1280dc09f7d3cf633338c64ae646378a4eaa370
Mateus Erkmann <erkmann08@gmail.com>
2023-02-01 09:44:32 -0300

    restart service

--- dbaas/physical/service/prometheus_exporter.py
+++ dbaas/physical/service/prometheus_exporter.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 
 from dbaas_credentials.models import CredentialType
@@ -7,19 +8,21 @@ LOG = logging.getLogger(__name__)
 
 
 class Exporter(object):
-    def __init__(self, path, user, password, project):
+    def __init__(self, path, user, password, project, service_name):
         self.exporter_path = path
         self.default_user = user
         self.default_password = password
         self.project = project
+        self.service_name = service_name
 
     def configure_host_exporter(self, host):
-        self.change_password(host)
+        self.change_credentials(host)
         self.change_address(host)
+        self.restart_service(host)
 
-    def change_password(self, host):
+    def change_credentials(self, host):
         LOG.info("Changing password for Host %s", host.hostname)
-        return self._change_password(host, self.get_infra_credentials_from_host(host))
+        return self._change_credentials(host, self.get_infra_credentials_from_host(host))
 
     def change_address(self, host, address=None):
         LOG.info("Changing address for Host %s", host.hostname)
@@ -30,16 +33,12 @@ class Exporter(object):
         return self._get_infra_credentials(host.infra)
 
     def overwrite_config_file(self, host, new_config_string):
-        LOG.info('Creating temp prometheus config file for Host %s', host.hostname)
-        host.ssh.create_temp_file(file_name='new_config.service', content=new_config_string)
+        LOG.info('Overwriting prometheus config file for Host %s', host.hostname)
+        return self._overwrite_config(host, new_config_string)
 
-        LOG.info('Moving original prometheus config file to backup for Host %s', host.hostname)
-        host.ssh.run_script('mv {} {}-backup'.format(self.exporter_path, self.exporter_path))
-
-        LOG.info('Moving temp config file as main config file for Host %s', host.hostname)
-        host.ssh.run_script('mv {} {}'.format('/tmp/new_config.service', self.exporter_path))
-
-        return True
+    def restart_service(self, host):
+        LOG.info('Restarting service for Host %s', host.hostname)
+        return self._restart_service(host)
 
     @property
     def cat_exporter_config_command(self):
@@ -49,7 +48,7 @@ class Exporter(object):
         LOG.info('Getting service file for Host %s', host.hostname)
         return self._get_service_file(host)
 
-    def _change_password(self, host, credentials):
+    def _change_credentials(self, host, credentials):
         return
 
     def _change_address(self, host, address=None):
@@ -62,6 +61,22 @@ class Exporter(object):
         service_config_file = host.ssh.run_script(self.cat_exporter_config_command)['stdout']
         return ''.join(service_config_file)
 
+    def _restart_service(self, host):
+        host.ssh.run_script('systemctl daemon-reload & systemctl restart {}'.format(self.service_name))
+
+    def _overwrite_config(self, host, new_config_string):
+        LOG.info('Creating temp prometheus config file for Host %s', host.hostname)
+        host.ssh.create_temp_file(file_name='new_config.service', content=new_config_string)
+
+        LOG.info('Moving original prometheus config file to backup for Host %s', host.hostname)
+        host.ssh.run_script('mv {} {}-backup-{}'.format(
+            self.exporter_path, self.exporter_path, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
+
+        LOG.info('Moving temp config file as main config file for Host %s', host.hostname)
+        host.ssh.run_script('mv {} {}'.format('/tmp/new_config.service', self.exporter_path))
+
+        return True
+
 
 class RedisExporter(Exporter):
     def __init__(self, project):
@@ -69,13 +84,14 @@ class RedisExporter(Exporter):
             path='/etc/systemd/system/redis_exporter.service',
             user=None,
             password=None,
-            project=project
+            project=project,
+            service_name='redis_exporter'
         )
 
     def _get_infra_credentials(self, infra):
         return {'user': None, 'password': infra.password}
 
-    def _change_password(self, host, credentials):
+    def _change_credentials(self, host, credentials):
         config_string = self.get_service_file(host)
 
         part1 = config_string.split('--redis.password=')[0]
@@ -107,7 +123,8 @@ class MongoDBExporter(Exporter):
             path='/etc/systemd/system/mongodb_exporter.service',
             user=credential.user,
             password=credential.password,
-            project=project
+            project=project,
+            service_name='mongodb_exporter'
         )
 
     def _get_infra_credentials(self, infra):
@@ -115,8 +132,23 @@ class MongoDBExporter(Exporter):
 
     def _change_address(self, host, address=None):
         config_string = self.get_service_file(host)
-        # TODO: change address for mongodb config file
-        return
+
+        part1 = config_string.split('@')[0]
+        part2 = config_string.split('@')[1]
+        part2 = ' --' + part2.split('--', 1)[1]
+
+        new_config_string = '{}@{}:27017/admin{}'.format(part1, host.address, part2)
+        self.overwrite_config_file(host, new_config_string)
+
+    def _change_credentials(self, host, credentials):
+        config_string = self.get_service_file(host)
+
+        part1 = config_string.split('mongodb://')[0]
+        part2 = config_string.split('mongodb://')[1]
+        part2 = '@' + part2.split('@', 1)[1]
+
+        new_config_string = '{}mongodb://{}:{}{}'.format(part1, credentials['user'], credentials['password'], part2)
+        self.overwrite_config_file(host, new_config_string)
 
 
 class MySQLExporter(Exporter):
@@ -127,11 +159,27 @@ class MySQLExporter(Exporter):
         )
 
         super(MySQLExporter, self).__init__(
-            path='/etc/systemd/system/mysql_exporter.service',
+            path='/etc/.dbconf.cnf',
             user=credential.user,
             password=credential.password,
-            project=project
+            project=project,
+            service_name='mysql_exporter'
         )
 
     def _get_infra_credentials(self, infra):
         return {'user': self.default_user, 'password': self.default_password}
+
+    def _change_credentials(self, host, credentials):
+        config_string = self.get_service_file(host)
+
+        part1_user = config_string.split('user=')[0]
+        part2_user = config_string.split('user=')[1]
+        part2_user = '\n' + part2_user.split('\n', 1)[1]
+
+        new_config_string = '{}user={}{}'.format(part1_user, credentials['user'], part2_user)
+
+        part1_pw = new_config_string.split('password=')[0]
+
+        new_config_string = '{}password={}'.format(part1_pw, credentials['password'])
+
+        self.overwrite_config_file(host, new_config_string)
