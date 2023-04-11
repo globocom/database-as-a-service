5fe0e391c94374af4b0555bc9f73fa752e27ec63
DANIELI.LAGACIONE <daniele.lagacione@WP090350.local>
2023-04-11 18:09:12 -0300

    has permission database report only role_dba

--- dbaas/dbaas/views.py
+++ dbaas/dbaas/views.py
@@ -1,15 +1,27 @@
 from system.models import Configuration
 from django.views.generic import TemplateView
+from account.models import Role
 
 
 def external_links(request):
     iaas_status = Configuration.get_by_name('iaas_status')
     iaas_quota = Configuration.get_by_name('iaas_quota')
 
+    user = str(request.user)
+    role_dba = Role.objects.get(name='role_dba')
+    users_if_dba_role = role_dba.team_set.values_list('name', flat=True)
+
+    if user in users_if_dba_role:
+        is_dba = True
+    else:
+        is_dba = False
 
     return {'iaas_status': iaas_status,
-            'iaas_quota': iaas_quota}
+            'iaas_quota': iaas_quota,
+            'is_dba': is_dba}
 
 
 class DeployView(TemplateView):
     template_name = 'deploy/deploy.html'
+
+
--- dbaas/dbaas_services/analyzing/views.py
+++ dbaas/dbaas_services/analyzing/views.py
@@ -47,23 +47,28 @@ class DatabaseReport(ListView):
         from_teams = [x.role for x in Team.objects.filter(users=request.user)]
         role_dba = Role.objects.get(name='role_dba')
 
-        if role_dba not in from_teams:
-            return render(request, 'databases/index.html', {"has_perm": False})
+        if role_dba in from_teams:
+            return render(request, 'databases/index.html', {"has_perm": True})
 
     def get(self, request, *args, **kwargs):
-        self.has_perm(request)
+        if self.has_perm(request):
+           return render(request, 'databases/index.html', {"has_perm": True})
 
-        return render(request, 'databases/index.html', {"has_perm": True})
+        return HttpResponseRedirect(reverse('admin:index'))
+
 
     def post(self, request, *args, **kwargs):
-        self.has_perm(request)
+        if self.has_perm(request):
+            database_report = request.POST.get("database_report", "")
 
-        database_report = request.POST.get("database_report", "")
 
-        if database_report == 'database_report':
-            return self.default_database_report()
-        else:
-            return self.vm_by_line_database_report()
+            if database_report == 'database_report':
+                return self.default_database_report()
+            else:
+                return self.vm_by_line_database_report()
+
+        return HttpResponseRedirect(reverse('admin:index'))
+
 
     def vm_by_line_database_report(self):
 
